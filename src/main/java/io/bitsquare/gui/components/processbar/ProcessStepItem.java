0d048de7fcaa4fed73f075fbd4c1fde64a1f57e4
Chris Beams <chris@beams.io>
2014-10-05 20:00:38 +0200

    Add missing GPL headers
    Per 8746ab7, IDEA's copyright configuration had been wiped out for some
    time. Now that it is back in place, this change is the result of a
    global "Update copyright..." action (with a bit of pruning afterward).

--- src/main/java/io/bitsquare/gui/components/processbar/ProcessStepItem.java
+++ src/main/java/io/bitsquare/gui/components/processbar/ProcessStepItem.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Bitsquare. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package io.bitsquare.gui.components.processbar;
 
 public class ProcessStepItem {
