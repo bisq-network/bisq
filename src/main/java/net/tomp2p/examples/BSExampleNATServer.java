75682741b04f52dd6858e710f31dc5931b8bca39
Manfred Karrer <mk@nucleo.io>
2014-10-13 17:55:42 +0200

    Test apps for basic bootstrapping

---
+++ src/main/java/net/tomp2p/examples/BSExampleNATServer.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Bitsquare.
+ *
+ * Bitsquare is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bitsquare is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bitsquare. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright 2011 Thomas Bocek
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.tomp2p.examples;
+
+import java.util.Random;
+
+import net.tomp2p.p2p.Peer;
+import net.tomp2p.p2p.PeerBuilder;
+import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.PeerAddress;
+
+public class BSExampleNATServer {
+    private final static int PORT_SERVER = 5000;
+
+    public static void startServer() throws Exception {
+        Random r = new Random(42L);
+        Peer peer = new PeerBuilder(new Number160(r)).ports(PORT_SERVER).start();
+        System.out.println("peer started.");
+        for (; ; ) {
+            for (PeerAddress pa : peer.peerBean().peerMap().all()) {
+                System.out.println("peer online (TCP):" + pa);
+            }
+            Thread.sleep(2000);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        startServer();
+    }
+}
