1a92b05bf4ad49a4749c1f9ff69976afc400c4ac
Chris Beams <chris@beams.io>
2014-11-06 13:02:49 +0100

    Introduce @Repeat and RepeatRule for repeatable @Test methods
    With thanks to original author Frank Appel (@fappel)
    
    See:
     - https://gist.github.com/fappel/8bcb2aea4b39ff9cfb6e
     - http://www.codeaffine.com/2013/04/10/running-junit-tests-repeatedly-without-loops/

---
+++ src/test/java/io/bitsquare/util/Repeat.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Bitsquare.
+ *
+ * Bitsquare is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bitsquare is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bitsquare. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.bitsquare.util;
+
+import java.lang.annotation.ElementType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method-level annotation to be used in connection with {@link RepeatRule} to cause a
+ * given {@link org.junit.Test} method to be repeated a specified number of times.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Repeat {
+
+    /**
+     * Specifies the number of times to repeat the annotated {@link org.junit.Test}.
+     */
+    int value();
+}
