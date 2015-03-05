74f785cdc228eebbc78bc2d6abea99239dafbca8
Manfred Karrer <mk@nucleo.io>
2015-03-05 21:13:49 +0100

    Fix win build scripts

--- package/windows/create_app.bat
+++ package/windows/create_app.bat
@@ -1,15 +1,10 @@
 cd ..\..\
 
-:: edit buildVersion number
-@echo off set buildVersion=2
-
-:: edit fullVersion number
-@echo off set fullVersion=1.1
-
 :: edit iss file -> AppVersion=0.1.1
 
 call mvn clean package -DskipTests -Dmaven.javadoc.skip=true
-copy gui\target\shaded.jar gui\updatefx\builds\%buildVersion%.jar
+:: edit buildVersion number
+copy gui\target\shaded.jar gui\updatefx\builds\1.jar
 
 :: edit url
 call java -Xmx2048m -jar ./updatefx/updatefx-app-1.2.jar --url=http://bitsquare.io/updateFX/ gui/updatefx
@@ -17,9 +12,10 @@ call java -Xmx2048m -jar ./updatefx/updatefx-app-1.2.jar --url=http://bitsquare.
 :: 64 bit build
 :: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php)
 :: Build with jdk1.8.0_40 fails but jdk1.8.0_3 works
-call "C:\Program Files\Java\jdk1.8.0_31\bin\javapackager.exe" -deploy -BappVersion=%fullVersion% -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.gui.BitsquareAppMain -srcfiles "gui\updatefx\builds\processed\%buildVersion%.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_31\jre"
+:: edit buildVersion and fullVersion number
+call "C:\Program Files\Java\jdk1.8.0_31\bin\javapackager.exe" -deploy -BappVersion=0.1.1 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.gui.BitsquareAppMain -srcfiles "gui\updatefx\builds\processed\1.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_31\jre"
 
 :: 32 bit build
-:: call "C:\Program Files (x86)\Java\jdk1.8.0_31\bin\javapackager.exe" -deploy -BappVersion=%fullVersion% -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.gui.BitsquareAppMain -srcfiles "gui\updatefx\builds\processed\%buildVersion%.jar" -outfile Bitsquare -Bruntime="C:\Program Files (x86)\Java\jdk1.8.0_31\jre"
+:: call "C:\Program Files (x86)\Java\jdk1.8.0_31\bin\javapackager.exe" -deploy -BappVersion=0.1.1 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.gui.BitsquareAppMain -srcfiles "gui\updatefx\builds\processed\1.jar" -outfile Bitsquare -Bruntime="C:\Program Files (x86)\Java\jdk1.8.0_31\jre"
 
 cd package\windows
--- package/windows/setup.bat
+++ package/windows/setup.bat
@@ -6,7 +6,7 @@ mkdir gui\updatefx\builds
 mkdir gui\updatefx\builds\processed
 mkdir gui\updatefx\site
 mkdir gui\deploy
-mkdir gui\win-32bit
+:: mkdir gui\win-32bit
 
 :: Copy wallet file from main build
 call java -Xmx2048m -jar ./updatefx/updatefx-app-1.2.jar --url=http://bitsquare.io/updateFX/ gui/updatefx
