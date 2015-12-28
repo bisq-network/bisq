7131bc47a12c0fc10d70cd8297abaf90664022e6
Manfred Karrer <mk@nucleo.io>
2015-12-28 19:50:18 +0100

    update build file

--- package/windows/create_app.bat
+++ package/windows/create_app.bat
@@ -10,10 +10,9 @@ mvn clean package -DskipTests -Dmaven.javadoc.skip=true
 
 :: 64 bit build
 :: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php)
-:: Build with jdk1.8.0_25 fails but jdk1.8.0_3 works
-call "C:\Program Files\Java\jdk1.8.0_25\bin\javapackager.exe" -deploy -BappVersion=0.3.2.2 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "gui\target\shaded.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_25\jre"
+call "C:\Program Files\Java\jdk1.8.0_66\bin\javapackager.exe" -deploy -BappVersion=0.3.2.2 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "gui\target\shaded.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_66\jre"
 
 :: 32 bit build
-:: call "C:\Program Files (x86)\Java\jdk1.8.0_25\bin\javapackager.exe" -deploy -BappVersion=0.3.2.2 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "gui\target\shaded.jar" -outfile Bitsquare -Bruntime="C:\Program Files (x86)\Java\jdk1.8.0_25\jre"
+:: call "C:\Program Files (x86)\Java\jdk1.8.0_66\bin\javapackager.exe" -deploy -BappVersion=0.3.2.2 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir gui\deploy -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "gui\target\shaded.jar" -outfile Bitsquare -Bruntime="C:\Program Files (x86)\Java\jdk1.8.0_66\jre"
 
 cd package\windows
