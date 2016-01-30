45fb2bfafbc34c4ed419b9fa5c47ba858d15f194
Manfred Karrer <mk@nucleo.io>
2016-01-30 04:31:26 +0100

    change log level

--- common/src/main/java/io/bitsquare/app/Log.java
+++ common/src/main/java/io/bitsquare/app/Log.java
@@ -48,7 +48,7 @@ public class Log {
         rollingPolicy.start();
 
         triggeringPolicy = new SizeBasedTriggeringPolicy();
-        triggeringPolicy.setMaxFileSize(useDetailedLogging ? "10MB" : "1MB");
+        triggeringPolicy.setMaxFileSize("1MB");
         triggeringPolicy.start();
 
         PatternLayoutEncoder encoder = new PatternLayoutEncoder();
@@ -63,7 +63,7 @@ public class Log {
 
         logbackLogger = loggerContext.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         //TODO for now use always trace 
-        logbackLogger.setLevel(useDetailedLogging ? Level.TRACE : Level.TRACE);
+        logbackLogger.setLevel(useDetailedLogging ? Level.TRACE : Level.INFO);
         // logbackLogger.setLevel(useDetailedLogging ? Level.TRACE : Level.DEBUG);
         logbackLogger.addAppender(appender);
     }
