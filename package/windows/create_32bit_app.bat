d6ca10523dd3eaddb3b99928bbae603d2148fb2c
Manfred Karrer <mk@nucleo.io>
2016-03-28 05:03:40 +0200

    Version 0.3.7

--- package/windows/create_32bit_app.bat
+++ package/windows/create_32bit_app.bat
@@ -1,13 +1,13 @@
 cd ..\..\
 mkdir gui\deploy
 
-:: edit iss file -> AppVersion=0.3.6
+:: edit iss file -> AppVersion=0.3.7
 
 :: Copy gui/deploy.Bitsquare.jar file from mac build to windows
-:: edit -> -BappVersion=0.3.6 and -srcfiles
+:: edit -> -BappVersion=0.3.7 and -srcfiles
 
 :: 32 bit build
 :: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php) 
-call "C:\Program Files\Java\jdk1.8.0_77\bin\javapackager.exe" -deploy -BappVersion=0.3.6 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir "\\VBOXSVR\vm_shared_windows_32bit" -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "\\VBOXSVR\vm_shared_windows_32bit\Bitsquare-0.3.6.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_77\jre" -BjvmProperties=-Djava.net.preferIPv4Stack=true  
+call "C:\Program Files\Java\jdk1.8.0_77\bin\javapackager.exe" -deploy -BappVersion=0.3.7 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir "\\VBOXSVR\vm_shared_windows_32bit" -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "\\VBOXSVR\vm_shared_windows_32bit\Bitsquare-0.3.7.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_77\jre" -BjvmProperties=-Djava.net.preferIPv4Stack=true  
 
 cd package\windows
