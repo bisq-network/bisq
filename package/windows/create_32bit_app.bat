76b3cb3b3430f2f811a3003eb75737ea65ab6ceb
Manfred Karrer <mk@nucleo.io>
2016-03-28 04:21:58 +0200

    Fix wrong path

--- package/windows/create_32bit_app.bat
+++ package/windows/create_32bit_app.bat
@@ -7,7 +7,7 @@ mkdir gui\deploy
 :: edit -> -BappVersion=0.3.6 and -srcfiles
 
 :: 32 bit build
-:: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php)
-call "C:\Program Files (x86)\Java\jdk1.8.0_77\bin\javapackager.exe" -deploy -BappVersion=0.3.6 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir "\\VBOXSVR\vm_shared_windows_32bit" -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "\\VBOXSVR\vm_shared_windows_32bit\Bitsquare-0.3.6.jar" -outfile Bitsquare -Bruntime="C:\Program Files (x86)\Java\jdk1.8.0_77\jre" -BjvmProperties=-Djava.net.preferIPv4Stack=true  
+:: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php) 
+call "C:\Program Files\Java\jdk1.8.0_77\bin\javapackager.exe" -deploy -BappVersion=0.3.6 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir "\\VBOXSVR\vm_shared_windows_32bit" -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "\\VBOXSVR\vm_shared_windows_32bit\Bitsquare-0.3.6.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_77\jre" -BjvmProperties=-Djava.net.preferIPv4Stack=true  
 
 cd package\windows
