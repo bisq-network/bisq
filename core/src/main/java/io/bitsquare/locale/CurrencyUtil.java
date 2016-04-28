bf2b4fd8275b816db8c0edcf2f6bead006a3665b
Manfred Karrer <mk@nucleo.io>
2016-04-29 00:02:37 +0200

    Added Sibcoin

--- core/src/main/java/io/bitsquare/locale/CurrencyUtil.java
+++ core/src/main/java/io/bitsquare/locale/CurrencyUtil.java
@@ -106,7 +106,8 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("MXT", "MarteXcoin"));
         result.add(new CryptoCurrency("GRS", "Groestlcoin"));
         result.add(new CryptoCurrency("IOC", "I/O Coin"));
-
+        result.add(new CryptoCurrency("SIB", "Sibcoin"));
+        
         // result.add(new CryptoCurrency("XMR", "Monero")); 
         // result.add(new CryptoCurrency("BCN", "Bytecoin"));
         return result;
