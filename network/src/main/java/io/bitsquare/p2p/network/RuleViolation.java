ad33d78fcbadbfd45c9e3425f7314e58191b1c1a
Manfred Karrer <mk@nucleo.io>
2016-05-24 18:44:50 +0200

    Relax rule violations

--- network/src/main/java/io/bitsquare/p2p/network/RuleViolation.java
+++ network/src/main/java/io/bitsquare/p2p/network/RuleViolation.java
@@ -1,11 +1,11 @@
 package io.bitsquare.p2p.network;
 
 public enum RuleViolation {
-    INVALID_DATA_TYPE(0),
+    INVALID_DATA_TYPE(2),
     WRONG_NETWORK_ID(0),
     MAX_MSG_SIZE_EXCEEDED(2),
-    THROTTLE_LIMIT_EXCEEDED(1),
-    TOO_MANY_REPORTED_PEERS_SENT(1);
+    THROTTLE_LIMIT_EXCEEDED(2),
+    TOO_MANY_REPORTED_PEERS_SENT(2);
 
     public final int maxTolerance;
 
