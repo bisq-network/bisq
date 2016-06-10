e2cb35110eb92e873d56c075dfe7bce24c44631a
Manfred Karrer <mk@nucleo.io>
2016-06-10 21:09:49 +0200

    Add scripts for stress test

--- package/stresstest/startnodes.sh
+++ package/stresstest/startnodes.sh
@@ -2,22 +2,24 @@
 
 cd ../../seednode/target
 
-logpath=/Users/mk/Documents/_intellij/bitsquare
+logpath=/Users/mk/Documents/_intellij/bitsquare/logs
 
-nohup java -jar SeedNode.jar hlitt7z4bec4kdh4.onion:8000 0 500 >/dev/null 2>$logpath/stresstest_seednode.log & 
+mkdir $logpath
+
+nohup java -jar SeedNode.jar hlitt7z4bec4kdh4.onion:8000 0 500 >/dev/null 2>$logpath/ST_0_seednode.log & 
 sleep 40
 
 cd ../../gui/target
-nohup java -jar shaded.jar --app.name=BS_arb --maxConnections=500 >/dev/null 2>$logpath/stresstest_arb.log & 
+nohup java -jar shaded.jar --app.name=BS_arb --maxConnections=120 >/dev/null 2>$logpath/ST_0_arb.log & 
 sleep 40
 
-delay=5
+delay=10
 for i in `seq 1 10`;
-	do
+  do
 	echo $i
-	nohup java -jar shaded.jar --app.name=BS_$i --maxConnections=500 >/dev/null 2>$logpath/stresstest_$i.log &  
+	nohup java -jar shaded.jar --app.name=BS_$i --maxConnections=120 >/dev/null 2>$logpath/ST_$i.log &  
 	sleep $delay
-done  
+  done  
 
 # kill `ps -ef | grep java | grep -v grep | awk '{print $2}'`
 
