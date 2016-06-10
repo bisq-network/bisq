5fea34bb5470484eeef2066d769ee43ba43fb432
Manfred Karrer <mk@nucleo.io>
2016-06-10 22:48:41 +0200

    update scripts

---
+++ package/stresstest/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+logpath=/Users/bs/bitsquare/logs
+datapath=/Users/bs/.local/shared
+
+mkdir -p $logpath
+
+cd ../../gui/target
+
+delay=5
+for i in `seq 1 10`;
+  do
+	echo $i
+	rm $datapath/BS_$i/mainnet/db/SequenceNumberMap
+	rm $datapath/BS_$i/mainnet/db/PersistedPeers
+	nohup java -jar shaded.jar --app.name=BS_$i --maxConnections=12 >/dev/null 2>$logpath/ST_$i.log &  
+	sleep $delay
+  done  
+
+# kill `ps -ef | grep java | grep -v grep | awk '{print $2}'`
+
