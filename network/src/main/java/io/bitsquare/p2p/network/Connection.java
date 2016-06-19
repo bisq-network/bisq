499e5997f06912441a023e2612d61cf6fc494296
Manfred Karrer <mk@nucleo.io>
2016-06-20 01:51:33 +0200

    Incrase throttle limit

--- network/src/main/java/io/bitsquare/p2p/network/Connection.java
+++ network/src/main/java/io/bitsquare/p2p/network/Connection.java
@@ -67,8 +67,8 @@ public class Connection implements MessageListener {
     static final int MAX_MSG_SIZE = 200 * 1024;                       // 200 kb
     static final int MAX_MSG_SIZE_GET_DATA = 6 * 1024 * 1024;         // 6 MB (425 offers resulted in about 660 kb, mailbox msg will add more to it) offer has usually 2 kb, mailbox 3kb.
     //TODO decrease limits again after testing
-    static final int MSG_THROTTLE_PER_SEC = 100;              // With MAX_MSG_SIZE of 200kb results in bandwidth of 20MB/sec or 2.5 mbit/sec
-    static final int MSG_THROTTLE_PER_10_SEC = 500;          // With MAX_MSG_SIZE of 200kb results in bandwidth of 10MB/sec or 1.25 mbit/sec
+    static final int MSG_THROTTLE_PER_SEC = 200;              // With MAX_MSG_SIZE of 200kb results in bandwidth of 40MB/sec or 5 mbit/sec
+    static final int MSG_THROTTLE_PER_10_SEC = 1000;          // With MAX_MSG_SIZE of 200kb results in bandwidth of 20MB/sec or 2.5 mbit/sec
     private static final int SOCKET_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(90);
 
     public static int getMaxMsgSize() {
