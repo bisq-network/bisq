954a5599414492534d0e62597ea089a4bd3a1962
Manfred Karrer <mk@nucleo.io>
2016-06-21 00:11:04 +0200

    Update bridge provider

--- jtorproxy/src/main/java/io/nucleo/net/bridge/BridgeProvider.java
+++ jtorproxy/src/main/java/io/nucleo/net/bridge/BridgeProvider.java
@@ -13,8 +13,13 @@ public class BridgeProvider {
 
     private static List<String> getDefaultBridges() {
         return Arrays.asList(
-                // private bridge
-                "bridge 158.69.102.94:443 C2FA8AE17D5115B493CC694C0D9851FD4166B11D");
+                "bridge 220.213.33.49:443 BF402231C0E600117354EBA1F6D333EBCF2EB0D0",
+                "bridge 95.27.160.46:9201 DDDF9CFCF995E02F8251F740DC32B2836A15B28A",
+                "bridge 85.204.57.66:443 E7CA5C41D8D137E00940235B9B7484E7D2138277",
+                "bridge 194.132.208.163:17538 255B37D126669DF93A51D767ECF1618BE0393B3A",
+                "bridge 80.240.140.199:8443 91C5392AB05F70ACDCC0005EA8A44A181C1478F1",
+                "bridge 78.46.223.54:443 C0B6748FD3F781798A264A74D7AB5436E248A309"
+        );
     }
 
     public static List<String> getBridges() {
