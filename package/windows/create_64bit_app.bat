ac0aac949b323fb51746c93ae83d9963098ea971
Manfred Karrer <mk@nucleo.io>
2016-06-29 19:00:18 +0200

    Add Xbootclasspath for windows (still not working with the app, but with the jar)

--- package/windows/create_64bit_app.bat
+++ package/windows/create_64bit_app.bat
@@ -1,4 +1,4 @@
-cd ..\..\
+﻿﻿cd ..\..\
 mkdir gui\deploy
 
 :: edit iss file -> AppVersion
@@ -8,6 +8,27 @@ mkdir gui\deploy
 
 :: 64 bit build
 :: Needs Inno Setup 5 or later (http://www.jrsoftware.org/isdl.php)
-call "C:\Program Files\Java\jdk1.8.0_92\bin\javapackager.exe" -deploy -BappVersion=0.4.9 -native exe -name Bitsquare -title Bitsquare -vendor Bitsquare -outdir "\\VBOXSVR\vm_shared_windows" -appclass io.bitsquare.app.BitsquareAppMain -srcfiles "\\VBOXSVR\vm_shared_windows\Bitsquare-0.4.9.jar" -outfile Bitsquare -Bruntime="C:\Program Files\Java\jdk1.8.0_92\jre" -BjvmProperties=-Djava.net.preferIPv4Stack=true  
- 
+
+SET version=0.4.9
+SET jdk=C:\Program Files\Java\jdk1.8.0_92
+SET outdir=\\VBOXSVR\vm_shared_windows
+
+call "%jdk%\bin\javapackager.exe" -deploy ^
+-BjvmOptions=-Xbootclasspath/a:^"jdkfix-%version%.jar^";^"..\runtime\lib\ext\jfxrt.jar^" ^
+-BappVersion=%version% ^
+-native exe ^
+-name Bitsquare ^
+-title Bitsquare ^
+-vendor Bitsquare ^
+-outdir %outdir% ^
+-appclass io.bitsquare.app.BitsquareAppMain ^
+-srcfiles "%outdir%\Bitsquare-%version%.jar;%outdir%\jdkfix-%version%.jar" ^
+-outfile Bitsquare ^
+-Bruntime="%jdk%\jre" ^
+-BjvmProperties=-Djava.net.preferIPv4Stack=true
+
+:: -BjvmOptions=-verbose:class
+:: that works if used form the terminal:
+:: java -Xbootclasspath/a:"jdkfix-0.4.9.jar";"C:\Users\asd\AppData\Local\Bitsquare\runtime\lib\ext\jfxrt.jar" -jar Bitsquare-0.4.9.jar
+
 cd package\windows
