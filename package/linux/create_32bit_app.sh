0274edca4aff7396a8123699ee368c6ad917b1fa
Manfred Karrer <mk@nucleo.io>
2016-07-05 15:45:08 +0200

    Update linux build script

--- package/linux/create_32bit_app.sh
+++ package/linux/create_32bit_app.sh
@@ -5,15 +5,19 @@ mkdir -p gui/deploy
 
 set -e
 
-# Edit versions
-fullVersion=0.4.9
-jarFile="/home/bitsquare/Desktop/sf_vm_shared_ubuntu14_32bit/Bitsquare-$fullVersion.jar"
+# Edit version
+version=0.4.9
+
+jarFile="/media/sf_vm_shared_ubuntu14_32bit/Bitsquare-$version.jar"
+jdkfixFile="/media/sf_vm_shared_ubuntu14_32bit/jdkfix-$version.jar"
 
 # Note: fakeroot needs to be installed on linux
 $JAVA_HOME/bin/javapackager \
     -deploy \
-    -BappVersion=$fullVersion \
-    -Bcategory=Office,Finance \
+    -BjvmOptions=-Xbootclasspath/a:"jdkfix-$version.jar":"../runtime/lib/ext/jfxrt.jar" \
+    -Bruntime="$JAVA_HOME/jre" \
+    -BappVersion=$version \
+    -Bcategory=Internet \
     -Bemail=team@bitsquare.io \
     -BlicenseType=GPLv3 \
     -BlicenseFile=LICENSE \
@@ -23,7 +27,7 @@ $JAVA_HOME/bin/javapackager \
     -title Bitsquare \
     -vendor Bitsquare \
     -outdir gui/deploy \
-    -srcfiles $jarFile \
+    -srcfiles $jarFile:$jdkfixFile \
     -srcfiles package/linux/LICENSE \
     -appclass io.bitsquare.app.BitsquareAppMain \
     -outfile Bitsquare
@@ -31,9 +35,9 @@ $JAVA_HOME/bin/javapackager \
 rm gui/deploy/Bitsquare.html
 rm gui/deploy/Bitsquare.jnlp
 rm gui/deploy/LICENSE
-mv "gui/deploy/bundles/bitsquare-$fullVersion.deb" "gui/deploy/Bitsquare-32bit-$fullVersion.deb"
+mv "gui/deploy/bundles/bitsquare-$version.deb" "gui/deploy/Bitsquare-32bit-$version.deb"
 rmdir gui/deploy/bundles
-cp "gui/deploy/Bitsquare-32bit-$fullVersion.deb" "/home/bitsquare/Desktop/sf_vm_shared_ubuntu14_32bit/Bitsquare-32bit-$fullVersion.deb"
-cp "gui/deploy/Bitsquare-32bit-$fullVersion.deb" "/home/bitsquare/Desktop/Bitsquare-32bit-$fullVersion.deb"
+cp "gui/deploy/Bitsquare-32bit-$version.deb" "/media/sf_vm_shared_ubuntu14_32bit/Bitsquare-32bit-$version.deb"
+cp "gui/deploy/Bitsquare-32bit-$version.deb" "/home/bitsquare/Desktop/Bitsquare-32bit-$version.deb"
 
 cd package/linux
