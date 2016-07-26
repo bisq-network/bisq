19d600c09d470b5176e07cd2e7cf45a3a052aac2
Manfred Karrer <mk@nucleo.io>
2016-07-26 23:03:51 +0200

    Fix ETHC info

--- core/src/main/java/io/bitsquare/locale/CurrencyUtil.java
+++ core/src/main/java/io/bitsquare/locale/CurrencyUtil.java
@@ -131,7 +131,7 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("JPYT", "JPY Tether"));
         result.add(new CryptoCurrency("WDC", "Worldcoin"));
         result.add(new CryptoCurrency("DAO", "DAO", true));
-        result.add(new CryptoCurrency("ETHC", "EtherClassic (old ticker deprecated ticker symbol)"));
+        result.add(new CryptoCurrency("ETHC", "EtherClassic (deprecated ticker symbol)"));
         return result;
     }
 
