96be70ffebd131b82c85429c5c69d1ddd98a9e44
Mike Rosseel <mike@eon-consult.be>
2016-09-01 08:17:57 +0200

    currency_list: sort on name

--- src/main/java/io/bitsquare/api/CurrencyList.java
+++ src/main/java/io/bitsquare/api/CurrencyList.java
@@ -1,6 +1,7 @@
 package io.bitsquare.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.ArrayList;
@@ -10,7 +11,6 @@ import java.util.List;
  * Created by mike on 31/08/16.
  */
 public class CurrencyList {
-    @JsonProperty
     public List<Currency> currencies = new ArrayList<>();
 
     public CurrencyList() {
@@ -19,6 +19,11 @@ public class CurrencyList {
 
     public void add(String symbol, String name, String type) {
         currencies.add(new Currency(symbol, name, type));
+    }
+
+    @JsonValue
+    public List<Currency> getCurrencies() {
+        return currencies;
     }
 }
 
