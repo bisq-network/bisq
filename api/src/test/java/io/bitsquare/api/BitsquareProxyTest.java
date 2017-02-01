45a672d184563f92e76b59453b1929db64b28e28
Mike Rosseel <mike@eon-consult.be>
2017-02-01 08:06:28 +0100

    empty unit test checkin

---
+++ api/src/test/java/io/bitsquare/api/BitsquareProxyTest.java
@@ -0,0 +1,51 @@
+package io.bitsquare.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mike on 01/02/2017.
+ */
+public class BitsquareProxyTest {
+    @Test
+    public void getCurrencyList() throws Exception {
+    }
+
+    @Test
+    public void getMarketList() throws Exception {
+    }
+
+    @Test
+    public void getAccountList() throws Exception {
+    }
+
+    @Test
+    public void offerCancel() throws Exception {
+    }
+
+    @Test
+    public void getOfferDetail() throws Exception {
+    }
+
+    @Test
+    public void getOfferList() throws Exception {
+    }
+
+    @Test
+    public void offerMake() throws Exception {
+    }
+
+    @Test
+    public void getWalletDetails() throws Exception {
+    }
+
+    @Test
+    public void getWalletTransactions() throws Exception {
+    }
+
+    @Test
+    public void getWalletAddresses() throws Exception {
+    }
+
+}
---
+++ api/src/test/java/io/bitsquare/api/service/ApiResourceTest.java
@@ -0,0 +1,55 @@
+package io.bitsquare.api.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mike on 01/02/2017.
+ */
+public class ApiResourceTest {
+    @Test
+    public void accountList() throws Exception {
+    }
+
+    @Test
+    public void currencyList() throws Exception {
+    }
+
+    @Test
+    public void marketList() throws Exception {
+    }
+
+    @Test
+    public void offerCancel() throws Exception {
+    }
+
+    @Test
+    public void offerDetail() throws Exception {
+    }
+
+    @Test
+    public void offerList() throws Exception {
+    }
+
+    @Test
+    public void offerMake() throws Exception {
+    }
+
+    @Test
+    public void offerTake() throws Exception {
+    }
+
+    @Test
+    public void walletDetail() throws Exception {
+    }
+
+    @Test
+    public void walletAddresses() throws Exception {
+    }
+
+    @Test
+    public void walletTransactionList() throws Exception {
+    }
+
+}
