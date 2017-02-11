71f98a5d28fddea129e51b97589e9efa2188fcc0
Manfred Karrer <mk@nucleo.io>
2017-02-11 18:18:41 -0500

    Update install script

--- doc/install_on_unix.sh
+++ doc/install_on_unix.sh
@@ -23,16 +23,15 @@ apt-get install update-alternatives
 
 echo "Enable unlimited Strength for cryptographic keys"
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
-apt-get install unzip
+#apt-get install unzip
 unzip jce_policy-8.zip
-cp UnlimitedJCEPolicyJDK8/US_export_policy.jar $JAVA_HOME/jre/lib/security/US_export_policy.jar
-cp UnlimitedJCEPolicyJDK8/local_policy.jar $JAVA_HOME/jre/lib/security/local_policy.jar
+sudo cp UnlimitedJCEPolicyJDK8/US_export_policy.jar $JAVA_HOME/jre/lib/security/US_export_policy.jar
+sudo cp UnlimitedJCEPolicyJDK8/local_policy.jar $JAVA_HOME/jre/lib/security/local_policy.jar
 
-chmod 777 $JAVA_HOME/jre/lib/security/US_export_policy.jar
-chmod 777 $JAVA_HOME/jre/lib/security/local_policy.jar
+sudo chmod 777 $JAVA_HOME/jre/lib/security/US_export_policy.jar
+sudo chmod 777 $JAVA_HOME/jre/lib/security/local_policy.jar
 
-rm -r UnlimitedJCEPolicyJDK8
-rm jce_policy-8.zip
+rm -r UnlimitedJCEPolicyJDK8 jce_policy-8.zip
 
 echo "Install bitcoinj"
 cd ~
@@ -48,6 +47,6 @@ mvn clean package -DskipTests -Dmaven.javadoc.skip=true
 
 echo "Add BountyCastle.jar"
 cd ~
-cp .m2/repository/org/bouncycastle/bcprov-jdk15on/1.53/bcprov-jdk15on-1.53.jar $JAVA_HOME/jre/lib/ext/bcprov-jdk15on-1.53.jar
+sudo cp .m2/repository/org/bouncycastle/bcprov-jdk15on/1.53/bcprov-jdk15on-1.53.jar $JAVA_HOME/jre/lib/ext/bcprov-jdk15on-1.53.jar
 
 
