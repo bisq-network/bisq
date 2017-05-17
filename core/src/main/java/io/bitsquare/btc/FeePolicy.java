c5e23feccd4620e633c07459a5125e2e53a5954c
Manfred Karrer <mk@nucleo.io>
2017-05-18 00:29:06 +0200

    Increase default tx fee for non-trade txs to 150_000 sat

--- core/src/main/java/io/bitsquare/btc/FeePolicy.java
+++ core/src/main/java/io/bitsquare/btc/FeePolicy.java
@@ -73,7 +73,7 @@ public class FeePolicy {
     // The BitcoinJ fee calculation use kb so a tx size  < 1kb will still pay the fee for a kb tx.
     // Our payout tx has about 370 bytes so we get a fee/kb value of about 90 satoshi/byte making it high priority
     // Other payout transactions (E.g. arbitrators many collected transactions) will go with 30 satoshi/byte if > 1kb
-    private static Coin NON_TRADE_FEE_PER_KB = Coin.valueOf(60_000); // 0.0003 BTC about 0.3 EUR @ 1000 EUR/BTC 
+    private static Coin NON_TRADE_FEE_PER_KB = Coin.valueOf(150_000); // 0.0003 BTC about 0.3 EUR @ 1000 EUR/BTC 
 
     public static void setNonTradeFeePerKb(Coin nonTradeFeePerKb) {
         NON_TRADE_FEE_PER_KB = nonTradeFeePerKb;
