b82a0610c3700c907e59df6e3a84a6dcb15091e7
Manfred Karrer <mk@nucleo.io>
2017-06-09 17:37:23 +0200

    Run reformat code over project

--- gui/src/main/java/io/bisq/gui/app/BisqApp.java
+++ gui/src/main/java/io/bisq/gui/app/BisqApp.java
@@ -305,7 +305,7 @@ public class BisqApp extends Application {
 
             // Used only for migrating old trade statistic to new data structure
             // injector.getInstance(TradeStatisticsMigrationTool.class);
-                    
+
             if (!Utilities.isCorrectOSArchitecture()) {
                 String osArchitecture = Utilities.getOSArchitecture();
                 // We don't force a shutdown as the osArchitecture might in strange cases return a wrong value.
