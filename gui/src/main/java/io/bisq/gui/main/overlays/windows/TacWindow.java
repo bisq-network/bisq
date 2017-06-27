d4269adda79f940479e8ac37e762ae8fe73c01f9
Manfred Karrer <mk@nucleo.io>
2017-06-27 13:07:41 +0200

    Update tac

--- gui/src/main/java/io/bisq/gui/main/overlays/windows/TacWindow.java
+++ gui/src/main/java/io/bisq/gui/main/overlays/windows/TacWindow.java
@@ -2,21 +2,18 @@ package io.bisq.gui.main.overlays.windows;
 
 import com.google.inject.Inject;
 import io.bisq.common.locale.Res;
-import io.bisq.core.user.Preferences;
 import io.bisq.gui.app.BisqApp;
+import io.bisq.gui.components.HyperlinkWithIcon;
 import io.bisq.gui.main.overlays.Overlay;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
 
 import static io.bisq.gui.util.FormBuilder.addHyperlinkWithIcon;
 
 public class TacWindow extends Overlay<TacWindow> {
-    private static final Logger log = LoggerFactory.getLogger(TacWindow.class);
-    private final Preferences preferences;
 
     @Inject
-    public TacWindow(Preferences preferences) {
-        this.preferences = preferences;
+    public TacWindow() {
         type = Type.Attention;
         width = 900;
     }
@@ -34,12 +31,25 @@ public class TacWindow extends Overlay<TacWindow> {
                 "In no event shall the authors or copyright holders be liable for any claim, damages or other " +
                 "liability, whether in an action of contract, tort or otherwise, " +
                 "arising from, out of or in connection with the software or the use or other dealings in the software.\n\n" +
-                "2. The user is responsible to use the software in compliance with local laws. Don't use bisq if the usage of "
-                + Res.getBaseCurrencyName() + " is not legal in your jurisdiction.\n\n" +
-                "3. " + Res.getBaseCurrencyName() + " market price is delivered by 3rd parties (BitcoinAverage, Poloniex, Coinmarketcap). " +
-                "It is your responsibility to double check the price with other sources.\n\n" +
-                "4. The user confirms that he has read and agreed to the rules regarding the dispute process:\n" +
-                "    - You must finalize trades within the maximum duration specified for each payment method.\n" +
+
+                "2. The user takes full responsibility for any potential losses experienced in relation to the use of Bisq. " +
+                "The user has to take care to remember and secure his wallet password, make regular backups and take care of his operational security.\n" +
+                "In no case he will claim legal responsibility to the authors or copyright holders of the software.\n\n" +
+
+                "3. The user is responsible to use the software in compliance with local laws. Don't use the software if the usage is not legal in your jurisdiction.\n\n" +
+
+                "4. The " + Res.getBaseCurrencyName() + " market price is delivered by 3rd parties (BitcoinAverage, Poloniex, Coinmarketcap). " +
+                "It is your responsibility to verify the price with other sources for correctness.\n\n" +
+
+                "5. Any Fiat payment method carries a potential risk for bank chargeback. By accepting the \"User Agreement\"  the users confirms " +
+                "to be aware of those risks and in no case will claim legal responsibility to the authors or copyright holders of the software.\n\n" +
+
+                "6. Any dispute, controversy or claim arising out of or relating to the use of the software shall be settled by arbitration in " +
+                "accordance with the Bisq arbitration rules as at present in force. The arbitration is conducted online.\n" +
+                "The language to be used in the arbitration proceedings shall be English if not otherwise stated.\n\n" +
+              
+                "7. The user confirms that he has read and agreed to the rules regarding the dispute process:\n" +
+                "    - You must complete trades within the maximum duration specified for each payment method.\n" +
                 "    - You must enter the trade ID in the \"reason for payment\" text field when doing the fiat payment transfer.\n" +
                 "    - If the bank of the fiat sender charges fees the sender (" + Res.getBaseCurrencyCode() + " buyer) has to cover the fees.\n" +
                 "    - You must cooperate with the arbitrator during the arbitration process.\n" +
@@ -58,8 +68,16 @@ public class TacWindow extends Overlay<TacWindow> {
     @Override
     protected void addMessage() {
         super.addMessage();
-        addHyperlinkWithIcon(gridPane, ++rowIndex, Res.get("tacWindow.arbitrationSystem"),
-                "https://bisq.io/arbitration_system.pdf", -6);
+        messageLabel.setStyle("-fx-font-size: 12;");
+        HyperlinkWithIcon hyperlinkWithIcon = addHyperlinkWithIcon(gridPane, ++rowIndex, Res.get("tacWindow.arbitrationSystem"),
+                "https://bisq.io/arbitration_system.pdf");
+        hyperlinkWithIcon.setStyle("-fx-font-size: 12;");
+        GridPane.setMargin(hyperlinkWithIcon, new Insets(-6, 0, -20, -4));
+    }
+
+    @Override
+    protected void setTruncatedMessage() {
+        truncatedMessage = message;
     }
 
     @Override
