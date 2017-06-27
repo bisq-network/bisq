bb4181dd333112fbb829d1804e0796ab02189803
Manfred Karrer <mk@nucleo.io>
2017-06-27 22:53:50 +0200

    Update build script

--- package/linux/32bitBuild.sh
+++ package/linux/32bitBuild.sh
@@ -41,7 +41,7 @@ $JAVA_HOME/bin/javapackager \
 
 # sudo alien -r -c -k gui/deploy/bundles/bisq-$version.deb
 
-cp "gui/deploy/bundles/bisq-$version.deb" "/home/bisq/Desktop/bisq-32bit-$version.deb"
+cp "gui/deploy/bundles/bisq-$version.deb" "/home/bitsquare/Desktop/bisq-32bit-$version.deb"
 mv "gui/deploy/bundles/bisq-$version.deb" "/media/sf_vm_shared_ubuntu14_32bit/bisq-32bit-$version.deb"
 
 # mv "bisq-$version-1.i386.rpm" "/media/sf_vm_shared_ubuntu14_32bit/bisq-32bit-$version.rpm"
