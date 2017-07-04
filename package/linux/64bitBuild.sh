4cdc2e321bfe1aa61d0dd879978e5a4a61f31fd7
Jonathan Cross <jcross@gmail.com>
2017-07-04 21:58:35 +0200

    Removing undefined jdkfixFile var from srcfiles param

--- package/linux/64bitBuild.sh
+++ package/linux/64bitBuild.sh
@@ -25,7 +25,7 @@ $JAVA_HOME/bin/javapackager \
     -title Bisq \
     -vendor Bisq \
     -outdir gui/deploy \
-    -srcfiles $jarFile:$jdkfixFile \
+    -srcfiles $jarFile \
     -srcfiles package/linux/LICENSE \
     -appclass io.bisq.gui.app.BisqAppMain \
     -BjvmOptions=-Xss1280k \
