2d6fa2be6f9d2a063de990b86c788922109a278e
Manfred Karrer <mk@nucleo.io>
2017-07-27 17:12:14 +0200

    Move removeCryptographyRestrictions method to Utilities and add static initializer to SeedNodeMain, ProviderMain and StatisticsMain

--- common/src/main/java/io/bisq/common/util/Utilities.java
+++ common/src/main/java/io/bisq/common/util/Utilities.java
@@ -31,10 +31,15 @@ import org.bitcoinj.core.Utils;
 import javax.annotation.Nullable;
 import javax.crypto.Cipher;
 import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.URI;
 import java.security.NoSuchAlgorithmException;
+import java.security.Permission;
+import java.security.PermissionCollection;
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
@@ -449,5 +454,55 @@ public class Utilities {
     @SuppressWarnings("unchecked")
     public static String collectionToCSV(Collection collection) {
         return collection.stream().map(Object::toString).collect(Collectors.joining(",")).toString();
+    }
+
+    public static void removeCryptographyRestrictions() {
+        if (!isRestrictedCryptography()) {
+            System.out.println("Cryptography restrictions removal not needed");
+            return;
+        }
+        try {
+        /*
+         * Do the following, but with reflection to bypass access checks:
+         *
+         * JceSecurity.isRestricted = false;
+         * JceSecurity.defaultPolicy.perms.clear();
+         * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
+         */
+            final Class<?> jceSecurity = Class.forName("javax.crypto.JceSecurity");
+            final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
+            final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
+
+            final Field isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
+            isRestrictedField.setAccessible(true);
+            final Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+            isRestrictedField.set(null, false);
+
+            final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
+            defaultPolicyField.setAccessible(true);
+            final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
+
+            final Field perms = cryptoPermissions.getDeclaredField("perms");
+            perms.setAccessible(true);
+            ((Map<?, ?>) perms.get(defaultPolicy)).clear();
+
+            final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
+            instance.setAccessible(true);
+            defaultPolicy.add((Permission) instance.get(null));
+
+            System.out.println("Successfully removed cryptography restrictions");
+        } catch (final Exception e) {
+            System.err.println("Failed to remove cryptography restrictions" + e);
+        }
+    }
+
+    public static boolean isRestrictedCryptography() {
+        // This matches Oracle Java 7 and 8, but not Java 9 or OpenJDK.
+        final String name = System.getProperty("java.runtime.name");
+        final String ver = System.getProperty("java.version");
+        return name != null && name.equals("Java(TM) SE Runtime Environment")
+                && ver != null && (ver.startsWith("1.7") || ver.startsWith("1.8"));
     }
 }
