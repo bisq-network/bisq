cee522e61f945b0ba755e15db4933914e539d91f
Manfred Karrer <mk@nucleo.io>
2017-10-15 11:24:27 -0500

    Refactoring: Change base interface of PersistedStoragePayload

--- network/src/main/java/io/bisq/network/p2p/storage/payload/PersistedStoragePayload.java
+++ network/src/main/java/io/bisq/network/p2p/storage/payload/PersistedStoragePayload.java
@@ -1,10 +1,11 @@
 package io.bisq.network.p2p.storage.payload;
 
+import io.bisq.common.proto.network.NetworkPayload;
 import io.bisq.common.proto.persistable.PersistablePayload;
 
 /**
  * Marker interface for payload which gets persisted.
  * Used for TradeStatistics.
  */
-public interface PersistedStoragePayload extends ProtectedStoragePayload, PersistablePayload {
+public interface PersistedStoragePayload extends NetworkPayload, PersistablePayload {
 }
