bb2b96e8ecdc4916e617bddcbf8f0fe3c1153cbf
Manfred Karrer <mk@nucleo.io>
2017-11-16 08:49:11 -0500

    Set provider version and log versions

--- provider/src/main/java/io/bisq/provider/ProviderVersion.java
+++ provider/src/main/java/io/bisq/provider/ProviderVersion.java
@@ -17,12 +17,6 @@
 
 package io.bisq.provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.google.inject.Inject;
-
 public class ProviderVersion {
-
-    public ProviderVersion() {
-    }
+    public static final String VERSION = "0.6.0";
 }
