bb1576e691b6612d6685289033282694fa0246d0
Manfred Karrer <mk@nucleo.io>
2017-11-28 12:42:42 -0500

    Remove unused windows

---
+++ gui/src/main/java/io/bisq/gui/main/overlays/windows/FeeOptionWindow.java
@@ -0,0 +1,185 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.bisq.gui.main.overlays.windows;
+
+import io.bisq.common.app.DevEnv;
+import io.bisq.common.locale.Res;
+import io.bisq.core.filter.PaymentAccountFilter;
+import io.bisq.gui.components.InputTextField;
+import io.bisq.gui.main.overlays.Overlay;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.bisq.gui.util.FormBuilder.addLabelCheckBox;
+import static io.bisq.gui.util.FormBuilder.addLabelInputTextField;
+
+public class FeeOptionWindow extends Overlay<FeeOptionWindow> {
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // Public API
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    public FeeOptionWindow() {
+        type = Type.Attention;
+    }
+
+    public void show() {
+        if (headLine == null)
+            headLine = Res.get("header.headline");
+
+        width = 900;
+        createGridPane();
+        addHeadLine();
+        addSeparator();
+        addContent();
+        applyStyles();
+        display();
+    }
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // Protected
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected void setupKeyHandler(Scene scene) {
+        if (!hideCloseButton) {
+            scene.setOnKeyPressed(e -> {
+                if (e.getCode() == KeyCode.ESCAPE) {
+                    e.consume();
+                    doClose();
+                }
+            });
+        }
+    }
+
+    private void addContent() {
+        InputTextField keyInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("shared.unlock"), 10).second;
+        if (DevEnv.USE_DEV_PRIVILEGE_KEYS)
+            keyInputTextField.setText(DevEnv.DEV_PRIVILEGE_PRIV_KEY);
+
+        InputTextField offerIdsInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.offers")).second;
+        InputTextField nodesInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.onions")).second;
+        InputTextField paymentAccountFilterInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.accounts")).second;
+        GridPane.setHalignment(paymentAccountFilterInputTextField, HPos.RIGHT);
+        InputTextField bannedCurrenciesInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.bannedCurrencies")).second;
+        InputTextField bannedPaymentMethodsInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.bannedPaymentMethods")).second;
+        InputTextField arbitratorsInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.arbitrators")).second;
+        InputTextField seedNodesInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.seedNode")).second;
+        InputTextField priceRelayNodesInputTextField = addLabelInputTextField(gridPane, ++rowIndex, Res.get("filterWindow.priceRelayNode")).second;
+        CheckBox preventPublicBtcNetworkCheckBox = addLabelCheckBox(gridPane, ++rowIndex, Res.get("filterWindow.preventPublicBtcNetwork")).second;
+
+        Button sendButton = new Button(Res.get("filterWindow.add"));
+        sendButton.setOnAction(e -> {
+            List<String> offerIds = new ArrayList<>();
+            List<String> nodes = new ArrayList<>();
+            List<PaymentAccountFilter> paymentAccountFilters = new ArrayList<>();
+            List<String> bannedCurrencies = new ArrayList<>();
+            List<String> bannedPaymentMethods = new ArrayList<>();
+            List<String> arbitrators = new ArrayList<>();
+            List<String> seedNodes = new ArrayList<>();
+            List<String> priceRelayNodes = new ArrayList<>();
+
+            if (!offerIdsInputTextField.getText().isEmpty()) {
+                offerIds = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(offerIdsInputTextField.getText())
+                        .split(",")));
+            }
+
+            if (!nodesInputTextField.getText().isEmpty()) {
+                nodes = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(nodesInputTextField.getText())
+                        .replace(":9999", "")
+                        .replace(".onion", "")
+                        .split(",")));
+            }
+
+            if (!paymentAccountFilterInputTextField.getText().isEmpty()) {
+                paymentAccountFilters = new ArrayList<>(Arrays.asList(paymentAccountFilterInputTextField.getText()
+                        .replace(", ", ",")
+                        .split(","))
+                        .stream().map(item -> {
+                            String[] list = item.split("\\|");
+                            if (list.length == 3)
+                                return new PaymentAccountFilter(list[0], list[1], list[2]);
+                            else
+                                return new PaymentAccountFilter("", "", "");
+                        })
+                        .collect(Collectors.toList()));
+            }
+
+            if (!bannedCurrenciesInputTextField.getText().isEmpty()) {
+                bannedCurrencies = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(bannedCurrenciesInputTextField.getText())
+                        .split(",")));
+            }
+
+            if (!bannedPaymentMethodsInputTextField.getText().isEmpty()) {
+                bannedPaymentMethods = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(bannedPaymentMethodsInputTextField.getText())
+                        .split(",")));
+            }
+
+            if (!arbitratorsInputTextField.getText().isEmpty()) {
+                arbitrators = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(arbitratorsInputTextField.getText())
+                        .replace(":9999", "")
+                        .replace(".onion", "")
+                        .split(",")));
+            }
+
+            if (!seedNodesInputTextField.getText().isEmpty()) {
+                seedNodes = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(seedNodesInputTextField.getText())
+                        .replace(":9999", "")
+                        .replace(".onion", "")
+                        .split(",")));
+            }
+
+            if (!priceRelayNodesInputTextField.getText().isEmpty()) {
+                priceRelayNodes = new ArrayList<>(Arrays.asList(StringUtils.deleteWhitespace(priceRelayNodesInputTextField.getText())
+                        .replace(":9999", "")
+                        .replace(".onion", "")
+                        .split(",")));
+            }
+        });
+
+
+        closeButton = new Button(Res.get("shared.close"));
+        closeButton.setOnAction(e -> {
+            hide();
+            closeHandlerOptional.ifPresent(Runnable::run);
+        });
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        GridPane.setRowIndex(hBox, ++rowIndex);
+        GridPane.setColumnIndex(hBox, 1);
+        gridPane.getChildren().add(hBox);
+        GridPane.setMargin(hBox, new Insets(10, 0, 0, 0));
+    }
+}
