bc6aa2eb126bca256fd37ad802b56911f3a608ac
Ed Zynda III <ezynda3@users.noreply.github.com>
2018-01-02 17:05:58 +0200

    Add WILD Token to currency list

--- common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
+++ common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
@@ -149,6 +149,7 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("XZC", "Zcoin"));
         result.add(new CryptoCurrency("ZEC", "Zcash"));
         result.add(new CryptoCurrency("ZEN", "ZenCash"));
+        result.add(new CryptoCurrency("WILD", "WILD Token", true));
         result.sort(TradeCurrency::compareTo);
 
         // Util for printing all altcoins for adding to FAQ page
