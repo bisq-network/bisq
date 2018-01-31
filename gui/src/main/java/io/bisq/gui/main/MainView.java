a8b0449fd15766f2d3183796d8c556706cccc3f0
Manfred Karrer <mk@nucleo.io>
2018-01-31 10:33:23 -0500

    Use sceneProperty listener instead of UserThread.runAfter at PR #1302

--- gui/src/main/java/io/bisq/gui/main/MainView.java
+++ gui/src/main/java/io/bisq/gui/main/MainView.java
@@ -151,37 +151,38 @@ public class MainView extends InitializableView<StackPane, MainViewModel> {
             daoButton.setManaged(false);
         }
 
-        UserThread.runAfter(() -> {
-            root.getScene().addEventHandler(KeyEvent.KEY_RELEASED, keyEvent -> {
-                // TODO can be removed once DAO is released
-                if (Utilities.isAltOrCtrlPressed(KeyCode.D, keyEvent)) {
-                    if (BisqEnvironment.getBaseCurrencyNetwork().isBitcoin()) {
-                        daoButton.setVisible(true);
-                        daoButton.setManaged(true);
+        root.sceneProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                newValue.addEventHandler(KeyEvent.KEY_RELEASED, keyEvent -> {
+                    // TODO can be removed once DAO is released
+                    if (Utilities.isAltOrCtrlPressed(KeyCode.D, keyEvent)) {
+                        if (BisqEnvironment.getBaseCurrencyNetwork().isBitcoin()) {
+                            daoButton.setVisible(true);
+                            daoButton.setManaged(true);
+                        }
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT1, keyEvent)) {
+                        marketButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT2, keyEvent)) {
+                        buyButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT3, keyEvent)) {
+                        sellButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT4, keyEvent)) {
+                        portfolioButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT5, keyEvent)) {
+                        fundsButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT6, keyEvent)) {
+                        disputesButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT7, keyEvent)) {
+                        settingsButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT8, keyEvent)) {
+                        accountButton.fire();
+                    } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT9, keyEvent)) {
+                        if (daoButton.isVisible())
+                            daoButton.fire();
                     }
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT1, keyEvent)) {
-                    marketButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT2, keyEvent)) {
-                    buyButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT3, keyEvent)) {
-                    sellButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT4, keyEvent)) {
-                    portfolioButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT5, keyEvent)) {
-                    fundsButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT6, keyEvent)) {
-                    disputesButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT7, keyEvent)) {
-                    settingsButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT8, keyEvent)) {
-                    accountButton.fire();
-                } else if (Utilities.isAltOrCtrlPressed(KeyCode.DIGIT9, keyEvent)) {
-                    if (daoButton.isVisible()) {
-                        daoButton.fire();
-                    }
-                }
-            });
-        }, 1);
+                });
+            }
+        });
 
         HBox leftNavPane = new HBox(marketButton, buyButton, sellButton, portfolioButtonHolder, fundsButton, disputesButtonHolder) {{
             setLeftAnchor(this, 10d);
