00f6f8ea4b134b470ec97347f18bbeeeb7430250
Hein Bloed <hein_bloed_git@protonmail.com>
2018-02-08 18:26:45 +0100

    added ODN to createAllSortedCryptoCurrenciesList

--- common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
+++ common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
@@ -131,6 +131,7 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("NMC", "Namecoin"));
         result.add(new CryptoCurrency("NBT", "NuBits"));
         result.add(new CryptoCurrency("NXT", "Nxt"));
+        result.add(new CryptoCurrency("ODN", "Obsidian"));
         result.add(new CryptoCurrency("888", "OctoCoin"));
         result.add(new CryptoCurrency("PART", "Particl"));
         result.add(new CryptoCurrency("PASC", "Pascal Coin", true));
