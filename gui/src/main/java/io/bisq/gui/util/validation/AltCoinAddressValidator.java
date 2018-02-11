9204c3b8c212fcac4e803d11d3503c7b4fc975dc
bitdaric <35429817+bitdaric@users.noreply.github.com>
2018-02-11 10:47:34 +0330

    add BitDaric

--- gui/src/main/java/io/bisq/gui/util/validation/AltCoinAddressValidator.java
+++ gui/src/main/java/io/bisq/gui/util/validation/AltCoinAddressValidator.java
@@ -438,6 +438,11 @@ public final class AltCoinAddressValidator extends InputValidator {
                         return regexTestFailed;
                     else
                         return new ValidationResult(true);
+                case "DARX":
+                    if (!input.matches("^[R][a-km-zA-HJ-NP-Z1-9]{25,34}$"))
+                        return regexTestFailed;
+                    else
+                        return new ValidationResult(true);
 
                     // Add new coins at the end...
                 default:
