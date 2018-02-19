a02f586a7ef49928392d3416b1e5917cac057ced
Christoph Atteneder <christoph.atteneder@gmail.com>
2018-02-19 10:08:53 +0100

    Add component tests

--- gui/src/test/java/io/bisq/gui/components/ColoredDecimalPlacesWithZerosTextTest.java
+++ gui/src/test/java/io/bisq/gui/components/ColoredDecimalPlacesWithZerosTextTest.java
@@ -1,5 +1,37 @@
 package io.bisq.gui.components;
 
+import javafx.scene.text.Text;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ColoredDecimalPlacesWithZerosTextTest {
+
+    @Test
+    public void testOnlyZeroDecimals() {
+        ColoredDecimalPlacesWithZerosText text = new ColoredDecimalPlacesWithZerosText("1.0000");
+        Text beforeZeros = (Text) text.getChildren().get(0);
+        Text zeroDecimals = (Text) text.getChildren().get(1);
+        assertEquals("1.0", beforeZeros.getText());
+        assertEquals("000", zeroDecimals.getText());
+    }
+
+    @Test
+    public void testOneZeroDecimal() {
+        ColoredDecimalPlacesWithZerosText text = new ColoredDecimalPlacesWithZerosText("1.2570");
+        Text beforeZeros = (Text) text.getChildren().get(0);
+        Text zeroDecimals = (Text) text.getChildren().get(1);
+        assertEquals("1.257", beforeZeros.getText());
+        assertEquals("0", zeroDecimals.getText());
+    }
+
+    @Test
+    public void testMultipleZeroDecimal() {
+        ColoredDecimalPlacesWithZerosText text = new ColoredDecimalPlacesWithZerosText("1.2000");
+        Text beforeZeros = (Text) text.getChildren().get(0);
+        Text zeroDecimals = (Text) text.getChildren().get(1);
+        assertEquals("1.2", beforeZeros.getText());
+        assertEquals("000", zeroDecimals.getText());
+    }
 
 }
