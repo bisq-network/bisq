fd18cca1618459787719b67de297a0732781dc5b
Manfred Karrer <mk@nucleo.io>
2018-03-02 09:50:33 -0500

    Add missing setProperty call for regTestBsqGenesisTxId

--- monitor/src/main/java/io/bisq/monitor/MonitorEnvironment.java
+++ monitor/src/main/java/io/bisq/monitor/MonitorEnvironment.java
@@ -97,6 +97,7 @@ public class MonitorEnvironment extends BisqEnvironment {
                 setProperty(DaoOptionKeys.RPC_BLOCK_NOTIFICATION_PORT, rpcBlockNotificationPort);
                 setProperty(DaoOptionKeys.DUMP_BLOCKCHAIN_DATA, dumpBlockchainData);
                 setProperty(DaoOptionKeys.FULL_DAO_NODE, fullDaoNode);
+                setProperty(DaoOptionKeys.REG_TEST_GENESIS_TX_ID, regTestBsqGenesisTxId);
 
                 setProperty(BtcOptionKeys.BTC_NODES, btcNodes);
                 setProperty(BtcOptionKeys.USE_TOR_FOR_BTC, useTorForBtc);
