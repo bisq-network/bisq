82f41cbfcd34bbcdd94fc72ee62b78d38d0db131
swordfish5975 <cryptofolio.info@gmail.com>
2018-03-04 15:56:44 +1000

    Strayacoin

--- common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
+++ common/src/main/java/io/bisq/common/locale/CurrencyUtil.java
@@ -169,6 +169,8 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("SOS", "SOS Coin", true));
         result.add(new CryptoCurrency("ACH", "AchieveCoin"));
         result.add(new CryptoCurrency("VDN", "vDinar"));
+		
+		result.add(new CryptoCurrency("NAH", "Strayacoin"));
 
         result.sort(TradeCurrency::compareTo);
 
