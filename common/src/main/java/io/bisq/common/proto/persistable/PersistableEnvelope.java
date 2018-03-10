2c0392bcebe6cb638a782dd9361fd564562cff5f
Chris Beams <chris@beams.io>
2018-03-10 17:29:00 +0100

    Move common module to new bisq-common repository

--- common/src/main/java/io/bisq/common/proto/persistable/PersistableEnvelope.java
+++
@@ -1,9 +0,0 @@
-package io.bisq.common.proto.persistable;
-
-import io.bisq.common.Envelope;
-
-/**
- * Interface for the outside envelope object persisted to disc.
- */
-public interface PersistableEnvelope extends Envelope {
-}
--- common/src/main/java/io/bisq/common/storage/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.bisq.common.storage;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String path) {
-        super("Resource not found: path = " + path);
-    }
-}
