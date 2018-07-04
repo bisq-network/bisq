66a7a19aa9b1e54b4afd2e6bcac2023ed1571851
Christoph Atteneder <christoph.atteneder@gmail.com>
2018-07-04 13:41:05 +0200

    Use version variable

--- package/osx/create_app.sh
+++ package/osx/create_app.sh
@@ -10,7 +10,7 @@ version="0.7.0"
 
 ./gradlew --include-build ../common --include-build ../assets --include-build ../p2p --include-build ../core build -x test shadowJar
 
-EXE_JAR=build/libs/bisq-desktop-0.7.0-all.jar
+EXE_JAR=build/libs/bisq-desktop-$version-all.jar
 
 echo SHA 256 before stripping jar file:
 shasum -a256 $EXE_JAR | awk '{print $1}'
