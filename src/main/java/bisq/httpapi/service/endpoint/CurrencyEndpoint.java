e13e90464391b944bba58703d3b13986f01ca297
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-09-13 12:11:02 +0200

    Surround all calls in CurrencyEndpoint with UserThread

--- src/main/java/bisq/httpapi/service/endpoint/CurrencyEndpoint.java
+++ src/main/java/bisq/httpapi/service/endpoint/CurrencyEndpoint.java
@@ -4,6 +4,8 @@ import bisq.httpapi.facade.PriceFeedFacade;
 import bisq.httpapi.model.CurrencyList;
 import bisq.httpapi.model.PriceFeed;
 
+import bisq.common.UserThread;
+
 import javax.inject.Inject;
 
 
@@ -15,6 +17,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 
 @Api(value = "currencies", authorizations = @Authorization(value = "accessToken"))
@@ -28,21 +32,33 @@ public class CurrencyEndpoint {
         this.priceFeedFacade = priceFeedFacade;
     }
 
-    @ApiOperation("List available currencies")
+    @ApiOperation(value = "List available currencies", response = CurrencyList.class)
     @GET
-    public CurrencyList getCurrencyList() {
-        return MarketEndpoint.getCurrencyList();
+    public void getCurrencyList(@Suspended final AsyncResponse asyncResponse) {
+        UserThread.execute(() -> {
+            try {
+                asyncResponse.resume(MarketEndpoint.getCurrencyList());
+            } catch (Throwable e) {
+                asyncResponse.resume(e);
+            }
+        });
     }
 
-    @ApiOperation(value = "Get market prices", notes = "If currencyCodes is not provided then currencies from preferences are used.")
+    @ApiOperation(value = "Get market prices", notes = "If currencyCodes is not provided then currencies from preferences are used.", response = PriceFeed.class)
     @GET
     @Path("/prices")
-    public PriceFeed getPriceFeed(@QueryParam("currencyCodes") String currencyCodes) {
-        final String[] codes;
-        if (null == currencyCodes || 0 == currencyCodes.length())
-            codes = new String[0];
-        else
-            codes = currencyCodes.split("\\s*,\\s*");
-        return priceFeedFacade.getPriceFeed(codes);
+    public void getPriceFeed(@Suspended final AsyncResponse asyncResponse, @QueryParam("currencyCodes") String currencyCodes) {
+        UserThread.execute(() -> {
+            try {
+                final String[] codes;
+                if (null == currencyCodes || 0 == currencyCodes.length())
+                    codes = new String[0];
+                else
+                    codes = currencyCodes.split("\\s*,\\s*");
+                asyncResponse.resume(priceFeedFacade.getPriceFeed(codes));
+            } catch (Throwable e) {
+                asyncResponse.resume(e);
+            }
+        });
     }
 }
