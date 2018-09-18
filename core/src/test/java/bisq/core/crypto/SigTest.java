db179914ada88a8e5378e28351ab77d510caa8c3
Christoph Atteneder <christoph.atteneder@gmail.com>
2018-09-18 20:50:01 +0200

    Remove not thrown exceptions

--- core/src/test/java/bisq/core/crypto/SigTest.java
+++ core/src/test/java/bisq/core/crypto/SigTest.java
@@ -23,13 +23,6 @@ import bisq.common.crypto.KeyStorage;
 import bisq.common.crypto.Sig;
 import bisq.common.storage.FileUtil;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Security;
-import java.security.cert.CertificateException;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -50,7 +43,7 @@ public class SigTest {
     private File dir;
 
     @Before
-    public void setup() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, CryptoException {
+    public void setup() throws IOException {
 
         dir = File.createTempFile("temp_tests", "");
         //noinspection ResultOfMethodCallIgnored
