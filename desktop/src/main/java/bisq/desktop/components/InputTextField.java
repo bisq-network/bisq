ef3e4ba9050d29c9daf8a9d487014773f8654842
Manfred Karrer <mk@nucleo.io>
2018-10-30 13:14:19 -0500

    Fix issue with not applied error msg in validator

--- desktop/src/main/java/bisq/desktop/components/InputTextField.java
+++ desktop/src/main/java/bisq/desktop/components/InputTextField.java
@@ -78,8 +78,12 @@ public class InputTextField extends JFXTextField {
         });
 
         focusedProperty().addListener((o, oldValue, newValue) -> {
-            if (oldValue && !newValue && validator != null)
-                validationResult.set(validator.validate(getText()));
+            if (oldValue && !newValue && validator != null) {
+                // Need to reset first otherwise jfxValidationWrapper is not reflecting new error message
+                this.validationResult.set(new InputValidator.ValidationResult(true));
+
+                this.validationResult.set(validator.validate(getText()));
+            }
         });
     }
 
