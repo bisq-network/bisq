a9f459ffc13d97d06ae1c7aeff7271e53ba7ec6e
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-11-01 18:43:19 -0400

    List Help The Homeless Coin (HTH)

--- assets/src/test/java/bisq/asset/coins/HelpTheHomelessTest.java
+++ assets/src/test/java/bisq/asset/coins/HelpTheHomelessTest.java
@@ -35,9 +35,9 @@ public class HelpTheHomelessTest extends AbstractAssetTest {
 
     @Test
     public void testInvalidAddresses() {
-        assertInvalidAddress("HX83aKuN7KfqJintZk4qYLYKMn2JiiABhL");
-        assertInvalidAddress("HX87aKuN3KfqJintLk4qKLYYZn2JiiABhM");
-        assertInvalidAddress("XH83aKuN7KfqJintLk4qYLYKMn2JiiAZhB");
+        assertInvalidAddress("HN83aKuX7KfqJintLk4QYLYKMn2jiiABhZ");
+        assertInvalidAddress("HN83aKuX7KfqJintLk4QYLYKMn2jiiABhZ");
+        assertInvalidAddress("HN83aKuX7KfqJintLk4QYLYKMn2jiiABhZ");
         assertInvalidAddress("HX83aKqN7KfqJintLk4uYLYKMn2JihABiZ");
         assertInvalidAddress("HX83aKuN7KfqJintLk4qYLYKMn2JiiZBhA");
     }
