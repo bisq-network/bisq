7612fe3571cea1b5016d843b0c18561154600310
Christoph Atteneder <christoph.atteneder@gmail.com>
2018-11-20 16:22:04 +0100

    Adapt WitdrawalView to use full window width

--- desktop/src/main/java/bisq/desktop/components/TextFieldWithNoPaddingLabelFloat.java
+++ desktop/src/main/java/bisq/desktop/components/TextFieldWithNoPaddingLabelFloat.java
@@ -6,6 +6,14 @@ import javafx.scene.control.Skin;
 
 public class TextFieldWithNoPaddingLabelFloat extends JFXTextField {
 
+    public TextFieldWithNoPaddingLabelFloat(String value) {
+        super(value);
+    }
+
+    public TextFieldWithNoPaddingLabelFloat() {
+        super();
+    }
+
     @Override
     protected Skin<?> createDefaultSkin() {
         return new JFXTextFieldSkinBisqStyle<>(this, 0);
