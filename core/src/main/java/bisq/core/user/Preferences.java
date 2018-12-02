fe458bce129b1ad7c1013ddd8c4ede0a10ec0fe9
Manfred Karrer <mk@nucleo.io>
2018-12-02 13:36:47 +0100

    Don't use tor for BitcoinJ if testnet

--- core/src/main/java/bisq/core/user/Preferences.java
+++ core/src/main/java/bisq/core/user/Preferences.java
@@ -692,9 +692,9 @@ public final class Preferences implements PersistedDataHost, BridgeAddressProvid
     }
 
     public boolean getUseTorForBitcoinJ() {
-        // We override the useTorForBitcoinJ and set to false if we have bitcoinNodes set
-        // This check includes localhost, so we also override useTorForBitcoinJ
-        if (BisqEnvironment.getBaseCurrencyNetwork().isRegtest()
+        // We override the useTorForBitcoinJ and set it to false if we detected a localhost node or if we are not on mainnet.
+        // At testnet there are very few Bitcoin tor nodes and we don't provide tor nodes.
+        if (!BisqEnvironment.getBaseCurrencyNetwork().isMainnet()
                 || bisqEnvironment.isBitcoinLocalhostNodeRunning())
             return false;
         else
