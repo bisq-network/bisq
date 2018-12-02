b8c85983e33474becebe1746672bce1f94b9071b
Manfred Karrer <mk@nucleo.io>
2018-12-03 00:06:13 +0100

    Remove devMode check

--- desktop/src/main/java/bisq/desktop/main/offer/offerbook/OfferBookView.java
+++ desktop/src/main/java/bisq/desktop/main/offer/offerbook/OfferBookView.java
@@ -56,7 +56,6 @@ import bisq.core.util.BSFormatter;
 
 import bisq.network.p2p.NodeAddress;
 
-import bisq.common.app.DevEnv;
 import bisq.common.util.Tuple3;
 
 import org.bitcoinj.core.Coin;
@@ -396,7 +395,7 @@ public class OfferBookView extends ActivatableViewAndModel<GridPane, OfferBookVi
                     Res.get("popup.warning.noTradingAccountSetup.msg"),
                     FiatAccountsView.class,
                     "navigation.account");
-        } else if (!model.hasPaymentAccountForCurrency() && !DevEnv.isDevMode()) {
+        } else if (!model.hasPaymentAccountForCurrency()) {
             new Popup<>().headLine(Res.get("offerbook.warning.noTradingAccountForCurrency.headline"))
                     .instruction(Res.get("offerbook.warning.noTradingAccountForCurrency.msg"))
                     .actionButtonText(Res.get("offerbook.yesCreateOffer"))
