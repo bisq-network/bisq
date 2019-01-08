845888556db7f536a344da7cb1f3c8856a0b363b
Manfred Karrer <mk@nucleo.io>
2019-01-08 03:21:53 +0100

    Update validation regex and add comment

--- assets/src/main/java/bisq/asset/coins/Beam.java
+++ assets/src/main/java/bisq/asset/coins/Beam.java
@@ -20,8 +20,18 @@ package bisq.asset.coins;
 import bisq.asset.Coin;
 import bisq.asset.RegexAddressValidator;
 
+/**
+ * Here is info from a Beam developer regarding validation.
+ *
+ * Well, unfortunately the range length is quite large. The BbsChannel is 64 bit = 8 bytes, the pubkey is 32 bytes.
+ * So, the length may be up to 80 chars. The minimum length "theoretically" can also drop to a small length, if the
+ * channel==0, and the pubkey starts with many zeroes  (very unlikely, but possible). So, besides being up to 80 chars
+ * lower-case hex there's not much can be tested. A more robust test would also check if the pubkey is indeed valid,
+ * but it's a more complex test, requiring cryptographic code.
+ *
+ */
 public class Beam extends Coin {
     public Beam() {
-        super("Beam", "BEAM", new RegexAddressValidator("^([0-9a-f]{64,65})$"));
+        super("Beam", "BEAM", new RegexAddressValidator("^([0-9a-f]{1,80})$"));
     }
 }
