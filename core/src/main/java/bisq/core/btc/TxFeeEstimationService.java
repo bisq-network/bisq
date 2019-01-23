250c7c1e19afdc98cd1fddfff5acef727c2e56e5
Manfred Karrer <mk@nucleo.io>
2019-01-23 12:32:06 +0100

    Improve comment

--- core/src/main/java/bisq/core/btc/TxFeeEstimationService.java
+++ core/src/main/java/bisq/core/btc/TxFeeEstimationService.java
@@ -127,17 +127,18 @@ public class TxFeeEstimationService {
     }
 
     // We start with the initialEstimatedTxSize for a tx with 1 input (260) bytes and get from BitcoinJ a tx back which
-    // contains the required inputs to fund that tx (out outputs + miner fee). The miner fee is that case is based on
+    // contains the required inputs to fund that tx (outputs + miner fee). The miner fee in that case is based on
     // the assumption that we only need 1 input. Once we receive back the real tx size from the tx BitcoinJ has created
-    // with the required inputs we compare that if it is in the tolerance of 20% from our assumed tx size. If we inside
-    // that tolerance we use that tx size for our fee estimation. If not (if there has been more then 1 inputs) we
-    // apply the new fee based on the reported tx size and request again from BitcoinJ to full that tx with the inputs
+    // with the required inputs we compare if the size is not more then 20% different to our assumed tx size. If we are inside
+    // that tolerance we use that tx size for our fee estimation, if not (if there has been more then 1 inputs) we
+    // apply the new fee based on the reported tx size and request again from BitcoinJ to fill that tx with the inputs
     // to be sufficiently funded. The algorithm how BitcoinJ selects utxos is complex and contains several aspects
     // (minimize fee, don't create too many tiny utxos,...). We treat that algorithm as an unknown and it is not
     // guaranteed that there are more inputs required if we increase the fee (it could be that there is a better
-    // selection of inputs chosen if we have increased the fee and therefor less inputs). As the increased fee might
+    // selection of inputs chosen if we have increased the fee and therefore less inputs and smaller tx size). As the increased fee might
     // change the number of inputs we need to repeat that process until we are inside of a certain tolerance. To avoid
-    // potential endless loops we add a counter. Worst case would be that the last size we got reported is > 20% off to
+    // potential endless loops we add a counter (we use 10, usually it takes just very few iterations).
+    // Worst case would be that the last size we got reported is > 20% off to
     // the real tx size but as fee estimation is anyway a educated guess in the best case we don't worry too much.
     // If we have underpaid the tx might take longer to get confirmed.
     @VisibleForTesting
