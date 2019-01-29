a52fff1acc55b1e4a71272c5e421170c0b38620d
Manfred Karrer <mk@nucleo.io>
2019-01-29 15:10:25 +0100

    Increase snapshot grid size to 20 blocks

--- core/src/main/java/bisq/core/dao/state/DaoStateSnapshotService.java
+++ core/src/main/java/bisq/core/dao/state/DaoStateSnapshotService.java
@@ -38,7 +38,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class DaoStateSnapshotService implements DaoStateListener {
-    private static final int SNAPSHOT_GRID = 10;
+    private static final int SNAPSHOT_GRID = 20;
 
     private final DaoStateService daoStateService;
     private final GenesisTxInfo genesisTxInfo;
