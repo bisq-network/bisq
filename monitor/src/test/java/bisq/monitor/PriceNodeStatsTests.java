84441b31082f335679b256573a24b6cc330a0bd7
Florian Reimair <office@florianreimair.at>
2019-02-06 11:40:19 +0100

    Fee history available

--- monitor/src/test/java/bisq/monitor/PriceNodeStatsTests.java
+++ monitor/src/test/java/bisq/monitor/PriceNodeStatsTests.java
@@ -25,12 +25,14 @@ import org.berndpruenster.netlayer.tor.TorCtlException;
 
 import java.io.File;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -41,10 +43,59 @@ public class PriceNodeStatsTests {
 
     private final static File torWorkingDirectory = new File("monitor/" + PriceNodeStatsTests.class.getSimpleName());
 
+    /**
+     * A dummy Reporter for development purposes.
+     */
+    private class DummyReporter extends Reporter {
+
+        private Map<String, String> results;
+
+        @Override
+        public void report(long value) {
+            Assert.fail();
+        }
+
+        public Map<String, String> results() {
+            return results;
+        }
+
+        @Override
+        public void report(Map<String, String> values) {
+            results = values;
+        }
+
+        @Override
+        public void report(Map<String, String> values, String prefix) {
+            report(values);
+        }
+
+        @Override
+        public void report(long value, String prefix) {
+            report(value);
+        }
+    }
+
     @BeforeAll
     public static void setup() throws TorCtlException {
         // simulate the tor instance available to all metrics
         Tor.setDefault(new NativeTor(torWorkingDirectory));
+    }
+
+    @Test
+    public void connect() {
+        DummyReporter reporter = new DummyReporter();
+        Metric DUT = new PriceNodeStats(reporter);
+
+
+        Properties configuration = new Properties();
+        configuration.put("PriceNodeStats.run.hosts", "http://5bmpx76qllutpcyp.onion");
+
+        DUT.configure(configuration);
+
+        DUT.execute();
+
+        Assert.assertNotNull(reporter.results());
+        Assert.assertTrue(reporter.results.size() > 0);
     }
 
     @AfterAll
