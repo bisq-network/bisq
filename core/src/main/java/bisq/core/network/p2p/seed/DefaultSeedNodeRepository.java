df540d7ce857927a09344bb2c37ab3f3540809a1
Florian Reimair <office@florianreimair.at>
2019-02-19 15:50:25 +0100

    Honor manually provided seed nodes

--- core/src/main/java/bisq/core/network/p2p/seed/DefaultSeedNodeRepository.java
+++ core/src/main/java/bisq/core/network/p2p/seed/DefaultSeedNodeRepository.java
@@ -19,10 +19,12 @@ package bisq.core.network.p2p.seed;
 
 import bisq.core.app.BisqEnvironment;
 
+import bisq.network.NetworkOptionKeys;
 import bisq.network.p2p.NodeAddress;
 import bisq.network.p2p.seed.SeedNodeRepository;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import java.net.URL;
 
@@ -30,25 +32,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
+
 public class DefaultSeedNodeRepository implements SeedNodeRepository {
     private static final Pattern pattern = Pattern.compile("^([a-z0-9]+\\.onion:\\d+)");
     private static final String ENDING = ".seednodes";
     private static final Collection<NodeAddress> cache = new HashSet<>();
-    private static Collection<NodeAddress> excludes = new HashSet<>();
     private final BisqEnvironment bisqEnvironment;
+    private final String seedNodes;
 
     @Inject
-    public DefaultSeedNodeRepository(BisqEnvironment environment) {
+    public DefaultSeedNodeRepository(BisqEnvironment environment,
+                                     @Nullable @Named(NetworkOptionKeys.SEED_NODES_KEY) String seedNodes) {
         bisqEnvironment = environment;
+        this.seedNodes = seedNodes;
     }
 
     private void reload() {
+
+        // see if there are any seed nodes configured manually
+        if(seedNodes != null && !seedNodes.isEmpty()) {
+            cache.clear();
+            Arrays.stream(seedNodes.split(",")).forEach(s -> cache.add(new NodeAddress(s)));
+
+            return;
+        }
+
+        // else, we fetch the seed nodes from our resources
         try {
             // read appropriate file
             final URL file = DefaultSeedNodeRepository.class.getClassLoader().getResource(BisqEnvironment.getBaseCurrencyNetwork().getNetwork().toLowerCase() + ENDING);
@@ -65,8 +82,7 @@ public class DefaultSeedNodeRepository implements SeedNodeRepository {
             });
 
             // filter
-            if(bisqEnvironment.getBannedSeedNodes() != null)
-                cache.removeAll(bisqEnvironment.getBannedSeedNodes().stream().map(s -> new NodeAddress(s)).collect(Collectors.toSet()));
+            cache.removeAll(bisqEnvironment.getBannedSeedNodes().stream().map(s -> new NodeAddress(s)).collect(Collectors.toSet()));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
