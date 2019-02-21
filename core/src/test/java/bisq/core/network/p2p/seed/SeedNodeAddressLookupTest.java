a188aa6dfa26b12698265703e0a81ba48be07523
Manfred Karrer <mk@nucleo.io>
2019-02-20 20:49:02 -0500

    Use mainnet notwork id for test as testnet does not has any seed node defined

--- core/src/test/java/bisq/core/network/p2p/seed/SeedNodeAddressLookupTest.java
+++ core/src/test/java/bisq/core/network/p2p/seed/SeedNodeAddressLookupTest.java
@@ -21,14 +21,9 @@ import bisq.core.app.BisqEnvironment;
 
 import bisq.network.p2p.NodeAddress;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-import java.security.Security;
-
 import java.util.Collections;
 import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -41,7 +36,7 @@ public class SeedNodeAddressLookupTest {
     @Test
     public void testResolveNodeAddressesWhenLocalAddressSpecified() {
         SeedNodeAddressLookup lookup = new SeedNodeAddressLookup(
-                mock(BisqEnvironment.class), false, 1, "192.168.0.1:1234",
+                mock(BisqEnvironment.class), false, 0, "192.168.0.1:1234",
                 "192.168.0.1:1234, 192.168.0.2:9897");
 
         Set<NodeAddress> actual = lookup.resolveNodeAddresses();
@@ -52,7 +47,7 @@ public class SeedNodeAddressLookupTest {
     @Test
     public void testResolveNodeAddressesWhenSeedNodesAreNull() {
         SeedNodeAddressLookup lookup = new SeedNodeAddressLookup(
-                mock(BisqEnvironment.class), false, 1, "192.168.0.1:1234", null);
+                mock(BisqEnvironment.class), false, 0, "192.168.0.1:1234", null);
 
         Set<NodeAddress> actual = lookup.resolveNodeAddresses();
         assertFalse(actual.isEmpty());
