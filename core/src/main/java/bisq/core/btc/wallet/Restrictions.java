e04d042e4e245958ecaa139282dc041a0037f010
Manfred Karrer <mk@nucleo.io>
2019-03-04 23:48:41 -0500

    Update security deposit values

--- core/src/main/java/bisq/core/btc/wallet/Restrictions.java
+++ core/src/main/java/bisq/core/btc/wallet/Restrictions.java
@@ -46,27 +46,27 @@ public class Restrictions {
 
     public static Coin getMinTradeAmount() {
         if (MIN_TRADE_AMOUNT == null)
-            MIN_TRADE_AMOUNT = Coin.valueOf(10_000); // 2 USD @ 20000 USD/BTC
+            MIN_TRADE_AMOUNT = Coin.valueOf(10_000); // 0,4 USD @ 4000 USD/BTC
         return MIN_TRADE_AMOUNT;
     }
 
     public static double getDefaultBuyerSecurityDepositAsPercent() {
-        return 0.01; // 1% of trade amount
+        return 0.02; // 2% of trade amount. For a 1 BTC trade it is about 80 USD @ 4000 USD/BTC.
     }
 
     public static double getMinBuyerSecurityDepositAsPercent() {
-        return 0.0005; // 0.05% of trade amount
+        return 0.0005; // 0.05% of trade amount. For a 1 BTC trade it is about 2 USD @ 4000 USD/BTC
     }
 
     public static double getMaxBuyerSecurityDepositAsPercent() {
-        return 0.1; // 10% of trade amount
+        return 0.1; // 10% of trade amount. For a 1 BTC trade it is about 400 USD @ 4000 USD/BTC
     }
 
     // We use MIN_BUYER_SECURITY_DEPOSIT as well as lower bound in case of small trade amounts.
     // So 0.0005 BTC is the min. buyer security deposit even with amount of 0.0001 BTC and 0.05% percentage value.
     public static Coin getMinBuyerSecurityDepositAsCoin() {
         if (MIN_BUYER_SECURITY_DEPOSIT == null)
-            MIN_BUYER_SECURITY_DEPOSIT = Coin.parseCoin("0.0005"); // 0.0005 BTC of a 1 BTC trade (0.05%)
+            MIN_BUYER_SECURITY_DEPOSIT = Coin.parseCoin("0.001"); // 0.001 BTC about 4 USD @ 4000 USD/BTC
         return MIN_BUYER_SECURITY_DEPOSIT;
     }
 
@@ -77,7 +77,7 @@ public class Restrictions {
 
     public static Coin getMinSellerSecurityDepositAsCoin() {
         if (SELLER_SECURITY_DEPOSIT == null)
-            SELLER_SECURITY_DEPOSIT = Coin.parseCoin("0.0005"); // 0.0005 BTC of a 1 BTC trade (0.05%)
+            SELLER_SECURITY_DEPOSIT = Coin.parseCoin("0.005"); // 0.005 BTC about 20 USD @ 4000 USD/BTC
         return SELLER_SECURITY_DEPOSIT;
     }
 }
