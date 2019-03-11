28ee0a31458fdee48f5762262d7c4f0182d1baf8
alex v <alex@encrypt-s.com>
2019-03-11 12:37:06 +0100

    List Navcoin (NAV)

---
+++ assets/src/test/java/bisq/asset/coins/NavcoinTest.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.asset.coins;
+
+import bisq.asset.AbstractAssetTest;
+
+import org.junit.Test;
+
+public class NavcoinTest extends AbstractAssetTest {
+
+    public NavcoinTest() {
+        super(new Navcoin());
+    }
+
+    @Test
+    public void testValidAddresses() {
+        assertValidAddress("NNR93HzmuhYKZ4Tnc9TGoD2DK6TVzXG9P7");
+        assertValidAddress("NSm5NyCe5BFRuV3gFY5VcfhxWx7GTu9U9F");
+        assertValidAddress("NaSdzJ64o8DQo5DMPexVrL4PYFCBZqcmsW");
+
+    }
+
+    @Test
+    public void testInvalidAddresses() {
+        assertInvalidAddress("NNR93HzmuhYKZ4Tnc9TGoD2DK6TVzXG9P");
+        assertInvalidAddress("NNR93HzmuhYKZ4TnO9TGoD2DK6TVzXG9P8");
+        assertInvalidAddress("NNR93HzmuhYKZ4Tnc9TGoD2DK6TVzXG9P71");
+    }
+}
