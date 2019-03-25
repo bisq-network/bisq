2113e02c63f8dfb6cd3318305f3258e9836c69fe
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2019-03-25 12:03:59 -0300

    Fix typo

--- common/src/main/java/bisq/common/proto/persistable/PersistableEnvelope.java
+++ common/src/main/java/bisq/common/proto/persistable/PersistableEnvelope.java
@@ -20,7 +20,7 @@ package bisq.common.proto.persistable;
 import bisq.common.Envelope;
 
 /**
- * Interface for the outside envelope object persisted to disc.
+ * Interface for the outside envelope object persisted to disk.
  */
 public interface PersistableEnvelope extends Envelope {
 }
