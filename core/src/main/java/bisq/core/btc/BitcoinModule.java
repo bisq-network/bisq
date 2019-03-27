fa8b8c57641c0242195e3d0efb8a8346ff2b2d0f
Manfred Karrer <mk@nucleo.io>
2019-03-26 21:14:30 -0500

    use new DAO_REGTEST server and seeds

--- core/src/main/java/bisq/core/btc/BitcoinModule.java
+++ core/src/main/java/bisq/core/btc/BitcoinModule.java
@@ -59,7 +59,7 @@ public class BitcoinModule extends AppModule {
         String regTestHost = environment.getProperty(BtcOptionKeys.REG_TEST_HOST, String.class, "");
         if (regTestHost.isEmpty()) {
             regTestHost = BisqEnvironment.getBaseCurrencyNetwork().isDaoRegTest() ?
-                    "104.248.31.39" :
+                    "134.209.242.206" :
                     RegTestHost.DEFAULT_HOST;
         }
 
