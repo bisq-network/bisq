d52259a227d06ddf0971cfc214b69dd1cb200233
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2019-04-09 12:40:48 -0300

    bitcoinj 0.15: ScriptException moved to another package.

--- core/src/main/java/bisq/core/btc/wallet/WalletService.java
+++ core/src/main/java/bisq/core/btc/wallet/WalletService.java
@@ -38,7 +38,7 @@ import org.bitcoinj.core.Context;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.InsufficientMoneyException;
 import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.ScriptException;
+import org.bitcoinj.script.ScriptException;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
 import org.bitcoinj.core.TransactionConfidence;
