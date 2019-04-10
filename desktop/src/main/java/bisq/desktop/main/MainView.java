3a3a11ed26bd5bad2adcfb953bb95448e9a7fb1d
Christoph Atteneder <christoph.atteneder@gmail.com>
2019-04-10 16:15:03 +0200

    Make blurred background even darker based on #2337.

--- desktop/src/main/java/bisq/desktop/main/MainView.java
+++ desktop/src/main/java/bisq/desktop/main/MainView.java
@@ -122,12 +122,12 @@ public class MainView extends InitializableView<StackPane, MainViewModel> {
 
     @SuppressWarnings("PointlessBooleanExpression")
     public static void blurLight() {
-        transitions.blur(MainView.rootContainer, Transitions.DEFAULT_DURATION, -0.3, false, 5);
+        transitions.blur(MainView.rootContainer, Transitions.DEFAULT_DURATION, -0.8, false, 5);
     }
 
     @SuppressWarnings("PointlessBooleanExpression")
     public static void blurUltraLight() {
-        transitions.blur(MainView.rootContainer, Transitions.DEFAULT_DURATION, -0.1, false, 2);
+        transitions.blur(MainView.rootContainer, Transitions.DEFAULT_DURATION, -0.8, false, 2);
     }
 
     @SuppressWarnings("PointlessBooleanExpression")
