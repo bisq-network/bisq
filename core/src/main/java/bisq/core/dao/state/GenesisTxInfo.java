cdff4d522249a38a7b5304dad76775b06e416ca1
Manfred Karrer <mk@nucleo.io>
2019-04-11 21:51:00 -0500

    Use betanet genesis for mainnet

--- core/src/main/java/bisq/core/dao/state/GenesisTxInfo.java
+++ core/src/main/java/bisq/core/dao/state/GenesisTxInfo.java
@@ -44,9 +44,9 @@ public class GenesisTxInfo {
     // Static
     ///////////////////////////////////////////////////////////////////////////////////////////
 
-    private static final String MAINNET_GENESIS_TX_ID = "25bd4ecedcac35a0db4ce3dd83c7addeca92845fee15f89698afbf7ceeae18e0";
-    private static final int MAINNET_GENESIS_BLOCK_HEIGHT = 570226; // 2019-04-04
-    private static final Coin MAINNET_GENESIS_TOTAL_SUPPLY = Coin.parseCoin("0.43838218");
+    private static final String MAINNET_GENESIS_TX_ID = "0bd66d8ff26476b55dfaf2a5db0c659a5d8635566488244df25606db63a08bd9";
+    private static final int MAINNET_GENESIS_BLOCK_HEIGHT = 567405; // 2019-03-16
+    private static final Coin MAINNET_GENESIS_TOTAL_SUPPLY = Coin.parseCoin("0.49998644");
 
     private static final String TESTNET_GENESIS_TX_ID = "09e70ce0ab7a962a82a2ca84c9ae8a89140bf1c3fb6f7efad6162e39e4b362ae";
     private static final int TESTNET_GENESIS_BLOCK_HEIGHT = 1446300; // 2018-12-02
