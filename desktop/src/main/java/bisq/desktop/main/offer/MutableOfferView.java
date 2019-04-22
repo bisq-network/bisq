e74aad7da65216a9c626374613edb247c35d3c1c
Manfred Karrer <mk@nucleo.io>
2019-04-21 23:39:04 -0500

    Remove rounded fiat amounts popup
    I think the features is now well known and can be removed.
    The account age check will add a nde popup so we would get more popup
    hell if we leave that one...

--- desktop/src/main/java/bisq/desktop/main/offer/MutableOfferView.java
+++ desktop/src/main/java/bisq/desktop/main/offer/MutableOfferView.java
@@ -344,15 +344,6 @@ public abstract class MutableOfferView<M extends MutableOfferViewModel> extends 
     // UI actions
     ///////////////////////////////////////////////////////////////////////////////////////////
 
-    protected void showFiatRoundingInfoPopup() {
-        if (CurrencyUtil.isFiatCurrency(model.tradeCurrencyCode.get()) && !DevEnv.isDevMode()) {
-            new Popup<>().headLine(Res.get("popup.roundedFiatValues.headline"))
-                    .information(Res.get("popup.roundedFiatValues.msg", model.tradeCurrencyCode.get()))
-                    .dontShowAgainId("FiatValuesRoundedWarning")
-                    .show();
-        }
-    }
-
     private void onPlaceOffer() {
         if (model.isReadyForTxBroadcast()) {
             if (model.getDataModel().isMakerFeeValid()) {
@@ -534,8 +525,6 @@ public abstract class MutableOfferView<M extends MutableOfferViewModel> extends 
                 model.onPaymentAccountSelected(paymentAccount);
                 model.onCurrencySelected(model.getDataModel().getTradeCurrency());
             }
-
-            showFiatRoundingInfoPopup();
         } else {
             currencySelection.setVisible(false);
             currencySelection.setManaged(false);
