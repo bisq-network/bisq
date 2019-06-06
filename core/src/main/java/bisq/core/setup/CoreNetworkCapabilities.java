dbb2794ebed780892cdb3e3e5b0db76c3f8b7c5d
Christoph Atteneder <christoph.atteneder@gmail.com>
2019-06-06 16:04:13 +0200

    Add domain layer for signed account age witnesses (credits ManfredKarrer and oscarguindzberg)

--- core/src/main/java/bisq/core/setup/CoreNetworkCapabilities.java
+++ core/src/main/java/bisq/core/setup/CoreNetworkCapabilities.java
@@ -29,6 +29,11 @@ import lombok.extern.slf4j.Slf4j;
 public class CoreNetworkCapabilities {
     public static void setSupportedCapabilities(BisqEnvironment bisqEnvironment) {
         Capabilities.app.addAll(Capability.TRADE_STATISTICS, Capability.TRADE_STATISTICS_2, Capability.ACCOUNT_AGE_WITNESS, Capability.ACK_MSG);
+        Capabilities.app.addAll(Capability.TRADE_STATISTICS,
+                Capability.TRADE_STATISTICS_2,
+                Capability.ACCOUNT_AGE_WITNESS,
+                Capability.ACK_MSG,
+                Capability.SIGNED_ACCOUNT_AGE_WITNESS);
         Capabilities.app.addAll(Capability.BUNDLE_OF_ENVELOPES, Capability.SIGNED_ACCOUNT_AGE_WITNESS);
 
         if (BisqEnvironment.isDaoActivated(bisqEnvironment)) {
