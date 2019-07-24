3798e63001711b1f32193e20d349f2920d9552f0
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2019-07-24 20:20:07 +0200

    Remove swagger jaxrs

--- api/src/main/java/bisq/api/http/service/HttpApiServer.java
+++ api/src/main/java/bisq/api/http/service/HttpApiServer.java
@@ -47,7 +47,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Slf4jRequestLog;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -73,7 +72,7 @@ public class HttpApiServer {
     public void startServer() {
         try {
             ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
-            contextHandlerCollection.setHandlers(new Handler[]{buildAPIHandler(), buildSwaggerUIOverrideHandler(), buildSwaggerUIHandler()});
+            contextHandlerCollection.setHandlers(new Handler[]{buildAPIHandler()});
             // Start server
             InetSocketAddress socketAddress = new InetSocketAddress(bisqEnvironment.getHttpApiHost(), bisqEnvironment.getHttpApiPort());
             Server server = new Server(socketAddress);
@@ -91,30 +90,11 @@ public class HttpApiServer {
         ResourceConfig resourceConfig = new ResourceConfig();
         ExceptionMappers.register(resourceConfig);
         resourceConfig.register(httpApiInterfaceV1);
-        resourceConfig.packages("io.swagger.v3.jaxrs2.integration.resources");
         ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS | ServletContextHandler.NO_SECURITY);
         servletContextHandler.setContextPath("/");
         servletContextHandler.addServlet(new ServletHolder(new ServletContainer(resourceConfig)), "/*");
         setupAuth(servletContextHandler);
         return servletContextHandler;
-    }
-
-    private ContextHandler buildSwaggerUIOverrideHandler() throws Exception {
-        ResourceHandler swaggerUIResourceHandler = new ResourceHandler();
-        swaggerUIResourceHandler.setResourceBase(getClass().getClassLoader().getResource("META-INF/custom-swagger-ui").toURI().toString());
-        ContextHandler swaggerUIContext = new ContextHandler();
-        swaggerUIContext.setContextPath("/docs");
-        swaggerUIContext.setHandler(swaggerUIResourceHandler);
-        return swaggerUIContext;
-    }
-
-    private ContextHandler buildSwaggerUIHandler() throws Exception {
-        ResourceHandler swaggerUIResourceHandler = new ResourceHandler();
-        swaggerUIResourceHandler.setResourceBase(getClass().getClassLoader().getResource("META-INF/resources/webjars/swagger-ui/3.20.1").toURI().toString());
-        ContextHandler swaggerUIContext = new ContextHandler();
-        swaggerUIContext.setContextPath("/docs");
-        swaggerUIContext.setHandler(swaggerUIResourceHandler);
-        return swaggerUIContext;
     }
 
     private void setupAuth(ServletContextHandler appContextHandler) {
