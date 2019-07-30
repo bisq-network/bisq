8d0a9b38baef937890ec60685e6171099c77fcc1
Christoph Sturm <me@christophsturm.com>
2019-07-30 14:35:28 +0200

    make test nicer

--- core/src/test/java/bisq/core/account/sign/SignedWitnessTest.java
+++ core/src/test/java/bisq/core/account/sign/SignedWitnessTest.java
@@ -8,31 +8,23 @@ import org.bitcoinj.core.Utils;
 
 import com.google.common.base.Charsets;
 
-import java.security.KeyPair;
-
 import java.time.Instant;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-
-
-import protobuf.PersistableNetworkPayload;
-
 public class SignedWitnessTest {
     @Test
     public void testProtoRoundTrip() {
         ECKey arbitrator1Key = new ECKey();
 
-        KeyPair peer1KeyPair = Sig.generateKeyPair();
-        byte[] witnessOwner1PubKey = Sig.getPublicKeyBytes(peer1KeyPair.getPublic());
+        byte[] witnessOwner1PubKey = Sig.getPublicKeyBytes(Sig.generateKeyPair().getPublic());
 
         byte[] witnessHash = Utils.sha256hash160(new byte[]{1});
-        byte[] signature1 = arbitrator1Key.signMessage(Utilities.encodeToHex(witnessHash)).getBytes(Charsets.UTF_8);
-        SignedWitness signedWitness = new SignedWitness(true, witnessHash, signature1, arbitrator1Key.getPubKey(), witnessOwner1PubKey, Instant.now().getEpochSecond(), 100);
-        PersistableNetworkPayload proto = signedWitness.toProtoMessage();
-        assertEquals(signedWitness, SignedWitness.fromProto(proto.getSignedWitness()));
+        byte[] witnessHashSignature = arbitrator1Key.signMessage(Utilities.encodeToHex(witnessHash)).getBytes(Charsets.UTF_8);
+        SignedWitness signedWitness = new SignedWitness(true, witnessHash, witnessHashSignature, arbitrator1Key.getPubKey(), witnessOwner1PubKey, Instant.now().getEpochSecond(), 100);
+        assertEquals(signedWitness, SignedWitness.fromProto(signedWitness.toProtoMessage().getSignedWitness()));
 
     }
 
