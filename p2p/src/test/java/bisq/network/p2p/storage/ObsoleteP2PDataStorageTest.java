8a93edec65dc9b9640c32e2376ac757fadfa0476
Christoph Sturm <me@christophsturm.com>
2019-08-14 13:28:59 +0200

    comment out obsolete test

--- p2p/src/test/java/bisq/network/p2p/storage/ObsoleteP2PDataStorageTest.java
+++ p2p/src/test/java/bisq/network/p2p/storage/ObsoleteP2PDataStorageTest.java
@@ -1,20 +1,4 @@
-/*
- * This file is part of Bisq.
- *
- * Bisq is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
- *
- * Bisq is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
- */
-
+/* left as documentation
 package bisq.network.p2p.storage;
 
 import bisq.network.crypto.EncryptionService;
@@ -229,5 +213,5 @@ public class ObsoleteP2PDataStorageTest {
     private boolean checkSignature(ProtectedStorageEntry entry) throws CryptoException {
         byte[] hashOfDataAndSeqNr = Hash.getHash(new P2PDataStorage.DataAndSeqNrPair(entry.getStoragePayload(), entry.sequenceNumber));
         return dataStorage1.checkSignature(entry.ownerPubKey, hashOfDataAndSeqNr, entry.signature);
-    }*/
 }
+    }*/
