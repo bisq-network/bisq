b50fca4233f6daed11cb7e69bea0c14310339e0e
chimp1984 <chimp1984@gmx.com>
2019-08-30 23:15:50 +0200

    Refactor: Rename arbitratorregistration package to arbitrator

--- desktop/src/main/java/bisq/desktop/main/account/register/arbitrator/ArbitratorRegistrationViewModel.java
+++ desktop/src/main/java/bisq/desktop/main/account/register/arbitrator/ArbitratorRegistrationViewModel.java
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.desktop.main.account.register.arbitratorregistration;
+package bisq.desktop.main.account.register.arbitrator;
 
 import bisq.desktop.common.model.ActivatableViewModel;
 
