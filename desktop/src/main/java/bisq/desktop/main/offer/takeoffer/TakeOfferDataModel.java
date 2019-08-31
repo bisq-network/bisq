9fdda0e8965f372ea94009e8944c9b170f2f168c
chimp1984 <chimp1984@gmx.com>
2019-08-31 16:56:23 +0200

    Add checks if any mediator is available

--- desktop/src/main/java/bisq/desktop/main/offer/takeoffer/TakeOfferDataModel.java
+++ desktop/src/main/java/bisq/desktop/main/offer/takeoffer/TakeOfferDataModel.java
@@ -29,6 +29,7 @@ import bisq.core.btc.wallet.BsqWalletService;
 import bisq.core.btc.wallet.BtcWalletService;
 import bisq.core.btc.wallet.Restrictions;
 import bisq.core.dispute.arbitration.Arbitrator;
+import bisq.core.dispute.mediator.Mediator;
 import bisq.core.filter.FilterManager;
 import bisq.core.locale.CurrencyUtil;
 import bisq.core.locale.Res;
@@ -426,6 +427,11 @@ class TakeOfferDataModel extends OfferDataModel {
     boolean hasAcceptedArbitrators() {
         final List<Arbitrator> acceptedArbitrators = user.getAcceptedArbitrators();
         return acceptedArbitrators != null && acceptedArbitrators.size() > 0;
+    }
+
+    boolean hasAcceptedMediators() {
+        final List<Mediator> acceptedMediators = user.getAcceptedMediators();
+        return acceptedMediators != null && acceptedMediators.size() > 0;
     }
 
     long getMaxTradeLimit() {
