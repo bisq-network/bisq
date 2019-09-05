5dcfa96741bf7c7bc177d8478a341320fc91f770
Christoph Atteneder <christoph.atteneder@gmail.com>
2019-09-05 10:35:35 +0200

    Add portuguese (Brazil) locale

--- core/update_translations.sh
+++ core/update_translations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-tx pull -l de,el_GR,es,ja,pt,ru,zh_CN,vi,th_TH,fa,fr
+tx pull -l de,el_GR,es,ja,pt,ru,zh_CN,vi,th_TH,fa,fr,pt_BR
 
 translations="translations/bisq-desktop.displaystringsproperties"
 i18n="src/main/resources/i18n"
@@ -17,5 +17,6 @@ mv "$translations/vi.properties" "$i18n/displayStrings_vi.properties"
 mv "$translations/th_TH.properties" "$i18n/displayStrings_th.properties"
 mv "$translations/fa.properties" "$i18n/displayStrings_fa.properties"
 mv "$translations/fr.properties" "$i18n/displayStrings_fr.properties"
+mv "$translations/pt_BR.properties" "$i18n/displayStrings_pt_BR.properties"
 
 rm -rf $translations
