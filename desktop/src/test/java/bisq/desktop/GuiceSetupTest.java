1ac19a21f1af479222c918b167da57b8f89871b1
Niyi Dada <neeyeed@gmail.com>
2019-09-16 21:33:07 +0100

    Cleaning up formatting.

--- desktop/src/test/java/bisq/desktop/GuiceSetupTest.java
+++ desktop/src/test/java/bisq/desktop/GuiceSetupTest.java
@@ -40,7 +40,9 @@ import bisq.core.user.Preferences;
 import bisq.core.user.User;
 import bisq.core.util.BSFormatter;
 import bisq.core.util.BsqFormatter;
+
 import bisq.core.xmr.wallet.XmrWalletRpcWrapper;
+
 import bisq.network.p2p.network.BridgeAddressProvider;
 import bisq.network.p2p.seed.SeedNodeRepository;
 
