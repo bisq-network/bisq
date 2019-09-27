2f631a8ed6e11a032f7104fea652f0945533cb25
chimp1984 <chimp1984@gmx.com>
2019-09-27 20:08:28 +0200

    Fix test

--- desktop/src/test/java/bisq/desktop/main/funds/transactions/TransactionAwareTradeTest.java
+++ desktop/src/test/java/bisq/desktop/main/funds/transactions/TransactionAwareTradeTest.java
@@ -17,6 +17,7 @@
 
 package bisq.desktop.main.funds.transactions;
 
+import bisq.core.btc.wallet.BtcWalletService;
 import bisq.core.support.dispute.Dispute;
 import bisq.core.support.dispute.arbitration.ArbitrationManager;
 import bisq.core.support.dispute.refund.RefundManager;
@@ -44,6 +45,7 @@ public class TransactionAwareTradeTest {
     private Trade delegate;
     private TransactionAwareTradable trade;
     private RefundManager refundManager;
+    private BtcWalletService btcWalletService;
 
     @Before
     public void setUp() {
@@ -53,7 +55,8 @@ public class TransactionAwareTradeTest {
         delegate = mock(Trade.class, RETURNS_DEEP_STUBS);
         arbitrationManager = mock(ArbitrationManager.class, RETURNS_DEEP_STUBS);
         refundManager = mock(RefundManager.class, RETURNS_DEEP_STUBS);
-        trade = new TransactionAwareTrade(delegate, arbitrationManager, refundManager, null, null);
+        btcWalletService = mock(BtcWalletService.class, RETURNS_DEEP_STUBS);
+        trade = new TransactionAwareTrade(delegate, arbitrationManager, refundManager, btcWalletService, null);
     }
 
     @Test
