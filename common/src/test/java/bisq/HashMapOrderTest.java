daa379760770b2cd751fe0b2c78ed011d547b3ae
Christoph Sturm <me@christophsturm.com>
2019-10-11 10:55:34 +0200

    Show that even with a order preserving TreeMap protobuf orders the map

--- common/src/test/java/bisq/HashMapOrderTest.java
+++ common/src/test/java/bisq/HashMapOrderTest.java
@@ -1,6 +1,6 @@
 package bisq;
 
-import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -13,9 +13,15 @@ import protobuf.TempProposalPayload;
 public class HashMapOrderTest {
     @Test
     public void mapIsNotOrderPreservingButAlwaysEnforcesKeyOrder() {
-        TempProposalPayload prop = TempProposalPayload.newBuilder().putAllExtraData(Map.of("k1", "v1", "k2", "v2")).build();
+        TreeMap<String, String> order1 = new TreeMap<>();
+        order1.put("k1", "v1");
+        order1.put("k2", "v2");
+        TreeMap<String, String> order2 = new TreeMap<>();
+        order2.put("k2", "v2");
+        order2.put("k1", "v1");
+        TempProposalPayload prop = TempProposalPayload.newBuilder().putAllExtraData(order1).build();
         byte[] bytes1 = prop.toByteArray();
-        TempProposalPayload prop2 = TempProposalPayload.newBuilder().putAllExtraData(Map.of("k2", "v2", "k1", "v1")).build();
+        TempProposalPayload prop2 = TempProposalPayload.newBuilder().putAllExtraData(order2).build();
         byte[] bytes2 = prop2.toByteArray();
         assertArrayEquals(bytes1, bytes2);
     }
