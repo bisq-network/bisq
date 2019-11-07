2b2e2c237d95c69cb664a1e234987ef4b7d60a4e
Christoph Atteneder <christoph.atteneder@gmail.com>
2019-11-07 16:00:45 +0100

    Release/v1.2.3 (#3577)
    * Bump version number
    
    * Revert to SNAPSHOT version

--- desktop/package/macosx/create_app.sh
+++ desktop/package/macosx/create_app.sh
@@ -6,7 +6,7 @@ mkdir -p deploy
 
 set -e
 
-version="1.2.2-SNAPSHOT"
+version="1.2.3-SNAPSHOT"
 
 cd ..
 ./gradlew :desktop:build -x test shadowJar
--- desktop/package/macosx/replace_version_number.sh
+++ desktop/package/macosx/replace_version_number.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)/../../../
 
-oldVersion=1.2.1
-newVersion=1.2.2
+oldVersion=1.2.2
+newVersion=1.2.3
 
 find . -type f \( -name "finalize.sh" \
 -o -name "create_app.sh" \
