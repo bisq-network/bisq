74b84159caf0ec138001d27ebdfa4089533dec2c
Florian Reimair <office@florianreimair.at>
2019-11-19 11:45:01 +0100

    Price monitor is more resilient against timeouts
    Recenty, a price node got removed. Unfortunately, this node
    has been the first in the list of configured price nodes in
    the monitor configuration.
    
    A misplaced catch block caused the loop to stop instead of
    trying the next configured price node in the list.

--- monitor/src/main/java/bisq/monitor/metric/PriceNodeStats.java
+++ monitor/src/main/java/bisq/monitor/metric/PriceNodeStats.java
@@ -88,65 +88,70 @@ public class PriceNodeStats extends Metric {
                 NodeAddress tmp = OnionParser.getNodeAddress(current);
 
                 // connect
-                SocksSocket socket = new SocksSocket(proxy, tmp.getHostName(), tmp.getPort());
+                try {
+                    SocksSocket socket = new SocksSocket(proxy, tmp.getHostName(), tmp.getPort());
 
-                // prepare to receive data
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    // prepare to receive data
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                // ask for fee data
-                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-                out.println("GET /getFees/");
-                out.println();
-                out.flush();
+                    // ask for fee data
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+                    out.println("GET /getFees/");
+                    out.println();
+                    out.flush();
 
-                // sift through the received lines and see if we got something json-like
-                String line;
-                while((line = in.readLine()) != null) {
-                    Matcher matcher = stringNumberPattern.matcher(line);
-                    if(matcher.find())
-                        if(!IGNORE.contains(matcher.group(1)))
-                            result.put("fees." + matcher.group(1), matcher.group(2));
+                    // sift through the received lines and see if we got something json-like
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        Matcher matcher = stringNumberPattern.matcher(line);
+                        if (matcher.find())
+                            if (!IGNORE.contains(matcher.group(1)))
+                                result.put("fees." + matcher.group(1), matcher.group(2));
+                    }
+
+                    in.close();
+                    out.close();
+                    socket.close();
+
+                    // connect
+                    socket = new SocksSocket(proxy, tmp.getHostName(), tmp.getPort());
+
+                    // prepare to receive data
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    // ask for exchange rate data
+                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+                    out.println("GET /getAllMarketPrices/");
+                    out.println();
+                    out.flush();
+
+                    String currencyCode = "";
+                    while ((line = in.readLine()) != null) {
+                        Matcher currencyCodeMatcher = currencyCodePattern.matcher(line);
+                        Matcher priceMatcher = pricePattern.matcher(line);
+                        if (currencyCodeMatcher.find()) {
+                            currencyCode = currencyCodeMatcher.group(1);
+                            if (!assets.contains(currencyCode))
+                                currencyCode = "";
+                        } else if (!"".equals(currencyCode) && priceMatcher.find())
+                            result.put("price." + currencyCode, priceMatcher.group(1));
+                    }
+
+                    // close all the things
+                    in.close();
+                    out.close();
+                    socket.close();
+
+                    // report
+                    reporter.report(result, getName());
+
+                    // only ask for data as long as we got none
+                    if (!result.isEmpty())
+                        break;
+                } catch (IOException e) {
+                    log.error("{} seems to be down. Trying next configured price node.", tmp.getHostName());
+                    e.printStackTrace();
                 }
-
-                in.close();
-                out.close();
-                socket.close();
-
-                // connect
-                socket = new SocksSocket(proxy, tmp.getHostName(), tmp.getPort());
-
-                // prepare to receive data
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                // ask for exchange rate data
-                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-                out.println("GET /getAllMarketPrices/");
-                out.println();
-                out.flush();
-
-                String currencyCode = "";
-                while((line = in.readLine()) != null) {
-                    Matcher currencyCodeMatcher = currencyCodePattern.matcher(line);
-                    Matcher priceMatcher = pricePattern.matcher(line);
-                    if(currencyCodeMatcher.find()) {
-                        currencyCode = currencyCodeMatcher.group(1);
-                        if(!assets.contains(currencyCode))
-                            currencyCode = "";
-                    } else if(!"".equals(currencyCode) && priceMatcher.find())
-                        result.put("price." + currencyCode, priceMatcher.group(1));
-                }
-
-                // close all the things
-                in.close();
-                out.close();
-                socket.close();
-
-                // report
-                reporter.report(result, getName());
-
-                // only ask for data as long as we got none
-                if(!result.isEmpty())
-                    break;
             }
         } catch (TorCtlException | IOException e) {
             // TODO Auto-generated catch block
