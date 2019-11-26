91b76d42310359686f9c7e772bd5d07397db2159
chimp1984 <chimp1984@gmx.com>
2019-11-26 12:51:57 -0500

    Add list of shortcuts

--- desktop/src/main/java/bisq/desktop/main/settings/about/AboutView.java
+++ desktop/src/main/java/bisq/desktop/main/settings/about/AboutView.java
@@ -94,6 +94,86 @@ public class AboutView extends ActivatableView<GridPane, Void> {
                         Version.getP2PMessageVersion(),
                         Version.LOCAL_DB_VERSION,
                         Version.TRADE_PROTOCOL_VERSION));
+
+        addTitledGroupBg(root, ++gridRow, 20, Res.get("setting.about.shortcuts"), Layout.GROUP_DISTANCE);
+
+        // basics
+        addCompactTopLabelTextField(root, gridRow, Res.get("setting.about.shortcuts.menuNav"),
+                Res.get("setting.about.shortcuts.menuNav.value"),
+                Layout.TWICE_FIRST_ROW_AND_GROUP_DISTANCE);
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.close"),
+                Res.get("setting.about.shortcuts.close.value", "q", "w"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.closePopup"),
+                Res.get("setting.about.shortcuts.closePopup.value"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.chatSendMsg"),
+                Res.get("setting.about.shortcuts.chatSendMsg.value"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.openDispute"),
+                Res.get("setting.about.shortcuts.openDispute.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "o")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.walletDetails"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "j"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.openEmergencyBtcWalletTool"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "e"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.openEmergencyBsqWalletTool"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "b"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.showDisputeStatistics"),
+                Res.get("setting.about.shortcuts.showDisputeStatistics.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "l")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.showTorLogs"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "t"));
+
+        // special
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.removeStuckTrade"),
+                Res.get("setting.about.shortcuts.removeStuckTrade.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "y")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.manualPayoutTxWindow"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "g"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.reRepublishAllGovernanceData"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "h"));
+
+        // for arbitrators
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.registerArbitrator"),
+                Res.get("setting.about.shortcuts.registerArbitrator.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "n")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.registerMediator"),
+                Res.get("setting.about.shortcuts.registerMediator.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "d")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.reOpenDispute"),
+                Res.get("setting.about.shortcuts.reOpenDispute.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "u")));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.openSignPaymentAccountsWindow"),
+                Res.get("setting.about.shortcuts.openSignPaymentAccountsWindow.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "s")));
+
+        // only for maintainers
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.sendAlertMsg"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "m"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.sendFilter"),
+                Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "f"));
+
+        addCompactTopLabelTextField(root, ++gridRow, Res.get("setting.about.shortcuts.sendPrivateNotification"),
+                Res.get("setting.about.shortcuts.sendPrivateNotification.value",
+                        Res.get("setting.about.shortcuts.ctrlOrAltOrCmd", "r")));
+
+        // Not added:
+        // allTradesWithReferralId, allOffersWithReferralId -> ReferralId is not used yet
+        // revert tx -> not tested well, high risk
+        // debug window -> not maintained, only for devs working on trade protocol relevant
     }
 
     @Override
