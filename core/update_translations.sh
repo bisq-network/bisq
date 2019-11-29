ba492b88e659b4bbeb85cdee8ce21c1c1e2c85ad
wiz <j@wiz.biz>
2019-11-29 11:34:22 +0900

    Adjust update_translations.sh language tags for script/regional variants

--- core/update_translations.sh
+++ core/update_translations.sh
@@ -12,11 +12,11 @@ mv "$translations/es.properties" "$i18n/displayStrings_es.properties"
 mv "$translations/ja.properties" "$i18n/displayStrings_ja.properties"
 mv "$translations/pt.properties" "$i18n/displayStrings_pt.properties"
 mv "$translations/ru.properties" "$i18n/displayStrings_ru.properties"
-mv "$translations/zh_CN.properties" "$i18n/displayStrings_zh.properties"
+mv "$translations/zh_CN.properties" "$i18n/displayStrings_zh-hans.properties"
 mv "$translations/vi.properties" "$i18n/displayStrings_vi.properties"
 mv "$translations/th_TH.properties" "$i18n/displayStrings_th.properties"
 mv "$translations/fa.properties" "$i18n/displayStrings_fa.properties"
 mv "$translations/fr.properties" "$i18n/displayStrings_fr.properties"
-mv "$translations/pt_BR.properties" "$i18n/displayStrings_pt_BR.properties"
+mv "$translations/pt_BR.properties" "$i18n/displayStrings_pt-br.properties"
 
 rm -rf $translations
