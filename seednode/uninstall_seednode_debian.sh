9c38a7395801e9d109534a4b2f37812705a36583
wiz <j@wiz.biz>
2020-01-25 12:13:37 +0900

    Fix seednode uninstall script to use correct service names

--- seednode/uninstall_seednode_debian.sh
+++ seednode/uninstall_seednode_debian.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "[*] Uninstalling Bitcoin and Bisq, will delete all data!!"
+sleep 10
 sudo rm -rf /root/bisq
 sudo systemctl stop bitcoin
-sudo systemctl stop bisq
+sudo systemctl stop bisq-seednode
 sudo systemctl disable bitcoin
-sudo systemctl disable bisq
-sleep 10
+sudo systemctl disable bisq-seednode
 sudo userdel -f -r bisq
-sleep 10
 sudo userdel -f -r bitcoin
 echo "[*] Done!"
