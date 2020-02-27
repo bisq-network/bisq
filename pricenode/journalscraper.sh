cfa55df4b2823dbf68b5854fbedd47242edf8c5f
Florian Reimair <office@florianreimair.at>
2020-02-27 11:26:26 +0100

    Align scraper script

--- pricenode/journalscraper.sh
+++ pricenode/journalscraper.sh
@@ -8,7 +8,7 @@ while true;
 do
 	now=$(date +"%F %T")
 
-	journalctl -u bisq --since="$last" --until="$now" | grep -Eo "getAllMarketPrices.*bisq/[0-9].[0-9].[0-9]" | cut -d / -f 2 | sort | uniq -c | while read -r line; do
+	journalctl -u bisq-pricenode --since="$last" --until="$now" | grep -Eo "getAllMarketPrices.*bisq/[0-9].[0-9].[0-9]" | cut -d / -f 2 | sort | uniq -c | while read -r line; do
 		number=$(echo "${line}" | cut -d ' ' -f 1);
 		version=$(echo "${line}" | cut -d \  -f 2);
 		version=${version//./_};
