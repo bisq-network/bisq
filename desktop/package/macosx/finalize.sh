066614f966d70436392f7a4206eb4d9ac845729e
Christoph Atteneder <christoph.atteneder@gmail.com>
2020-03-13 17:16:09 +0100

    Bump version number for v1.2.9

--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-version="1.2.8"
+version="1.2.9"
 
 target_dir="releases/$version"
 
--- seednode/src/main/java/bisq/seednode/SeedNodeMain.java
+++ seednode/src/main/java/bisq/seednode/SeedNodeMain.java
@@ -30,7 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SeedNodeMain extends ExecutableForAppWithP2p {
-    private static final String VERSION = "1.2.8";
+    private static final String VERSION = "1.2.9";
     private SeedNode seedNode;
 
     public SeedNodeMain() {
