862ded2428d13fe9e3340ef8c8e2b8405d0e4549
Florian Reimair <office@florianreimair.at>
2020-04-01 12:34:22 +0200

    Fix unit name

--- pricenode/journalscraper_hsversion.sh
+++ pricenode/journalscraper_hsversion.sh
@@ -8,7 +8,7 @@ while true;
 do
 	now=$(date +"%F %T")
 
-	journalctl -u pricenode --since="$last" --until="$now" | grep -Eo "getAllMarketPrices.*HSv[0-9]" | grep -o "HSv[0-9]" | sort | uniq -c | while read -r line; do
+	journalctl -u bisq-pricenode --since="$last" --until="$now" | grep -Eo "getAllMarketPrices.*HSv[0-9]" | grep -o "HSv[0-9]" | sort | uniq -c | while read -r line; do
 		number=$(echo "${line}" | cut -d ' ' -f 1);
 		version=$(echo "${line}" | cut -d \  -f 2);
 		version=${version//./_};
