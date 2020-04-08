51ffd9d95c2823d5ac13ab0ba235698d199d9440
Christoph Atteneder <christoph.atteneder@gmail.com>
2020-04-08 20:04:13 +0200

    Bump version number for v1.3.1

--- desktop/package/macosx/create_app.sh
+++ desktop/package/macosx/create_app.sh
@@ -6,7 +6,7 @@ mkdir -p deploy
 
 set -e
 
-version="1.3.0"
+version="1.3.1"
 
 cd ..
 ./gradlew :desktop:build -x test shadowJar
--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-version="1.3.0"
+version="1.3.1"
 
 target_dir="releases/$version"
 
