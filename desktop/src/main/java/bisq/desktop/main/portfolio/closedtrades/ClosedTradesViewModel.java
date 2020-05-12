f2f519ada66a1dfeb39e428619f1b57ee2b5b76e
jmacxx <47253594+jmacxx@users.noreply.github.com>
2020-05-12 15:17:10 -0500

    Display correct trade and tx fees in CSV export
    When exporting trade history to CSV, users would expect to see
    their own transaction and trade fees listed.  Maker fees are
    stored on the Offer, Taker fees are stored on the Trade object.
    The code was not properly checking whether the user was the
    Offer Maker or Taker, and as such the report was always displaying
    the Taker information for Tx and Trade fee.  Therefore any trades
    where the user was Maker showed the wrong fees.
    
    The column title `Maker Fee` is incorrect and misleading since
    the user doing the export is not necessarily the Maker.  We
    rename this column to `Trade Fee` to indicate that it is the fee
    the user paid for trading regardless of Maker/Taker status.
    
    Fixes #4207

--- desktop/src/main/java/bisq/desktop/main/portfolio/closedtrades/ClosedTradesViewModel.java
+++ desktop/src/main/java/bisq/desktop/main/portfolio/closedtrades/ClosedTradesViewModel.java
@@ -91,7 +91,7 @@ class ClosedTradesViewModel extends ActivatableWithDataModel<ClosedTradesDataMod
         if (item == null)
             return "";
         Tradable tradable = item.getTradable();
-        if (tradable instanceof Trade)
+        if (!wasMyOffer(tradable) && (tradable instanceof Trade))
             return formatter.formatCoin(((Trade) tradable).getTxFee());
         else
             return formatter.formatCoin(tradable.getOffer().getTxFee());
@@ -101,7 +101,7 @@ class ClosedTradesViewModel extends ActivatableWithDataModel<ClosedTradesDataMod
         if (item == null)
             return "";
         Tradable tradable = item.getTradable();
-        if (tradable instanceof Trade)
+        if (!wasMyOffer(tradable) && (tradable instanceof Trade))
             return formatter.formatCoin(((Trade) tradable).getTakerFee());
         else
             return formatter.formatCoin(tradable.getOffer().getMakerFee());
@@ -194,5 +194,9 @@ class ClosedTradesViewModel extends ActivatableWithDataModel<ClosedTradesDataMod
                         ((Trade) e).getTradingPeerNodeAddress().getFullAddress().equals(((Trade) tradable).getTradingPeerNodeAddress().getFullAddress()))
                 .collect(Collectors.toSet())
                 .size();
+    }
+
+    boolean wasMyOffer(Tradable tradable) {
+        return dataModel.closedTradableManager.wasMyOffer(tradable.getOffer());
     }
 }
