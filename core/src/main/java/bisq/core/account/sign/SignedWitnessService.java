bdb78071316331fa8fe3b04ab64094ff7a80039e
sqrrm <sqrrm1@gmail.com>
2020-06-14 20:08:22 +0200

    Sign accounts with same name
    If the user has an account that can sign, it signs other accounts with
    the same holder name when they are added, or on startup if one of
    several accounts with the same name got signer rights.

--- core/src/main/java/bisq/core/account/sign/SignedWitnessService.java
+++ core/src/main/java/bisq/core/account/sign/SignedWitnessService.java
@@ -213,6 +213,12 @@ public class SignedWitnessService {
         log.info("Arbitrator signed witness {}", signedWitness.toString());
     }
 
+    public void selfSignAccountAgeWitness(AccountAgeWitness accountAgeWitness) throws CryptoException {
+        log.info("Sign own accountAgeWitness {}", accountAgeWitness);
+        signAccountAgeWitness(MINIMUM_TRADE_AMOUNT_FOR_SIGNING, accountAgeWitness,
+                keyRing.getSignatureKeyPair().getPublic());
+    }
+
     // Any peer can sign with DSA key
     public void signAccountAgeWitness(Coin tradeAmount,
                                       AccountAgeWitness accountAgeWitness,
