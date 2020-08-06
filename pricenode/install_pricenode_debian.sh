11076e773dccb95f39a08d4ae5e218a677ae91bc
cd2357 <15956136+cd2357@users.noreply.github.com>
2020-08-06 10:26:13 +0200

    Set quiet flag for java install command
    Reduce verbosity of install command to only errors
    
    Co-authored-by: wiz <j@wiz.biz>

--- pricenode/install_pricenode_debian.sh
+++ pricenode/install_pricenode_debian.sh
@@ -61,7 +61,7 @@ sudo -H -i -u "${BISQ_USER}" git config --global advice.detachedHead false
 sudo -H -i -u "${BISQ_USER}" git clone --branch "${BISQ_REPO_TAG}" "${BISQ_REPO_URL}" "${BISQ_HOME}/${BISQ_REPO_NAME}"
 
 echo "[*] Installing OpenJDK 11"
-sudo -H -i -u "${ROOT_USER}" apt-get install -y openjdk-11-jdk
+sudo -H -i -u "${ROOT_USER}" apt-get install -qq -y openjdk-11-jdk
 
 echo "[*] Checking out Bisq ${BISQ_LATEST_RELEASE}"
 sudo -H -i -u "${BISQ_USER}" sh -c "cd ${BISQ_HOME}/${BISQ_REPO_NAME} && git checkout ${BISQ_LATEST_RELEASE}"
