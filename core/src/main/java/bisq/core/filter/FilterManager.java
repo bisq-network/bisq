1b5e449114dfdc4e073657debfb4fd45adf1985a
chimp1984 <chimp1984@gmx.com>
2020-08-31 12:34:16 -0500

    Remove dev test code

--- core/src/main/java/bisq/core/filter/FilterManager.java
+++ core/src/main/java/bisq/core/filter/FilterManager.java
@@ -119,8 +119,7 @@ public class FilterManager {
         this.providersRepository = providersRepository;
         this.ignoreDevMsg = ignoreDevMsg;
 
-        //todo test
-        publicKeys = !useDevPrivilegeKeys ?
+        publicKeys = useDevPrivilegeKeys ?
                 Collections.singletonList(DevEnv.DEV_PRIVILEGE_PUB_KEY) :
                 List.of("0358d47858acdc41910325fce266571540681ef83a0d6fedce312bef9810793a27",
                         "029340c3e7d4bb0f9e651b5f590b434fecb6175aeaa57145c7804ff05d210e534f");
