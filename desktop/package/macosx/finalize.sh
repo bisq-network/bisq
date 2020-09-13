aadd2e43d3395db5775eeae5a969601de9cc9f96
chimp1984 <chimp1984@gmx.com>
2020-09-13 03:35:09 -0500

    Revert version to 1.3.7 to not break build

--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-version="1.3.8"
+version="1.3.7"
 
 target_dir="releases/$version"
 
--- desktop/package/windows/release.bat
+++ desktop/package/windows/release.bat
@@ -6,7 +6,7 @@
 
 @echo off
 
-set version=1.3.8
+set version=1.3.7
 set release_dir=%~dp0..\..\..\releases\%version%
 set package_dir=%~dp0..
 
