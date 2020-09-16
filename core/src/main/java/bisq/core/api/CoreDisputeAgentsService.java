57423f4fdb16a8720ff245c44ac1126bd0ba8079
ghubstan <36207203+ghubstan@users.noreply.github.com>
2020-09-16 11:14:30 -0300

    Refactor to fix dispute agent mgr usage

--- core/src/main/java/bisq/core/api/CoreDisputeAgentsService.java
+++ core/src/main/java/bisq/core/api/CoreDisputeAgentsService.java
@@ -32,8 +32,6 @@ import org.bitcoinj.core.ECKey;
 
 import javax.inject.Inject;
 
-import java.net.InetAddress;
-
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -42,6 +40,7 @@ import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 
 import static bisq.common.app.DevEnv.DEV_PRIVILEGE_PRIV_KEY;
+import static java.net.InetAddress.getLoopbackAddress;
 
 @Slf4j
 class CoreDisputeAgentsService {
@@ -51,6 +50,8 @@ class CoreDisputeAgentsService {
     private final MediatorManager mediatorManager;
     private final RefundAgentManager refundAgentManager;
     private final P2PService p2PService;
+    private final NodeAddress nodeAddress;
+    private final List<String> languageCodes;
 
     @Inject
     public CoreDisputeAgentsService(Config config,
@@ -63,6 +64,8 @@ class CoreDisputeAgentsService {
         this.mediatorManager = mediatorManager;
         this.refundAgentManager = refundAgentManager;
         this.p2PService = p2PService;
+        this.nodeAddress = new NodeAddress(getLoopbackAddress().getHostAddress(), config.nodePort);
+        this.languageCodes = Arrays.asList("de", "en", "es", "fr");
     }
 
     public void registerDisputeAgent(String disputeAgentType, String registrationKey) {
@@ -77,22 +80,20 @@ class CoreDisputeAgentsService {
         if (!registrationKey.equals(DEV_PRIVILEGE_PRIV_KEY))
             throw new IllegalArgumentException("invalid registration key");
 
+        ECKey ecKey;
+        String signature;
         switch (disputeAgentType) {
             case "arbitrator":
                 throw new IllegalArgumentException("arbitrators must be registered in a Bisq UI");
             case "mediator":
+                ecKey = mediatorManager.getRegistrationKey(registrationKey);
+                signature = mediatorManager.signStorageSignaturePubKey(Objects.requireNonNull(ecKey));
+                registerMediator(nodeAddress, languageCodes, ecKey, signature);
+                return;
             case "refundagent":
-                NodeAddress nodeAddress = new NodeAddress(
-                        InetAddress.getLoopbackAddress().getHostAddress(), config.nodePort);
-                List<String> languageCodes = Arrays.asList("de", "en", "es", "fr");
-                ECKey ecKey = mediatorManager.getRegistrationKey(registrationKey);
-                String signature = mediatorManager.signStorageSignaturePubKey(Objects.requireNonNull(ecKey));
-
-                if (disputeAgentType.equals("mediator"))
-                    registerMediator(nodeAddress, languageCodes, ecKey, signature);
-                else
-                    registerRefundAgent(nodeAddress, languageCodes, ecKey, signature);
-
+                ecKey = refundAgentManager.getRegistrationKey(registrationKey);
+                signature = refundAgentManager.signStorageSignaturePubKey(Objects.requireNonNull(ecKey));
+                registerRefundAgent(nodeAddress, languageCodes, ecKey, signature);
                 return;
             default:
                 throw new IllegalArgumentException("unknown dispute agent type " + disputeAgentType);
@@ -103,8 +104,7 @@ class CoreDisputeAgentsService {
                                   List<String> languageCodes,
                                   ECKey ecKey,
                                   String signature) {
-        Mediator mediator = new Mediator(
-                nodeAddress,
+        Mediator mediator = new Mediator(nodeAddress,
                 keyRing.getPubKeyRing(),
                 languageCodes,
                 new Date().getTime(),
@@ -125,8 +125,7 @@ class CoreDisputeAgentsService {
                                      List<String> languageCodes,
                                      ECKey ecKey,
                                      String signature) {
-        RefundAgent refundAgent = new RefundAgent(
-                nodeAddress,
+        RefundAgent refundAgent = new RefundAgent(nodeAddress,
                 keyRing.getPubKeyRing(),
                 languageCodes,
                 new Date().getTime(),
