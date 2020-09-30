5142d2831323d9609a00864be2d15da292e03ad1
chimp1984 <chimp1984@gmx.com>
2020-09-30 18:37:20 -0500

    Make method public

--- common/src/main/java/bisq/common/file/FileUtil.java
+++ common/src/main/java/bisq/common/file/FileUtil.java
@@ -201,7 +201,7 @@ public class FileUtil {
         FileUtils.copyDirectory(source, destination);
     }
 
-    static File createNewFile(Path path) throws IOException {
+    public static File createNewFile(Path path) throws IOException {
         File file = path.toFile();
         if (!file.createNewFile()) {
             throw new IOException("There already exists a file with path: " + path);
