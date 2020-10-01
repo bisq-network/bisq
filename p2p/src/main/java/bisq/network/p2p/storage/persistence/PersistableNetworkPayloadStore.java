3a348d5a0ffa37436782f28e4c6b6e3790dd1817
chimp1984 <chimp1984@gmx.com>
2020-10-01 17:12:31 -0500

    Remove ThreadedPersistableEnvelope and use PersistableEnvelope instead
    We will not need threading support anymore once we use the new persistenceManager as serialisation is done on the userThread.

--- p2p/src/main/java/bisq/network/p2p/storage/persistence/PersistableNetworkPayloadStore.java
+++ p2p/src/main/java/bisq/network/p2p/storage/persistence/PersistableNetworkPayloadStore.java
@@ -20,7 +20,7 @@ package bisq.network.p2p.storage.persistence;
 import bisq.network.p2p.storage.P2PDataStorage;
 import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
 
-import bisq.common.proto.persistable.ThreadedPersistableEnvelope;
+import bisq.common.proto.persistable.PersistableEnvelope;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,7 +31,7 @@ import lombok.Getter;
  *  Base class for store implementations using a map with a PersistableNetworkPayload
  *  as the type of the map value.
  */
-public abstract class PersistableNetworkPayloadStore implements ThreadedPersistableEnvelope {
+public abstract class PersistableNetworkPayloadStore implements PersistableEnvelope {
     @Getter
     public Map<P2PDataStorage.ByteArray, PersistableNetworkPayload> map = new ConcurrentHashMap<>();
 }
