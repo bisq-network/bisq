cb6a5abbc1a098b84064e79d636f21a5b0f50605
chimp1984 <chimp1984@gmx.com>
2020-09-30 20:32:56 -0500

    Refactor: Move dao storage classes to storage package

--- core/src/main/java/bisq/core/dao/state/storage/DaoStateStore.java
+++ core/src/main/java/bisq/core/dao/state/storage/DaoStateStore.java
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.dao.state;
+package bisq.core.dao.state.storage;
 
 import bisq.core.dao.monitoring.model.DaoStateHash;
 import bisq.core.dao.state.model.DaoState;
