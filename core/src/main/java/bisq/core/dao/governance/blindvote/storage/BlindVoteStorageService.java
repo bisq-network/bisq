dc77b5cab851cadbb51ce1fdbce368e25eeeb560
chimp1984 <chimp1984@gmx.com>
2020-10-01 17:36:54 -0500

    Use PersistenceManager

--- core/src/main/java/bisq/core/dao/governance/blindvote/storage/BlindVoteStorageService.java
+++ core/src/main/java/bisq/core/dao/governance/blindvote/storage/BlindVoteStorageService.java
@@ -22,10 +22,10 @@ import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
 import bisq.network.p2p.storage.persistence.MapStoreService;
 
 import bisq.common.config.Config;
-import bisq.common.storage.Storage;
+import bisq.common.persistence.PersistenceManager;
 
-import javax.inject.Named;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import java.io.File;
 
@@ -50,8 +50,8 @@ public class BlindVoteStorageService extends MapStoreService<BlindVoteStore, Per
 
     @Inject
     public BlindVoteStorageService(@Named(Config.STORAGE_DIR) File storageDir,
-                                   Storage<BlindVoteStore> persistableNetworkPayloadMapStorage) {
-        super(storageDir, persistableNetworkPayloadMapStorage);
+                                   PersistenceManager<BlindVoteStore> persistenceManager) {
+        super(storageDir, persistenceManager);
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
