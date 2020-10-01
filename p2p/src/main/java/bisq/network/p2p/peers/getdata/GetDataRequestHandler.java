7122ef0356add96a11c533880f76ee6f248d329d
chimp1984 <chimp1984@gmx.com>
2020-10-01 15:01:33 -0500

    Refactoring: Rename variables

--- p2p/src/main/java/bisq/network/p2p/peers/getdata/GetDataRequestHandler.java
+++ p2p/src/main/java/bisq/network/p2p/peers/getdata/GetDataRequestHandler.java
@@ -88,22 +88,22 @@ public class GetDataRequestHandler {
                 .map(e -> "node address " + e.getFullAddress())
                 .orElseGet(() -> "connection UID " + connection.getUid());
 
-        AtomicBoolean outPersistableNetworkPayloadOutputTruncated = new AtomicBoolean(false);
-        AtomicBoolean outProtectedStoragePayloadOutputTruncated = new AtomicBoolean(false);
+        AtomicBoolean wasPersistableNetworkPayloadsTruncated = new AtomicBoolean(false);
+        AtomicBoolean wasProtectedStorageEntriesTruncated = new AtomicBoolean(false);
         GetDataResponse getDataResponse = dataStorage.buildGetDataResponse(
                 getDataRequest,
                 MAX_ENTRIES,
-                outPersistableNetworkPayloadOutputTruncated,
-                outProtectedStoragePayloadOutputTruncated,
+                wasPersistableNetworkPayloadsTruncated,
+                wasProtectedStorageEntriesTruncated,
                 connection.getCapabilities());
 
-        if (outPersistableNetworkPayloadOutputTruncated.get()) {
+        if (wasPersistableNetworkPayloadsTruncated.get()) {
             log.warn("The getData request from peer with {} caused too much PersistableNetworkPayload " +
                             "entries to get delivered. We limited the entries for the response to {} entries",
                     connectionInfo, MAX_ENTRIES);
         }
 
-        if (outProtectedStoragePayloadOutputTruncated.get()) {
+        if (wasProtectedStorageEntriesTruncated.get()) {
             log.warn("The getData request from peer with {} caused too much ProtectedStorageEntry " +
                             "entries to get delivered. We limited the entries for the response to {} entries",
                     connectionInfo, MAX_ENTRIES);
