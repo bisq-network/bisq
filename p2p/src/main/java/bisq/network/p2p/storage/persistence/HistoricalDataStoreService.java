9d517140b28606cc212052521e26c6f5753fc249
chimp1984 <54558767+chimp1984@users.noreply.github.com>
2020-10-02 13:16:04 -0500

    Update p2p/src/main/java/bisq/network/p2p/storage/persistence/HistoricalDataStoreService.java
    Co-authored-by: sqrrm <sqrrm@users.noreply.github.com>

--- p2p/src/main/java/bisq/network/p2p/storage/persistence/HistoricalDataStoreService.java
+++ p2p/src/main/java/bisq/network/p2p/storage/persistence/HistoricalDataStoreService.java
@@ -79,7 +79,7 @@ public abstract class HistoricalDataStoreService<T extends PersistableNetworkPay
                     boolean newVersion = Version.isNewVersion(storeVersion, requestersVersion);
                     String details = newVersion ?
                             "As our historical store is a newer version we add the data to our result map." :
-                            "As the requester version is not older as our historical store we do not " +
+                            "As the requester version is not older than our historical store we do not " +
                                     "add the data to the result map.";
                     log.info("The requester had version {}. Our historical data store has version {}.\n{}",
                             requestersVersion, storeVersion, details);
