c71ad8489c0a0e7ec89a9a0c1575b683f216548f
ghubstan <36207203+ghubstan@users.noreply.github.com>
2020-10-02 17:57:31 -0300

    Change 'break' to 'return' at end of switch.

--- core/src/main/java/bisq/core/api/CoreDisputeAgentsService.java
+++ core/src/main/java/bisq/core/api/CoreDisputeAgentsService.java
@@ -99,7 +99,7 @@ class CoreDisputeAgentsService {
                     ecKey = refundAgentManager.getRegistrationKey(registrationKey);
                     signature = refundAgentManager.signStorageSignaturePubKey(Objects.requireNonNull(ecKey));
                     registerRefundAgent(nodeAddress, languageCodes, ecKey, signature);
-                    break;
+                    return;
             }
         } else {
             throw new IllegalArgumentException("unknown dispute agent type " + disputeAgentTypeString);
