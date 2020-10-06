cea3867992b42e9ce1f13d02c7cec965eb722b41
chimp1984 <chimp1984@gmx.com>
2020-10-05 20:17:07 -0500

    Do conversion in a thread to not block UI thread. takes about 4 seconds on my machine. Add shutdown method to TradeStatisticsConverter and call it via TradeStatisticsManager. Now as we have a reference to TradeStatisticsConverter in we don't need the hack anymore in applyInjector. Set log level for com.neemre.btcdcli4j.core.client.ClientConfigurator to error as there is an expected warn log because of the outdated version.

--- core/src/main/java/bisq/core/app/BisqExecutable.java
+++ core/src/main/java/bisq/core/app/BisqExecutable.java
@@ -25,7 +25,7 @@ import bisq.core.offer.OpenOfferManager;
 import bisq.core.setup.CorePersistedDataHost;
 import bisq.core.setup.CoreSetup;
 import bisq.core.support.dispute.arbitration.arbitrator.ArbitratorManager;
-import bisq.core.trade.statistics.TradeStatisticsConverter;
+import bisq.core.trade.statistics.TradeStatisticsManager;
 import bisq.core.trade.txproof.xmr.XmrTxProofService;
 
 import bisq.network.p2p.P2PService;
@@ -155,10 +155,6 @@ public abstract class BisqExecutable implements GracefulShutDownHandler, BisqSet
 
     protected void applyInjector() {
         // Subclasses might configure classes with the injector here
-
-        // As TradeStatisticsConverter is not used by any other class we need to enforce that guice is creating it by
-        // requesting an instance here.
-        injector.getInstance(TradeStatisticsConverter.class);
     }
 
     protected void readAllPersisted(Runnable completeHandler) {
@@ -225,6 +221,7 @@ public abstract class BisqExecutable implements GracefulShutDownHandler, BisqSet
 
         try {
             injector.getInstance(ArbitratorManager.class).shutDown();
+            injector.getInstance(TradeStatisticsManager.class).shutDown();
             injector.getInstance(XmrTxProofService.class).shutDown();
             injector.getInstance(DaoSetup.class).shutDown();
             injector.getInstance(AvoidStandbyModeService.class).shutDown();
