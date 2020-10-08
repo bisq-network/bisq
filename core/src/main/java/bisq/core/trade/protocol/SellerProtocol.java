18a27e90676c91e3b107355e1d85378a56981069
chimp1984 <chimp1984@gmx.com>
2020-10-08 13:00:31 -0500

    Republish trade statistics from seller side if peer capability is know. This is not the case without getting PR #4609 merges as well. We only do it for 2 weeks after planned release time as then it can be assumed that enough nodes have updated that the normal publishing will distribute the object sufficiently.

--- core/src/main/java/bisq/core/trade/protocol/SellerProtocol.java
+++ core/src/main/java/bisq/core/trade/protocol/SellerProtocol.java
@@ -39,17 +39,43 @@ import bisq.network.p2p.NodeAddress;
 
 import bisq.common.handlers.ErrorMessageHandler;
 import bisq.common.handlers.ResultHandler;
+import bisq.common.util.Utilities;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class SellerProtocol extends DisputeProtocol {
     enum SellerEvent implements FluentProtocol.Event {
+        STARTUP,
         PAYMENT_RECEIVED
     }
 
     public SellerProtocol(SellerTrade trade) {
         super(trade);
+    }
+
+    @Override
+    protected void onInitialized() {
+        super.onInitialized();
+
+        // We get called the constructor with any possible state and phase. As we don't want to log an error for such
+        // cases we use the alternative 'given' method instead of 'expect'.
+
+        // We only re-publish for about 2 weeks after 1.4.0 release until most nodes have updated to
+        // achieve sufficient resilience.
+        boolean currentDateBeforeCutOffDate = new Date().before(Utilities.getUTCDate(2020, GregorianCalendar.NOVEMBER, 1));
+        given(anyPhase(Trade.Phase.DEPOSIT_PUBLISHED,
+                Trade.Phase.DEPOSIT_CONFIRMED,
+                Trade.Phase.FIAT_SENT,
+                Trade.Phase.FIAT_RECEIVED,
+                Trade.Phase.PAYOUT_PUBLISHED)
+                .with(SellerEvent.STARTUP)
+                .preCondition(currentDateBeforeCutOffDate))
+                .setup(tasks(SellerPublishesTradeStatistics.class))
+                .executeTasks();
     }
 
 
