5fc50630e1f3bc57b318f6ecce212e4e113b5e96
ghubstan <36207203+ghubstan@users.noreply.github.com>
2020-10-15 16:53:58 -0300

    Reformat, tidy up OfferUtil

--- core/src/main/java/bisq/core/offer/OfferUtil.java
+++ core/src/main/java/bisq/core/offer/OfferUtil.java
@@ -39,7 +39,6 @@ import bisq.core.util.coin.CoinUtil;
 import bisq.network.p2p.P2PService;
 
 import bisq.common.app.Capabilities;
-import bisq.common.util.MathUtils;
 
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.utils.Fiat;
@@ -55,15 +54,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Nullable;
 
+import static bisq.common.util.MathUtils.roundDoubleToLong;
+import static bisq.common.util.MathUtils.scaleUpByPowerOf10;
+import static bisq.core.offer.OfferPayload.*;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * This class holds utility methods for the creation of an Offer.
- * Most of these are extracted here because they are used both in the GUI and in the API.
- * <p>
- * Long-term there could be a GUI-agnostic OfferService which provides these and other functionality to both the
- * GUI and the API.
+ * This class holds utility methods for creating and taking an Offer.
  */
 @Slf4j
 @Singleton
@@ -98,17 +96,19 @@ public class OfferUtil {
      * Given the direction, is this a BUY?
      *
      * @param direction the offer direction
-     * @return {@code true} for an offer to buy BTC from the taker, {@code false} for an offer to sell BTC to the taker
+     * @return {@code true} for an offer to buy BTC from the taker, {@code false} for an
+     * offer to sell BTC to the taker
      */
-    public boolean isBuyOffer(OfferPayload.Direction direction) {
-        return direction == OfferPayload.Direction.BUY;
+    public boolean isBuyOffer(Direction direction) {
+        return direction == Direction.BUY;
     }
 
     /**
      * Returns the makerFee as Coin, this can be priced in BTC or BSQ.
      *
      * @param amount           the amount of BTC to trade
-     * @return the maker fee for the given trade amount, or {@code null} if the amount is {@code null}
+     * @return the maker fee for the given trade amount, or {@code null} if the amount
+     * is {@code null}
      */
     @Nullable
     public Coin getMakerFee(@Nullable Coin amount) {
@@ -117,11 +117,12 @@ public class OfferUtil {
     }
 
     /**
-     * Checks if the maker fee should be paid in BTC, this can be the case due to user preference or because the user
-     * doesn't have enough BSQ.
+     * Checks if the maker fee should be paid in BTC, this can be the case due to user
+     * preference or because the user doesn't have enough BSQ.
      *
      * @param amount           the amount of BTC to trade
-     * @return {@code true} if BTC is preferred or the trade amount is nonnull and there isn't enough BSQ for it
+     * @return {@code true} if BTC is preferred or the trade amount is nonnull and there
+     * isn't enough BSQ for it.
      */
     public boolean isCurrencyForMakerFeeBtc(@Nullable Coin amount) {
         boolean payFeeInBtc = preferences.getPayFeeInBtc();
@@ -139,8 +140,9 @@ public class OfferUtil {
         Coin availableBalance = bsqWalletService.getAvailableConfirmedBalance();
         Coin makerFee = CoinUtil.getMakerFee(false, amount);
 
-        // If we don't know yet the maker fee (amount is not set) we return true, otherwise we would disable BSQ
-        // fee each time we open the create offer screen as there the amount is not set.
+        // If we don't know yet the maker fee (amount is not set) we return true,
+        // otherwise we would disable BSQ fee each time we open the create offer screen
+        // as there the amount is not set.
         if (makerFee == null)
             return true;
 
@@ -172,8 +174,9 @@ public class OfferUtil {
         Coin availableBalance = bsqWalletService.getAvailableConfirmedBalance();
         Coin takerFee = getTakerFee(false, amount);
 
-        // If we don't know yet the maker fee (amount is not set) we return true, otherwise we would disable BSQ
-        // fee each time we open the create offer screen as there the amount is not set.
+        // If we don't know yet the maker fee (amount is not set) we return true,
+        // otherwise we would disable BSQ fee each time we open the create offer screen
+        // as there the amount is not set.
         if (takerFee == null)
             return true;
 
@@ -184,8 +187,9 @@ public class OfferUtil {
         return !availableBalance.subtract(takerFee).isNegative();
     }
 
-    public Optional<Volume> getFeeInUserFiatCurrency(Coin makerFee, boolean isCurrencyForMakerFeeBtc,
-                                                            CoinFormatter bsqFormatter) {
+    public Optional<Volume> getFeeInUserFiatCurrency(Coin makerFee,
+                                                     boolean isCurrencyForMakerFeeBtc,
+                                                     CoinFormatter bsqFormatter) {
         String countryCode = preferences.getUserCountry().code;
         String userCurrencyCode = CurrencyUtil.getCurrencyByCountryCode(countryCode).getCode();
         return getFeeInUserFiatCurrency(makerFee,
@@ -194,15 +198,16 @@ public class OfferUtil {
                 bsqFormatter);
     }
 
-    private Optional<Volume> getFeeInUserFiatCurrency(Coin makerFee, boolean isCurrencyForMakerFeeBtc,
-                                                             String userCurrencyCode,
-                                                             CoinFormatter bsqFormatter) {
-        // We use the users currency derived from his selected country.
-        // We don't use the preferredTradeCurrency from preferences as that can be also set to an altcoin.
-
+    private Optional<Volume> getFeeInUserFiatCurrency(Coin makerFee,
+                                                      boolean isCurrencyForMakerFeeBtc,
+                                                      String userCurrencyCode,
+                                                      CoinFormatter bsqFormatter) {
+        // We use the users currency derived from his selected country.  We don't use the
+        // preferredTradeCurrency from preferences as that can be also set to an altcoin.
         MarketPrice marketPrice = priceFeedService.getMarketPrice(userCurrencyCode);
         if (marketPrice != null && makerFee != null) {
-            long marketPriceAsLong = MathUtils.roundDoubleToLong(MathUtils.scaleUpByPowerOf10(marketPrice.getPrice(), Fiat.SMALLEST_UNIT_EXPONENT));
+            long marketPriceAsLong = roundDoubleToLong(
+                    scaleUpByPowerOf10(marketPrice.getPrice(), Fiat.SMALLEST_UNIT_EXPONENT));
             Price userCurrencyPrice = Price.valueOf(userCurrencyCode, marketPriceAsLong);
 
             if (isCurrencyForMakerFeeBtc) {
@@ -224,41 +229,41 @@ public class OfferUtil {
         }
     }
 
-
     public Map<String, String> getExtraDataMap(PaymentAccount paymentAccount,
-                                                      String currencyCode,
-                                                      OfferPayload.Direction direction) {
+                                               String currencyCode,
+                                               Direction direction) {
         Map<String, String> extraDataMap = new HashMap<>();
         if (CurrencyUtil.isFiatCurrency(currencyCode)) {
-            String myWitnessHashAsHex = accountAgeWitnessService.getMyWitnessHashAsHex(paymentAccount.getPaymentAccountPayload());
-            extraDataMap.put(OfferPayload.ACCOUNT_AGE_WITNESS_HASH, myWitnessHashAsHex);
+            String myWitnessHashAsHex = accountAgeWitnessService
+                    .getMyWitnessHashAsHex(paymentAccount.getPaymentAccountPayload());
+            extraDataMap.put(ACCOUNT_AGE_WITNESS_HASH, myWitnessHashAsHex);
         }
 
         if (referralIdService.getOptionalReferralId().isPresent()) {
-            extraDataMap.put(OfferPayload.REFERRAL_ID, referralIdService.getOptionalReferralId().get());
+            extraDataMap.put(REFERRAL_ID, referralIdService.getOptionalReferralId().get());
         }
 
         if (paymentAccount instanceof F2FAccount) {
-            extraDataMap.put(OfferPayload.F2F_CITY, ((F2FAccount) paymentAccount).getCity());
-            extraDataMap.put(OfferPayload.F2F_EXTRA_INFO, ((F2FAccount) paymentAccount).getExtraInfo());
+            extraDataMap.put(F2F_CITY, ((F2FAccount) paymentAccount).getCity());
+            extraDataMap.put(F2F_EXTRA_INFO, ((F2FAccount) paymentAccount).getExtraInfo());
         }
 
-        extraDataMap.put(OfferPayload.CAPABILITIES, Capabilities.app.toStringList());
+        extraDataMap.put(CAPABILITIES, Capabilities.app.toStringList());
 
-        if (currencyCode.equals("XMR") && direction == OfferPayload.Direction.SELL) {
+        if (currencyCode.equals("XMR") && direction == Direction.SELL) {
             preferences.getAutoConfirmSettingsList().stream()
                     .filter(e -> e.getCurrencyCode().equals("XMR"))
                     .filter(AutoConfirmSettings::isEnabled)
-                    .forEach(e -> extraDataMap.put(OfferPayload.XMR_AUTO_CONF, OfferPayload.XMR_AUTO_CONF_ENABLED_VALUE));
+                    .forEach(e -> extraDataMap.put(XMR_AUTO_CONF, XMR_AUTO_CONF_ENABLED_VALUE));
         }
 
         return extraDataMap.isEmpty() ? null : extraDataMap;
     }
 
     public void validateOfferData(double buyerSecurityDeposit,
-                                         PaymentAccount paymentAccount,
-                                         String currencyCode,
-                                         Coin makerFeeAsCoin) {
+                                  PaymentAccount paymentAccount,
+                                  String currencyCode,
+                                  Coin makerFeeAsCoin) {
         checkNotNull(makerFeeAsCoin, "makerFee must not be null");
         checkNotNull(p2PService.getAddress(), "Address must not be null");
         checkArgument(buyerSecurityDeposit <= Restrictions.getMaxBuyerSecurityDepositAsPercent(),
@@ -272,14 +277,4 @@ public class OfferUtil {
         checkArgument(!filterManager.isPaymentMethodBanned(paymentAccount.getPaymentMethod()),
                 Res.get("offerbook.warning.paymentMethodBanned"));
     }
-
-    // TODO no code duplication found in UI code (added for API)
-   /* public static Coin getFundsNeededForOffer(Coin tradeAmount, Coin buyerSecurityDeposit, OfferPayload.Direction direction) {
-        boolean buyOffer = isBuyOffer(direction);
-        Coin needed = buyOffer ? buyerSecurityDeposit : Restrictions.getSellerSecurityDeposit();
-        if (!buyOffer)
-            needed = needed.add(tradeAmount);
-
-        return needed;
-    }*/
 }
