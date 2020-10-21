82da7391343336910ad25dcc494a94ed12c35951
chimp1984 <chimp1984@gmx.com>
2020-10-21 15:37:48 -0500

    Add missing import (seems got lost in merge)

--- core/src/main/java/bisq/core/api/CoreOffersService.java
+++ core/src/main/java/bisq/core/api/CoreOffersService.java
@@ -47,6 +47,7 @@ import static bisq.common.util.MathUtils.scaleUpByPowerOf10;
 import static bisq.core.locale.CurrencyUtil.isCryptoCurrency;
 import static bisq.core.offer.OfferPayload.Direction;
 import static bisq.core.offer.OfferPayload.Direction.BUY;
+import static java.lang.String.format;
 
 @Slf4j
 class CoreOffersService {
