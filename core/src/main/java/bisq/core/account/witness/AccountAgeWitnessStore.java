f8aa1afcc8ce44de3c679a2ead5fb8710bd35c5f
chimp1984 <chimp1984@gmx.com>
2020-10-22 12:08:03 -0500

    Make constructor public

--- core/src/main/java/bisq/core/account/witness/AccountAgeWitnessStore.java
+++ core/src/main/java/bisq/core/account/witness/AccountAgeWitnessStore.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AccountAgeWitnessStore extends PersistableNetworkPayloadStore<AccountAgeWitness> {
 
-    AccountAgeWitnessStore() {
+    public AccountAgeWitnessStore() {
     }
 
 
