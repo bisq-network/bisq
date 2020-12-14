e76a801b0b97ef85ec1ca8e807b580623a60facb
ghubstan <36207203+ghubstan@users.noreply.github.com>
2020-12-14 11:42:44 -0300

    Adjust create TransferwiseAccount test
    As per commit 88f26f93241af698ae689bf081205d0f9dc929fa,
    do not autofill all currencies by default but keep all unselected.

--- apitest/src/test/java/bisq/apitest/method/payment/CreatePaymentAccountTest.java
+++ apitest/src/test/java/bisq/apitest/method/payment/CreatePaymentAccountTest.java
@@ -746,7 +746,10 @@ public class CreatePaymentAccountTest extends AbstractPaymentAccountTest {
         String jsonString = getCompletedFormAsJsonString();
         TransferwiseAccount paymentAccount = (TransferwiseAccount) createPaymentAccount(alicedaemon, jsonString);
         verifyUserPayloadHasPaymentAccountWithId(paymentAccount.getId());
-        verifyAccountTradeCurrencies(getAllTransferwiseCurrencies(), paymentAccount);
+        // As per commit 88f26f93241af698ae689bf081205d0f9dc929fa
+        // Do not autofill all currencies by default but keep all unselected.
+        // verifyAccountTradeCurrencies(getAllTransferwiseCurrencies(), paymentAccount);
+        assertEquals(0, paymentAccount.getTradeCurrencies().size());
         verifyCommonFormEntries(paymentAccount);
         assertEquals(COMPLETED_FORM_MAP.get(PROPERTY_NAME_EMAIL), paymentAccount.getEmail());
         log.debug("Deserialized {}: {}", paymentAccount.getClass().getSimpleName(), paymentAccount);
