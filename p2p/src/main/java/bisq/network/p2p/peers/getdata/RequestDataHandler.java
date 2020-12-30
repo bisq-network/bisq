b79ef8b61d4cc2ac896f1e8de46b44c56dd05d18
chimp1984 <chimp1984@gmx.com>
2020-12-30 03:28:05 -0500

    Increase timeouts

--- p2p/src/main/java/bisq/network/p2p/peers/getdata/RequestDataHandler.java
+++ p2p/src/main/java/bisq/network/p2p/peers/getdata/RequestDataHandler.java
@@ -53,7 +53,7 @@ import org.jetbrains.annotations.Nullable;
 
 @Slf4j
 class RequestDataHandler implements MessageListener {
-    private static final long TIMEOUT = 180;
+    private static final long TIMEOUT = 300;
 
     private NodeAddress peersNodeAddress;
     private String getDataRequestType;
