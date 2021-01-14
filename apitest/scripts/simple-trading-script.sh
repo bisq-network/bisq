d756d3dcf377418daf1523bf095921c26e56053a
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-01-14 19:15:16 -0300

    Add simple api trading script
    Emulates Bob and Alice trading with each other with regtest BTC.

---
+++ apitest/scripts/simple-trading-script.sh
@@ -0,0 +1,209 @@
+#! /bin/bash
+
+# Script for running simple fiat <-> btc trading scenarios using the API CLI on regtest.
+#
+# Prerequisites:
+#
+#  - Linux or OSX with bash, Java 10, or Java 11-12 (JDK language compatibility 10).
+#
+#  - Bisq must be fully built with apitest dao setup files installed.
+#    Build command:  `./gradlew clean build :apitest:installDaoSetup`
+#
+#  - All supporting nodes must be run locally, in dev/dao/regtest mode:
+#           bitcoind, seednode, arbdaemon, alicedaemon, bobdaemon
+#
+#    These should be run using the apitest harness.  From the root project dir, run:
+#    `./bisq-apitest --apiPassword=xyz --supportingApps=bitcoind,seednode,arbdaemon,alicedaemon,bobdaemon --shutdownAfterTests=false`
+#
+#  - Only regtest btc can be bought or sold with the default dummy eur account.
+#    The CLI supports creating other fiat payment accts but this script doesn't (todo).
+#
+# Usage:
+#
+#  This script must be run from the root of the project, e.g.:
+#
+#     `apitest/scripts/simple-trading-script.sh -d buy -c eur -m 3.00 -a 0.125`
+#
+#  Script options:  -d <direction> -c <currency-code> -m <mkt-price-margin(%)> - f <fixed-price> -a <amount(btc)>
+#
+# Examples:
+#
+#    Create a buy/eur offer to buy 0.125 btc at a mkt-price-margin of 0%:
+#
+#       `apitest/scripts/simple-trading-script.sh -d buy -c usd -m 0.00 -a 0.125`
+#
+#    Create a sell/eur offer to sell 0.125 btc at a fixed-price of 23,000 euros:
+#
+#       `apitest/scripts/simple-trading-script.sh -d sell -c usd -f 23000 -a 0.125`
+
+APP_BASE_NAME=`basename "$0"`
+APP_HOME="`pwd -P`"
+APITEST_SCRIPTS_HOME="${APP_HOME}/apitest/scripts"
+
+# Source the env and some helper functions.
+. ${APITEST_SCRIPTS_HOME}/trading-script-env.sh
+
+checksetup
+parseopts "$@"
+
+printdate "Started ${APP_BASE_NAME} with parameters:"
+printscriptparams
+
+registerdisputeagents
+
+# Get balances.
+printdate "Bob & Alice's balances before trade:"
+printdate_sameline "ALICE CLI:"
+printbalances $ALICE_PORT
+printbreak
+
+printdate_sameline "BOB CLI:"
+printbalances $BOB_PORT
+printbreak
+
+printdate_sameline "ALICE CLI:"
+getpaymentaccts ${ALICE_PORT}
+
+ALICE_ACCT_ID=$(getdummyacctid ${ALICE_PORT})
+printdate "ALICE ${ALICE_ROLE}: Fiat Acct ID: ${ALICE_ACCT_ID}"
+printbreak
+
+
+printdate_sameline "BOB CLI:"
+getpaymentaccts ${BOB_PORT}
+
+BOB_ACCT_ID=$(getdummyacctid ${BOB_PORT})
+printdate "Bob's Fiat Acct ID: ${BOB_ACCT_ID}"
+printbreak
+
+printdate "ALICE ${ALICE_ROLE}:  Creating ${DIRECTION} ${CURRENCY_CODE} offer with payment acct ${ALICE_ACCT_ID}."
+CMD="$CLI_BASE --port=${ALICE_PORT} createoffer"
+CMD+=" --payment-account=${ALICE_ACCT_ID}"
+CMD+=" --direction=${DIRECTION}"
+CMD+=" --currency-code=${CURRENCY_CODE}"
+CMD+=" --amount=${AMOUNT}"
+if [ -z "${MKT_PRICE_MARGIN}" ]; then
+    CMD+=" --fixed-price=${FIXED_PRICE}"
+else
+    CMD+=" --market-price-margin=${MKT_PRICE_MARGIN}"
+fi
+CMD+=" --security-deposit=15.0"
+CMD+=" --fee-currency=BSQ"
+printdate_sameline "ALICE CLI:"
+printcmd "$CMD"
+
+OFFER_ID=$(createoffer "${CMD}")
+printdate "ALICE ${ALICE_ROLE}:  Created offer with id: ${OFFER_ID}."
+printbreak
+sleeptraced 10
+
+# Generate some btc blocks.
+printdate "Generating btc blocks after publishing Alice's offer."
+genbtcblocks 3 5
+printbreak
+sleeptraced 10
+
+# List offers.
+printdate "BOB ${BOB_ROLE}:  Looking at ${DIRECTION} ${CURRENCY_CODE} offers."
+CMD="$CLI_BASE --port=${BOB_PORT} getoffers --direction=${DIRECTION} --currency-code=${CURRENCY_CODE}"
+printdate_sameline "BOB CLI:"
+printcmd "$CMD"
+OFFERS=$($CMD)
+echo "${OFFERS}"
+printbreak
+sleeptraced 3
+
+# Take offer.
+printdate "BOB ${BOB_ROLE}:  Taking offer ${OFFER_ID} with payment acct ${BOB_ACCT_ID}."
+CMD="$CLI_BASE --port=${BOB_PORT} takeoffer --offer-id=${OFFER_ID} --payment-account=${BOB_ACCT_ID} --fee-currency=bsq"
+printdate_sameline "BOB CLI:"
+printcmd "$CMD"
+TRADE=$($CMD)
+echo "${TRADE}"
+printbreak
+sleeptraced 10
+
+# Generating some btc blocks
+printdate "Generating btc blocks after Bob takes Alice's offer."
+genbtcblocks 3 3
+printbreak
+sleeptraced 6
+
+# Send payment sent and received messages.
+if [ $DIRECTION = "BUY" ]
+then
+    PAYER="ALICE ${ALICE_ROLE}"
+    PAYER_PORT=${ALICE_PORT}
+    PAYER_CLI="ALICE CLI"
+    PAYEE="BOB ${BOB_ROLE}"
+    PAYEE_PORT=${BOB_PORT}
+    PAYEE_CLI="BOB CLI"
+else
+    PAYER="BOB ${BOB_ROLE}"
+    PAYER_PORT=${BOB_PORT}
+    PAYER_CLI="BOB CLI"
+    PAYEE="ALICE ${ALICE_ROLE}"
+    PAYEE_PORT=${ALICE_PORT}
+    PAYEE_CLI="ALICE CLI"
+fi
+
+# Confirm payment started.
+printdate "${PAYER}:  Sending fiat payment sent msg."
+CMD="$CLI_BASE --port=${PAYER_PORT} confirmpaymentstarted --trade-id=${OFFER_ID}"
+printdate_sameline "${PAYER_CLI}:"
+printcmd "$CMD"
+SENT_MSG=$($CMD)
+printdate "${SENT_MSG}"
+printbreak
+
+sleeptraced 2
+printdate "Generating btc blocks after fiat payment sent msg."
+genbtcblocks 3 5
+sleeptraced 2
+
+# Confirm payment received.
+printdate "${PAYEE}:  Sending fiat payment received msg."
+CMD="$CLI_BASE --port=${PAYEE_PORT} confirmpaymentreceived --trade-id=${OFFER_ID}"
+printdate_sameline "${PAYEE_CLI}:"
+printcmd "$CMD"
+RCVD_MSG=$($CMD)
+printdate "${RCVD_MSG}"
+printbreak
+sleeptraced 4
+
+
+# Generate some btc blocks
+printdate "Generating btc blocks after fiat transfer."
+genbtcblocks 3 5
+printbreak
+sleeptraced 3
+
+# Complete the trade on the seller side.
+if [ $DIRECTION = "BUY" ]
+then
+	printdate "BOB ${BOB_ROLE}:  Closing trade by keeping funds in Bisq wallet."
+    CMD="$CLI_BASE --port=${BOB_PORT} keepfunds --trade-id=${OFFER_ID}"
+    printdate_sameline "BOB CLI:"
+    printcmd "$CMD"
+else
+	printdate "ALICE (taker):  Closing trade by keeping funds in Bisq wallet."
+    CMD="$CLI_BASE --port=${ALICE_PORT} keepfunds --trade-id=${OFFER_ID}"
+    printdate_sameline "ALICE CLI:"
+    printcmd "$CMD"
+fi
+KEEP_FUNDS_MSG=$($CMD)
+printdate "${KEEP_FUNDS_MSG}"
+sleeptraced 5
+printbreak
+
+
+# Get balances after trade completion.
+printdate "Bob & Alice's balances after trade:"
+printdate_sameline "ALICE CLI:"
+printbalances $ALICE_PORT
+printbreak
+printdate_sameline "BOB CLI:"
+printbalances $BOB_PORT
+printbreak
+
+exit 0
---
+++ apitest/scripts/trading-script-env.sh
@@ -0,0 +1,256 @@
+#! /bin/bash
+
+# This file must be sourced by the main driver.
+
+export CLI_BASE="./bisq-cli --password=xyz"
+export ALICE_PORT=9998
+export BOB_PORT=9999
+
+printdate() {
+	echo "[`date`]  $@"
+}
+
+printdate "Started ${APP_HOME}/${APP_BASE_NAME}."
+
+checksetup() {
+    apitestusage() {
+        echo "The apitest harness must be running a local bitcoin regtest node, a seednode, arbitration node, and Bob & Alice daemons."
+        echo ""
+        echo "From the project's root dir, start all supporting nodes from a terminal:"
+        echo "./bisq-apitest --apiPassword=xyz --supportingApps=bitcoind,seednode,arbdaemon,alicedaemon,bobdaemon  --shutdownAfterTests=false"
+        echo ""
+        echo "Register dispute agents in the arbitration daemon after it initializes."
+        echo "./bisq-cli --password=xyz --port=9997 registerdisputeagent --dispute-agent-type=mediator \
+#           --registration-key=6ac43ea1df2a290c1c8391736aa42e4339c5cb4f110ff0257a13b63211977b7a"
+        echo "./bisq-cli --password=xyz --port=9997 registerdisputeagent --dispute-agent-type=refundagent \
+#           --registration-key=6ac43ea1df2a290c1c8391736aa42e4339c5cb4f110ff0257a13b63211977b7a"
+        exit 1;
+    }
+    printdate "Checking ${APP_HOME} for some expected directories and files."
+    if [ -d "${APP_HOME}/apitest" ]; then
+        printdate "Subproject apitest exists.";
+    else
+        printdate "Error:  Subproject apitest not found, maybe because you are not running the script from the project root dir."
+        exit 1
+    fi
+    if [ -f "${APP_HOME}/bisq-cli" ]; then
+        printdate "The bisq-cli script exists.";
+    else
+        printdate "Error:  The bisq-cli script not found, maybe because you are not running the script from the project root dir."
+        exit 1
+    fi
+    printdate "Checking to see local bitcoind is running."
+    checkbitcoindrunning
+    printdate "Checking to see bisq servers are running."
+    if pgrep -f "bisq.seednode.SeedNodeMain" > /dev/null ; then
+        printdate "The seednode is running on host."
+    else
+        printdate "Error:  seed is not running on host, exiting."
+        apitestusage
+    fi
+    if pgrep -f "bisq.daemon.app.BisqDaemonMain --appName=bisq-BTC_REGTEST_Arb_dao" > /dev/null ; then
+        printdate "The arbitration node is running on host."
+    else
+        printdate "Error:  arbitration node is not running on host, exiting."
+        apitestusage
+    fi
+    if pgrep -f "bisq.daemon.app.BisqDaemonMain --appName=bisq-BTC_REGTEST_Alice_dao" > /dev/null ; then
+        printdate "Alice's daemon node is running on host."
+    else
+        printdate "Error:  Alice's daemon node is not running on host, exiting."
+        apitestusage
+    fi
+    if pgrep -f "bisq.daemon.app.BisqDaemonMain --appName=bisq-BTC_REGTEST_Bob_dao" > /dev/null ; then
+        printdate "Bob's daemon node is running on host."
+    else
+        printdate "Error:  Bob's daemon node is not running on host, exiting."
+        apitestusage
+    fi
+}
+
+parseopts() {
+	usage() {
+		echo "Usage: $0 [-d buy|sell] [-c <currency-code>] [-f <fixed-price> || -m <margin-from-price>] [-a <amount in btc>]" 1>&2
+		exit 1;
+	}
+
+	local OPTIND o d c f m a
+	while getopts "d:c:f:m:a:" o; do
+		case "${o}" in
+		    d) d=$(echo ${OPTARG} | tr '[:lower:]' '[:upper:]')
+		        ((d == "BUY" || d == "SELL")) || usage
+		        export DIRECTION=${d}
+		        ;;
+		    c) c=$(echo ${OPTARG} | tr '[:lower:]' '[:upper:]')
+		        export CURRENCY_CODE=${c}
+		    	;;
+		    f) f=${OPTARG}
+			   export FIXED_PRICE=${f}
+		       ;;
+		    m) m=${OPTARG}
+			   export MKT_PRICE_MARGIN=${m}
+		       ;;
+		    a) a=${OPTARG}
+		       export AMOUNT=${a}
+		       ;;
+		    *) usage ;;
+		esac
+	done
+	shift $((OPTIND-1))
+
+	if [ -z "${d}" ] || [ -z "${c}" ] || [ -z "${a}" ]; then
+		usage
+	fi
+
+	if [ -z "${f}" ] && [ -z "${m}" ]; then
+		usage
+	fi
+
+	if [ -n "${f}" ] && [ -n "${m}" ]; then
+		printdate "Must use margin-from-price param (-m) or fixed-price param (-f), not both."
+		usage
+	fi
+
+    if [ $DIRECTION = "SELL" ]
+    then
+        export BOB_ROLE="Bob (taker/buyer)"
+        export ALICE_ROLE="Alice (maker/seller)"
+    else
+        export BOB_ROLE="Bob (taker/seller)"
+        export ALICE_ROLE="Alice (maker/buyer)"
+    fi
+}
+
+printscriptparams() {
+	echo "	DIRECTION = ${DIRECTION}"
+	echo "	CURRENCY_CODE = ${CURRENCY_CODE}"
+	echo "	FIXED_PRICE = ${FIXED_PRICE}"
+	echo "	MKT_PRICE_MARGIN = ${MKT_PRICE_MARGIN}"
+	echo "	AMOUNT = ${AMOUNT}"
+	echo "	BOB_ROLE = ${BOB_ROLE}"
+	echo "	ALICE_ROLE = ${ALICE_ROLE}"
+}
+
+checkbitcoindrunning() {
+    # There may be a '+' char in the path and we have to escape it for pgrep.
+    if [[ ${APP_HOME} == *"+"* ]]; then
+        ESCAPED_APP_HOME=$(escapepluschar ${APP_HOME})
+    else
+        ESCAPED_APP_HOME=${APP_HOME}
+    fi
+    if pgrep -f "bitcoind -datadir=${ESCAPED_APP_HOME}/apitest/build/resources/main/Bitcoin-regtest" > /dev/null ; then
+        printdate "The regtest bitcoind node is running on host."
+    else
+        printdate "Error:  regtest bitcoind node is not running on host, exiting."
+        apitestusage
+    fi
+}
+
+registerdisputeagents() {
+    # Silently register dev dispute agents.  It's easy to forget.
+    REG_KEY="6ac43ea1df2a290c1c8391736aa42e4339c5cb4f110ff0257a13b63211977b7a"
+    SILENT=$(./bisq-cli --password=xyz --port=9997 registerdisputeagent --dispute-agent-type=mediator --registration-key=${REG_KEY})
+    SILENT=$(./bisq-cli --password=xyz --port=9997 registerdisputeagent --dispute-agent-type=refundagent --registration-key=${REG_KEY})
+}
+
+
+printbreak() {
+	echo ""
+	echo ""
+}
+
+printcmd() {
+	echo -en "$@\n"
+}
+
+printdate_sameline() {
+	echo -n "[`date`]  $@ "
+}
+
+sleeptraced() {
+  PERIOD=$1
+  printdate "sleeping for $PERIOD"
+  sleep $PERIOD
+}
+
+printbalances() {
+    PORT=$1
+    printcmd "${CLI_BASE} --port=${PORT} getbalance"
+    $CLI_BASE --port=${PORT} getbalance
+}
+
+getpaymentaccts() {
+    PORT=$1
+    printcmd "${CLI_BASE} --port=${PORT} getpaymentaccts"
+    $CLI_BASE --port=${PORT} getpaymentaccts
+}
+
+getdummyacctid() {
+	PORT=$1
+	PAYMENT_ACCTS=$(${CLI_BASE} --port=${PORT} getpaymentaccts)
+	DUMMY_ACCT_1=$(echo -e "${PAYMENT_ACCTS}" | sed -n '2p')
+	DUMMY_ACCT_2=$(echo -e "${PAYMENT_ACCTS}" | sed -n '3p')
+	if [[ "$DUMMY_ACCT_1=" == *"PerfectMoney dummy"* ]]; then
+		DUMMY_ACCT=$DUMMY_ACCT_1
+	else
+		DUMMY_ACCT=$DUMMY_ACCT_2
+	fi
+	ACCT_ID=$(echo -e $DUMMY_ACCT | awk '{print $NF}')
+	echo "${ACCT_ID}"
+}
+
+createoffer() {
+ 	CREATE_OFFER_CMD=$1
+    OFFER_DESC=$($CREATE_OFFER_CMD)
+
+    ### This is an effort to handling any createoffer error.  If error, should echo Error, and the calling script
+    ### should sleep awhile then exit 1.
+	if [[ "$OFFER_DESC" != "Buy/Sell"* ]]; then
+	    echo "=========================================================="
+        echo "Error: ${OFFER_DESC}"
+        echo "=========================================================="
+        sleep 5
+    fi
+
+	OFFER_DETAIL=$(echo -e "${OFFER_DESC}" | sed -n '2p')
+	NEW_OFFER_ID=$(echo -e ${OFFER_DETAIL} | awk '{print $NF}')
+	echo "${NEW_OFFER_ID}"
+}
+
+genbtcblocks() {
+	NUM_BLOCKS=$1
+	SECONDS_BETWEEN_BLOCKS=$2
+	for i in $(seq -f "%02g" 1 ${NUM_BLOCKS})
+	do
+        genbtcblock
+        sleep ${SECONDS_BETWEEN_BLOCKS}
+	done
+}
+
+genbtcblock() {
+    # TODO use bitcoin-cli to get new address; cannot assume same address exists on other regtest bitcoin-core hosts.
+    CMD='bitcoin-cli -regtest -rpcport=19443 -rpcuser=apitest -rpcpassword=apitest generatetoaddress 1 "2N5J6MyjAsWnashimGiNwoRzUXThsQzRmbv"'
+    printcmd "$CMD"
+    bitcoin-cli -regtest -rpcport=19443 -rpcuser=apitest -rpcpassword=apitest generatetoaddress 1 "2N5J6MyjAsWnashimGiNwoRzUXThsQzRmbv"
+}
+
+escapepluschar() {
+ 	STRING=$1
+	NEW_STRING=$(sed 's/+/\\&/g' <<< ${STRING})
+    echo "${NEW_STRING}"
+}
+
+# Keep this in case there is a need to ready user input from stdin.
+readYesOrNo() {
+	question=$1
+	echo -n "$question  Yes or No: "
+	read answer
+	answer=`echo $answer | tr [a-z] [A-Z]`
+	if [ $answer = Y ]
+	then
+		echo You answered yes: $answer
+	else
+		echo You answered no: $answer
+	fi
+	return 0
+}
