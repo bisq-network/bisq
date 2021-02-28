978743774b4d614c429e0aa1912591115fbb2797
forcryptos <79342318+forcryptos@users.noreply.github.com>
2021-02-27 22:20:57 -0500

    Update StringUtils.java

--- common/src/main/java/bisq/common/util/StringUtils.java
+++ common/src/main/java/bisq/common/util/StringUtils.java
@@ -3,7 +3,7 @@ package bisq.common.util;
 public class StringUtils {
 
     public static String truncatePromptString(String str, int strMaxLen) {
-        if(str.length() <= strMaxLen){
+        if (str.length() <= strMaxLen) {
             return str;
         } else {
             return str.substring(0, strMaxLen - 3).concat("...");
