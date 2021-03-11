fb053e85ad38abfef7d0e95102ec8aff9d324984
Christoph Atteneder <christoph.atteneder@gmail.com>
2021-03-11 16:24:32 +0100

    Revert to SNAPSHOT version

--- desktop/package/macosx/create_app.sh
+++ desktop/package/macosx/create_app.sh
@@ -6,7 +6,7 @@ mkdir -p deploy
 
 set -e
 
-version="1.5.9"
+version="1.5.9-SNAPSHOT"
 
 cd ..
 ./gradlew :desktop:build -x test shadowJar
--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-version="1.5.9"
+version="1.5.9-SNAPSHOT"
 
 target_dir="releases/$version"
 
