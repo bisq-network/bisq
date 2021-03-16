99123b7297af2fa0dfa93e481396bd19593eca13
sqrrm <sqrrm1@gmail.com>
2021-03-16 13:07:42 +0100

    Lower tolerated small amount
    Split tolerated amount for placing offers with unsigned accounts
    and tolerated amount for taking offers.

--- core/src/main/java/bisq/core/offer/OfferRestrictions.java
+++ core/src/main/java/bisq/core/offer/OfferRestrictions.java
@@ -36,7 +36,11 @@ public class OfferRestrictions {
         return new Date().after(REQUIRE_UPDATE_DATE);
     }
 
-    public static Coin TOLERATED_SMALL_TRADE_AMOUNT = Coin.parseCoin("0.01");
+    // These values should generally be the same. However, to avoid voiding offers created with a higher limit
+    // the TOLERATED_SMALL_AMOUNT_PEER value can be set to the higher limit to allow those offers to be taken
+    // for a while.
+    public static Coin TOLERATED_SMALL_AMOUNT_SELF = Coin.parseCoin("0.0025");
+    public static Coin TOLERATED_SMALL_AMOUNT_PEER = Coin.parseCoin("0.01");
 
     static boolean hasOfferMandatoryCapability(Offer offer, Capability mandatoryCapability) {
         Map<String, String> extraDataMap = offer.getOfferPayload().getExtraDataMap();
