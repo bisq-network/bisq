05ea293595d2d77e6a818ad519ba4fb1b94f4b6c
Steven Barclay <stejbac@gmail.com>
2021-03-20 21:39:26 +0000

    Activate Segwit DAO hard fork 4 weeks after v1.6.0 release
    Push back the (2nd) DAO hard fork activation block height to 680300,
    which is 4 weeks after the planned 1.6.0 release around 2021/03/25.
    (Also push back the testnet block activation height to 1943000 - 2 weeks
    from now assuming an average block time of 10 minutes.)

--- core/src/main/java/bisq/core/dao/node/full/RpcService.java
+++ core/src/main/java/bisq/core/dao/node/full/RpcService.java
@@ -71,8 +71,8 @@ import org.jetbrains.annotations.NotNull;
  */
 @Slf4j
 public class RpcService {
-    private static final int ACTIVATE_HARD_FORK_2_HEIGHT_MAINNET = 672646;
-    private static final int ACTIVATE_HARD_FORK_2_HEIGHT_TESTNET = 1906689;
+    private static final int ACTIVATE_HARD_FORK_2_HEIGHT_MAINNET = 680300;
+    private static final int ACTIVATE_HARD_FORK_2_HEIGHT_TESTNET = 1943000;
     private static final int ACTIVATE_HARD_FORK_2_HEIGHT_REGTEST = 1;
     private static final Range<Integer> SUPPORTED_NODE_VERSION_RANGE = Range.closedOpen(180000, 210100);
 
