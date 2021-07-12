622f7e9adda51df60db0910ce79cf8898f633834
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-07-12 11:39:53 -0300

    Remove old TODO because relevant refactoring was approved

--- apitest/src/test/java/bisq/apitest/scenario/bot/BotClient.java
+++ apitest/src/test/java/bisq/apitest/scenario/bot/BotClient.java
@@ -39,11 +39,6 @@ import bisq.cli.GrpcClient;
 
 /**
  * Convenience GrpcClient wrapper for bots using gRPC services.
- *
- * TODO Consider if the duplication smell is bad enough to force a BotClient user
- *  to use the GrpcClient instead (and delete this class).  But right now, I think it is
- *  OK because moving some of the non-gRPC related methods to GrpcClient is even smellier.
- *
  */
 @SuppressWarnings({"JavaDoc", "unused"})
 @Slf4j
