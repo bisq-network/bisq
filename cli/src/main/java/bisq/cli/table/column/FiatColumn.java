30e9db8d67f2bbf19917219f324e607ed82a0c6c
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-09-21 13:05:53 -0300

    Add license banners and class level javadoc

--- cli/src/main/java/bisq/cli/table/column/FiatColumn.java
+++ cli/src/main/java/bisq/cli/table/column/FiatColumn.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bisq.cli.table.column;
 
 import java.util.stream.IntStream;
@@ -8,6 +25,10 @@ import static bisq.cli.table.column.Column.JUSTIFICATION.RIGHT;
 import static bisq.cli.table.column.FiatColumn.DISPLAY_MODE.PRICE;
 import static bisq.cli.table.column.FiatColumn.DISPLAY_MODE.TRIGGER_PRICE;
 
+/**
+ * For displaying fiat values as volume, price, or optional trigger price
+ * with appropriate precision.
+ */
 public class FiatColumn extends LongColumn {
 
     public enum DISPLAY_MODE {
--- cli/src/main/java/bisq/cli/table/column/ZippedStringColumns.java
+++ cli/src/main/java/bisq/cli/table/column/ZippedStringColumns.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bisq.cli.table.column;
 
 import java.util.ArrayList;
@@ -13,6 +30,10 @@ import static bisq.cli.table.column.ZippedStringColumns.DUPLICATION_MODE.INCLUDE
 
 import bisq.cli.table.column.Column.JUSTIFICATION;
 
+/**
+ * For zipping multiple StringColumns into a single StringColumn.
+ * Useful for displaying amount and volume range values.
+ */
 public class ZippedStringColumns {
 
     public enum DUPLICATION_MODE {
