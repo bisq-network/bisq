63f2d2520ed01680fe76623348d6088b0bad70a9
chimp1984 <chimp1984@gmx.com>
2021-10-08 15:59:49 +0200

    Add fee listener

--- core/src/main/java/bisq/core/offer/BsqSwapOfferManager.java
+++ core/src/main/java/bisq/core/offer/BsqSwapOfferManager.java
@@ -33,6 +33,8 @@ import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
 
 import javax.inject.Inject;
 
+import javafx.beans.InvalidationListener;
+
 import javafx.collections.ListChangeListener;
 
 import java.util.HashSet;
@@ -41,6 +43,17 @@ import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Listen on wallet change events, BSQ offer updates and feeService updates and
+ * recalculates the required funds for the open BSQ offers. If funds in wallets are not
+ * sufficient we close offers recursively until funds are sufficient.
+ * We notify listeners when we de-active an offer, so the UI can show a popup to the user.
+ * We do not re-activate offers after sufficient funds are available again but leave that
+ * to the user.
+ * At the moment there is no edit feature for BSQ offers so we do not need to treat the
+ * case that existing offers change, but once that is implemented that need to be covered
+ * as well.
+ */
 @Slf4j
 public class BsqSwapOfferManager implements WalletChangeEventListener {
 
@@ -51,7 +64,9 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
     private final FeeService feeService;
     private final KeyRing keyRing;
     private final Set<OpenOffer> openBsqSwapOffers = new HashSet<>();
-    private final ListChangeListener<OpenOffer> listChangeListener;
+    private final ListChangeListener<OpenOffer> offerListChangeListener;
+    private final InvalidationListener feeChangeListener;
+    private long txFeePerVbyte;
     private int tryFeeServiceCounter = 0;
 
     @Inject
@@ -68,12 +83,19 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
         this.feeService = feeService;
         this.keyRing = keyRing;
 
-        listChangeListener = c -> {
+        offerListChangeListener = c -> {
             c.next();
             if (c.wasAdded()) {
                 onOpenOffersAdded(c.getAddedSubList());
             } else if (c.wasRemoved()) {
                 onOpenOffersRemoved(c.getRemoved());
+            }
+        };
+        feeChangeListener = observable -> {
+            long value = feeService.getTxFeePerVbyte().value;
+            if (value != txFeePerVbyte) {
+                txFeePerVbyte = value;
+                updateFundingForAllOffers();
             }
         };
     }
@@ -83,17 +105,21 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
     // API
     ///////////////////////////////////////////////////////////////////////////////////////////
 
+
     public void onAllServicesInitialized() {
         btcWalletService.addChangeEventListener(this);
         bsqWalletService.addChangeEventListener(this);
-        openOfferManager.getObservableList().addListener(listChangeListener);
+        feeService.feeUpdateCounterProperty().addListener(feeChangeListener);
+        openOfferManager.getObservableList().addListener(offerListChangeListener);
+
         onOpenOffersAdded(openOfferManager.getObservableList());
     }
 
     public void shutDown() {
         btcWalletService.removeChangeEventListener(this);
         bsqWalletService.removeChangeEventListener(this);
-        openOfferManager.getObservableList().removeListener(listChangeListener);
+        feeService.feeUpdateCounterProperty().removeListener(feeChangeListener);
+        openOfferManager.getObservableList().removeListener(offerListChangeListener);
     }
 
 
@@ -102,19 +128,22 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
     ///////////////////////////////////////////////////////////////////////////////////////////
 
     public void onWalletChanged(Wallet wallet) {
-        openBsqSwapOffers.forEach(this::updateFundingState);
+        updateFundingForAllOffers();
     }
-
 
     ///////////////////////////////////////////////////////////////////////////////////////////
     // Private
     ///////////////////////////////////////////////////////////////////////////////////////////
 
+    private void updateFundingForAllOffers() {
+        openBsqSwapOffers.forEach(this::updateFundingForOffer);
+    }
+
     private void onOpenOffersAdded(List<? extends OpenOffer> list) {
         list.stream().filter(openOffer -> openOffer.getOffer().isBsqSwapOffer())
                 .forEach(openOffer -> {
                     openBsqSwapOffers.add(openOffer);
-                    updateFundingState(openOffer);
+                    updateFundingForOffer(openOffer);
                 });
     }
 
@@ -123,13 +152,13 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
                 .forEach(openBsqSwapOffers::remove);
     }
 
-    private void updateFundingState(OpenOffer openOffer) {
+    private void updateFundingForOffer(OpenOffer openOffer) {
         if (!feeService.isFeeAvailable() && tryFeeServiceCounter < 10) {
             // At startup the fee service might not be available still so we call again after a delay
             UserThread.runAfter(() -> {
                 tryFeeServiceCounter++;
                 if (openBsqSwapOffers.contains(openOffer)) {
-                    updateFundingState(openOffer);
+                    updateFundingForOffer(openOffer);
                 }
             }, 10);
             return;
@@ -144,7 +173,17 @@ public class BsqSwapOfferManager implements WalletChangeEventListener {
         }
     }
 
+
     private boolean isFunded(Offer offer) {
+        /*if (offer.isBuyOffer()) {
+            // BTC buyer requires BSQ inputs
+            Coin requiredBsqInputs = BsqSwapCalculation.getBuyersRequiredBsqInputs(offer);
+        } else {
+            // BTC seller requires BTC inputs but as we do not know tx size which depends on peers inputs
+            // we can only use an average tx size for our estimation.
+
+        }*/
+
         var isMaker = offer.isMyOffer(keyRing);
         var isBuyer = isMaker == offer.isBuyOffer();
         var price = offer.getPrice();
