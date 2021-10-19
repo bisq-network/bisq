a2d1e8888d0c6274121428b86cdd39b32f3d6ba0
chimp1984 <chimp1984@gmx.com>
2021-10-19 21:18:20 +0200

    Move AvailabilityResult into package bisq.core.offer.availability

--- core/src/main/java/bisq/core/proto/ProtoDevUtil.java
+++ core/src/main/java/bisq/core/proto/ProtoDevUtil.java
@@ -18,9 +18,9 @@
 package bisq.core.proto;
 
 import bisq.core.btc.model.AddressEntry;
-import bisq.core.offer.AvailabilityResult;
 import bisq.core.offer.Offer;
 import bisq.core.offer.OpenOffer;
+import bisq.core.offer.availability.AvailabilityResult;
 import bisq.core.offer.bisq_v1.OfferPayload;
 import bisq.core.support.dispute.DisputeResult;
 import bisq.core.trade.Trade;
