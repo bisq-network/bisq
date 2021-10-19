042476a68a22ea95eb9faaa84eeed8594a6c3d06
chimp1984 <chimp1984@gmx.com>
2021-10-19 21:16:46 +0200

    Move MutableOfferPayloadFields into package bisq.core.offer.bisq_v1

--- core/src/main/java/bisq/core/offer/OfferUtil.java
+++ core/src/main/java/bisq/core/offer/OfferUtil.java
@@ -25,6 +25,7 @@ import bisq.core.locale.CurrencyUtil;
 import bisq.core.locale.Res;
 import bisq.core.monetary.Price;
 import bisq.core.monetary.Volume;
+import bisq.core.offer.bisq_v1.MutableOfferPayloadFields;
 import bisq.core.offer.bisq_v1.OfferPayload;
 import bisq.core.payment.CashByMailAccount;
 import bisq.core.payment.F2FAccount;
