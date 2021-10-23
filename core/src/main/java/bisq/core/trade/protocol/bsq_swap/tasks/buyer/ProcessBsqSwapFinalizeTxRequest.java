ecdda101ad19ce0e258a5e49a8cdaa8d4ed4fc1f
chimp1984 <chimp1984@gmx.com>
2021-10-23 20:40:44 +0200

    Improve naming, comments, extract variables

--- core/src/main/java/bisq/core/trade/protocol/bsq_swap/tasks/buyer/ProcessBsqSwapFinalizeTxRequest.java
+++ core/src/main/java/bisq/core/trade/protocol/bsq_swap/tasks/buyer/ProcessBsqSwapFinalizeTxRequest.java
@@ -66,42 +66,44 @@ public abstract class ProcessBsqSwapFinalizeTxRequest extends BsqSwapTask {
             checkNotNull(request);
             Validator.checkTradeId(protocolModel.getOfferId(), request);
 
-            // We will use only the sellers inputs from the tx so we do not verify anything else
+            // We will use only the sellers buyersBsqInputs from the tx so we do not verify anything else
             byte[] tx = request.getTx();
             Transaction sellersTransaction = protocolModel.getBtcWalletService().getTxFromSerializedTx(tx);
-            List<RawTransactionInput> rawInputs = request.getBtcInputs();
-            checkArgument(!rawInputs.isEmpty(), "Sellers BTC inputs must not be empty");
+            List<RawTransactionInput> sellersRawBtcInputs = request.getBtcInputs();
+            checkArgument(!sellersRawBtcInputs.isEmpty(), "Sellers BTC buyersBsqInputs must not be empty");
 
-            List<RawTransactionInput> inputs = protocolModel.getInputs();
-            int buyersInputSize = Objects.requireNonNull(inputs).size();
+            List<RawTransactionInput> buyersBsqInputs = protocolModel.getInputs();
+            int buyersInputSize = Objects.requireNonNull(buyersBsqInputs).size();
             List<TransactionInput> sellersBtcInputs = sellersTransaction.getInputs().stream()
-                    .filter(i -> i.getIndex() >= buyersInputSize)
+                    .filter(input -> input.getIndex() >= buyersInputSize)
                     .collect(Collectors.toList());
-            checkArgument(sellersBtcInputs.size() == rawInputs.size(),
-                    "Number of inputs in tx must match the number of rawInputs");
+            checkArgument(sellersBtcInputs.size() == sellersRawBtcInputs.size(),
+                    "Number of buyersBsqInputs in tx must match the number of sellersRawBtcInputs");
 
-            boolean hasUnSignedInputs = sellersBtcInputs.stream().anyMatch(i -> i.getScriptSig() == null && !i.hasWitness());
-            checkArgument(!hasUnSignedInputs, "Inputs from tx has unsigned inputs");
-
-            long sumInputs = rawInputs.stream().mapToLong(rawTransactionInput -> rawTransactionInput.value).sum();
-            int sellersTxSize = BsqSwapCalculation.getVBytesSize(rawInputs, request.getBtcChange());
-            long sellersBtcInputAmount = BsqSwapCalculation.getSellersBtcInputValue(trade, sellersTxSize, getSellersTradeFee()).getValue();
-            // It can be that there have been dust change which got added to miner fees
-            checkArgument(sumInputs >= sellersBtcInputAmount,
-                    "Sellers BTC input amount do not match our calculated required BTC input amount");
+            boolean hasUnSignedInputs = sellersBtcInputs.stream()
+                    .anyMatch(input -> input.getScriptSig() == null && !input.hasWitness());
+            checkArgument(!hasUnSignedInputs, "SellersBtcInputs from tx has unsigned inputs");
 
             long change = request.getBtcChange();
             checkArgument(change == 0 || Restrictions.isAboveDust(Coin.valueOf(change)),
                     "BTC change must be 0 or above dust");
 
-            int buyersTxSize = BsqSwapCalculation.getVBytesSize(inputs, protocolModel.getChange());
-            long buyersTxFee = BsqSwapCalculation.getAdjustedTxFee(trade, buyersTxSize, getBuyersTradeFee());
-            long sellersTxFee = BsqSwapCalculation.getAdjustedTxFee(trade, sellersTxSize, getSellersTradeFee());
+            long sumInputs = sellersRawBtcInputs.stream().mapToLong(input -> input.value).sum();
+            int sellersTxSize = BsqSwapCalculation.getVBytesSize(sellersRawBtcInputs, change);
+            long sellersBtcInputAmount = BsqSwapCalculation.getSellersBtcInputValue(trade, sellersTxSize, getSellersTradeFee()).getValue();
+            // It can be that there have been dust change which got added to miner fees, so sumInputs could be a bit larger.
+            checkArgument(sumInputs >= sellersBtcInputAmount,
+                    "Sellers BTC input amount do not match our calculated required BTC input amount");
+
+            int buyersTxSize = BsqSwapCalculation.getVBytesSize(buyersBsqInputs, protocolModel.getChange());
+            long txFeePerVbyte = trade.getTxFeePerVbyte();
+            long buyersTxFee = BsqSwapCalculation.getAdjustedTxFee(txFeePerVbyte, buyersTxSize, getBuyersTradeFee());
+            long sellersTxFee = BsqSwapCalculation.getAdjustedTxFee(txFeePerVbyte, sellersTxSize, getSellersTradeFee());
             long buyersBtcPayout = protocolModel.getPayout();
             long expectedChange = sumInputs - buyersBtcPayout - sellersTxFee - buyersTxFee;
             boolean isChangeAboveDust = Restrictions.isAboveDust(Coin.valueOf(expectedChange));
             if (expectedChange != change && isChangeAboveDust) {
-                log.warn("Sellers BTC change is not as expected. This can happen if fee estimation for inputs did not " +
+                log.warn("Sellers BTC change is not as expected. This can happen if fee estimation for buyersBsqInputs did not " +
                         "succeed (e.g. dust change, max. iterations reached,...");
                 log.warn("buyersBtcPayout={}, sumInputs={}, sellersTxFee={}, buyersTxFee={}, expectedChange={}, change={}",
                         buyersBtcPayout, sumInputs, sellersTxFee, buyersTxFee, expectedChange, change);
@@ -124,7 +126,7 @@ public abstract class ProcessBsqSwapFinalizeTxRequest extends BsqSwapTask {
             BsqSwapTradePeer tradePeer = protocolModel.getTradePeer();
             tradePeer.setTx(tx);
             tradePeer.setTransactionInputs(sellersBtcInputs);
-            tradePeer.setInputs(rawInputs);
+            tradePeer.setInputs(sellersRawBtcInputs);
             tradePeer.setChange(change);
             tradePeer.setBtcAddress(sellersBtcChangeAddress);
             tradePeer.setBsqAddress(sellersBsqPayoutAddress);
