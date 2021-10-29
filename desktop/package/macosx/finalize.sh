50798f0d628c02b937636026e25c54963e8c0401
Christoph Atteneder <christoph.atteneder@gmail.com>
2021-10-29 16:06:49 +0200

    Revert to SNAPSHOT version

--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-version="1.7.5"
+version="1.7.5-SNAPSHOT"
 
 target_dir="releases/$version"
 
--- desktop/package/macosx/insert_snapshot_version.sh
+++ desktop/package/macosx/insert_snapshot_version.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)/../../../
 
-version=1.7.4
+version=1.7.5
 
 find . -type f \( -name "finalize.sh" \
 -o -name "create_app.sh" \
