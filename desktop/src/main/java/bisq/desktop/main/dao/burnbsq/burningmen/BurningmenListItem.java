39a49adb924495772dbfd09d5c79c0c049e0bc29
HenrikJannsen <boilingfrog@gmx.com>
2022-11-03 17:43:31 -0500

    Add Burningman UI
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

---
+++ desktop/src/main/java/bisq/desktop/main/dao/burnbsq/burningmen/BurningmenListItem.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.desktop.main.dao.burnbsq.burningmen;
+
+import bisq.core.dao.burningman.BurningManCandidate;
+import bisq.core.util.FormattingUtils;
+import bisq.core.util.coin.BsqFormatter;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+class BurningmenListItem {
+    private final BurningManCandidate burningManCandidate;
+    private final String name, burnOutputShareAsString, effectiveBurnOutputShareAsString, issuanceShareAsString,
+            accumulatedDecayedBurnAmountAsBsq, allowedBurnAmountAsBsq, accumulatedBurnAmountAsBsq,
+            accumulatedDecayedCompensationAmountAsBsq, accumulatedCompensationAmountAsBsq;
+    private final long allowedBurnAmount, accumulatedDecayedBurnAmount, accumulatedBurnAmount,
+            accumulatedDecayedCompensationAmount, accumulatedCompensationAmount;
+    private final int numBurnOutputs, numIssuances;
+    private final double burnOutputShare, effectiveBurnOutputShare, issuanceShare;
+
+    BurningmenListItem(String name, BurningManCandidate burningManCandidate, BsqFormatter bsqFormatter) {
+        this.burningManCandidate = burningManCandidate;
+
+        this.name = name;
+
+        // Burn
+        allowedBurnAmount = burningManCandidate.getAllowedBurnAmount();
+        allowedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(allowedBurnAmount);
+        accumulatedBurnAmount = burningManCandidate.getAccumulatedBurnAmount();
+        accumulatedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedBurnAmount);
+        accumulatedDecayedBurnAmount = burningManCandidate.getAccumulatedDecayedBurnAmount();
+        accumulatedDecayedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedDecayedBurnAmount);
+        burnOutputShare = burningManCandidate.getBurnOutputShare();
+        burnOutputShareAsString = FormattingUtils.formatToPercentWithSymbol(burnOutputShare);
+        effectiveBurnOutputShare = burningManCandidate.getEffectiveBurnOutputShare();
+        effectiveBurnOutputShareAsString = FormattingUtils.formatToPercentWithSymbol(effectiveBurnOutputShare);
+        numBurnOutputs = burningManCandidate.getBurnOutputModels().size();
+
+        // Issuance
+        accumulatedCompensationAmount = burningManCandidate.getAccumulatedCompensationAmount();
+        accumulatedCompensationAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedCompensationAmount);
+        accumulatedDecayedCompensationAmount = burningManCandidate.getAccumulatedDecayedCompensationAmount();
+        accumulatedDecayedCompensationAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedDecayedCompensationAmount);
+        issuanceShare = burningManCandidate.getIssuanceShare();
+        issuanceShareAsString = FormattingUtils.formatToPercentWithSymbol(issuanceShare);
+        numIssuances = burningManCandidate.getCompensationModels().size();
+    }
+}
