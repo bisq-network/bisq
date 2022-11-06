cebc48cb96e13ffd141e362e052d4d3ea7dfcf41
HenrikJannsen <boilingfrog@gmx.com>
2022-11-06 16:10:33 -0500

    Rename getBtcFeeReceiverAddress to getAddress Cleanup
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

--- core/src/main/java/bisq/core/offer/placeoffer/bisq_v1/tasks/CreateMakerFeeTx.java
+++ core/src/main/java/bisq/core/offer/placeoffer/bisq_v1/tasks/CreateMakerFeeTx.java
@@ -66,7 +66,7 @@ public class CreateMakerFeeTx extends Task<PlaceOfferModel> {
             TradeWalletService tradeWalletService = model.getTradeWalletService();
 
             if (offer.isCurrencyForMakerFeeBtc()) {
-                String feeReceiver = model.getBtcFeeReceiverService().getBtcFeeReceiverAddress();
+                String feeReceiver = model.getBtcFeeReceiverService().getAddress();
                 tradeWalletService.createBtcTradingFeeTx(
                         fundingAddress,
                         reservedForTradeAddress,
