3fda9495268559bd3bd6a2e3caad2cef51a14d3b
HenrikJannsen <boilingfrog@gmx.com>
2022-11-06 13:37:32 -0500

    Add sorting for output index needed for genesis tx outputs. Use static factory methods for the 2 diff. use cases of CompensationModel.
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

--- core/src/main/java/bisq/core/dao/burningman/CompensationModel.java
+++ core/src/main/java/bisq/core/dao/burningman/CompensationModel.java
@@ -18,6 +18,7 @@
 package bisq.core.dao.burningman;
 
 import java.util.Date;
+import java.util.Optional;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,26 +26,65 @@ import lombok.Getter;
 @Getter
 @EqualsAndHashCode
 public final class CompensationModel {
+    public static CompensationModel fromCompensationRequest(String address,
+                                                            long amount,
+                                                            long decayedAmount,
+                                                            int height,
+                                                            String txId,
+                                                            long date,
+                                                            int cycleIndex) {
+        return new CompensationModel(address,
+                amount,
+                decayedAmount,
+                height,
+                txId,
+                Optional.empty(),
+                date,
+                cycleIndex);
+    }
+
+    public static CompensationModel fromGenesisOutput(String address,
+                                                      long amount,
+                                                      long decayedAmount,
+                                                      int height,
+                                                      String txId,
+                                                      int outputIndex,
+                                                      long date,
+                                                      int cycleIndex) {
+        return new CompensationModel(address,
+                amount,
+                decayedAmount,
+                height,
+                txId,
+                Optional.of(outputIndex),
+                date,
+                cycleIndex);
+    }
+
+
     private final String address;
     private final long amount;
     private final long decayedAmount;
     private final int height;
     private final String txId;
+    private final Optional<Integer> outputIndex; // Only set for genesis tx outputs as needed for sorting
     private final long date;
     private final int cycleIndex;
 
-    public CompensationModel(String address,
-                             long amount,
-                             long decayedAmount,
-                             int height,
-                             String txId,
-                             long date,
-                             int cycleIndex) {
+    private CompensationModel(String address,
+                              long amount,
+                              long decayedAmount,
+                              int height,
+                              String txId,
+                              Optional<Integer> outputIndex,
+                              long date,
+                              int cycleIndex) {
         this.address = address;
         this.amount = amount;
         this.decayedAmount = decayedAmount;
         this.height = height;
         this.txId = txId;
+        this.outputIndex = outputIndex;
         this.date = date;
         this.cycleIndex = cycleIndex;
     }
@@ -57,6 +97,7 @@ public final class CompensationModel {
                 ",\r\n               decayedAmount=" + decayedAmount +
                 ",\r\n               height=" + height +
                 ",\r\n               txId='" + txId + '\'' +
+                ",\r\n               outputIndex=" + outputIndex +
                 ",\r\n               date=" + new Date(date) +
                 ",\r\n               cycleIndex=" + cycleIndex +
                 "\r\n          }";
