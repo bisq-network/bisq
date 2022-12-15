d58545673276699265570d965a9e7931e9a97f4e
HenrikJannsen <boilingfrog@gmx.com>
2022-12-15 11:18:54 -0500

    Refactor: move out fields used the same way in both if/else branches.
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

--- desktop/src/main/java/bisq/desktop/main/dao/burnbsq/burningman/BurningManListItem.java
+++ desktop/src/main/java/bisq/desktop/main/dao/burnbsq/burningman/BurningManListItem.java
@@ -50,30 +50,29 @@ class BurningManListItem {
         this.name = name;
         address = burningManCandidate.getMostRecentAddress().orElse(Res.get("shared.na"));
 
+        adjustedBurnAmountShare = burningManCandidate.getAdjustedBurnAmountShare();
+        cappedBurnAmountShare = burningManCandidate.getCappedBurnAmountShare();
+        accumulatedBurnAmount = burningManCandidate.getAccumulatedBurnAmount();
+        accumulatedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedBurnAmount);
+        numBurnOutputs = burningManCandidate.getBurnOutputModels().size();
+
         if (burningManCandidate instanceof LegacyBurningMan) {
             // Burn
             burnTarget = 0;
-            burnTargetAsBsq = "";
             maxBurnTarget = 0;
-            accumulatedBurnAmount = burningManCandidate.getAccumulatedBurnAmount();
-            accumulatedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedBurnAmount);
+            burnTargetAsBsq = "";
+
             accumulatedDecayedBurnAmount = 0;
             accumulatedDecayedBurnAmountAsBsq = "";
 
-            adjustedBurnAmountShare = burningManCandidate.getAdjustedBurnAmountShare();
-
-            cappedBurnAmountShare = burningManCandidate.getCappedBurnAmountShare();
-
             // LegacyBurningManForDPT is the one defined by DAO voting, so only that would receive BTC if new BM do not cover 100%.
             if (burningManPresentationService.getLegacyBurningManForDPT().equals(burningManCandidate)) {
-                expectedRevenue = burningManPresentationService.getExpectedRevenue(burningManCandidate);
                 cappedBurnAmountShareAsString = FormattingUtils.formatToPercentWithSymbol(adjustedBurnAmountShare);
+                expectedRevenue = burningManPresentationService.getExpectedRevenue(burningManCandidate);
             } else {
-                expectedRevenue = 0;
                 cappedBurnAmountShareAsString = FormattingUtils.formatToPercentWithSymbol(0);
+                expectedRevenue = 0;
             }
-            expectedRevenueAsBsq = bsqFormatter.formatCoinWithCode(expectedRevenue);
-            numBurnOutputs = burningManCandidate.getBurnOutputModels().size();
 
             // There is no issuance for legacy BM
             accumulatedCompensationAmount = 0;
@@ -90,12 +89,9 @@ class BurningManListItem {
             burnTarget = burnTargetTuple.first;
             maxBurnTarget = burnTargetTuple.second;
             burnTargetAsBsq = Res.get("dao.burningman.burnTarget.fromTo", bsqFormatter.formatCoin(burnTarget), bsqFormatter.formatCoin(maxBurnTarget));
-            accumulatedBurnAmount = burningManCandidate.getAccumulatedBurnAmount();
-            accumulatedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedBurnAmount);
+
             accumulatedDecayedBurnAmount = burningManCandidate.getAccumulatedDecayedBurnAmount();
             accumulatedDecayedBurnAmountAsBsq = bsqFormatter.formatCoinWithCode(accumulatedDecayedBurnAmount);
-            adjustedBurnAmountShare = burningManCandidate.getAdjustedBurnAmountShare();
-            cappedBurnAmountShare = burningManCandidate.getCappedBurnAmountShare();
             if (adjustedBurnAmountShare != cappedBurnAmountShare) {
                 cappedBurnAmountShareAsString = Res.get("dao.burningman.table.burnAmountShare.capped",
                         FormattingUtils.formatToPercentWithSymbol(cappedBurnAmountShare),
@@ -104,8 +100,6 @@ class BurningManListItem {
                 cappedBurnAmountShareAsString = FormattingUtils.formatToPercentWithSymbol(cappedBurnAmountShare);
             }
             expectedRevenue = burningManPresentationService.getExpectedRevenue(burningManCandidate);
-            expectedRevenueAsBsq = bsqFormatter.formatCoinWithCode(expectedRevenue);
-            numBurnOutputs = burningManCandidate.getBurnOutputModels().size();
 
             // Issuance
             accumulatedCompensationAmount = burningManCandidate.getAccumulatedCompensationAmount();
@@ -117,5 +111,8 @@ class BurningManListItem {
             numIssuances = burningManCandidate.getCompensationModels().size();
             numIssuancesAsString = String.valueOf(numIssuances);
         }
+
+        expectedRevenueAsBsq = bsqFormatter.formatCoinWithCode(expectedRevenue);
+
     }
 }
