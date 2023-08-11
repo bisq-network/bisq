b223a658e5701fe3e118d88a27db6fa7c1e216e6
HenrikJannsen <boilingfrog@gmx.com>
2023-08-11 13:23:15 +0200

    Add new tasks for BuyerAsMakerProtocol_v5. We might generalize later to BuyerProtocol_v5, but for now we start with one variant (BuyerAsMakerProtocol_v5 and SellerAsTakerProtocol_v5)
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

---
+++ core/src/main/java/bisq/core/trade/protocol/bisq_v5/tasks/buyer/BuyerCreatesWarningTx.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.trade.protocol.bisq_v5.tasks.buyer;
+
+import bisq.core.btc.model.AddressEntry;
+import bisq.core.btc.wallet.BtcWalletService;
+import bisq.core.btc.wallet.TradeWalletService;
+import bisq.core.trade.model.bisq_v1.Trade;
+import bisq.core.trade.protocol.bisq_v1.tasks.TradeTask;
+import bisq.core.trade.protocol.bisq_v5.model.StagedPayoutTxParameters;
+
+import bisq.common.taskrunner.TaskRunner;
+import bisq.common.util.Tuple2;
+
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionOutput;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BuyerCreatesWarningTx extends TradeTask {
+    public BuyerCreatesWarningTx(TaskRunner<Trade> taskHandler, Trade trade) {
+        super(taskHandler, trade);
+    }
+
+    @Override
+    protected void run() {
+        try {
+            runInterceptHook();
+
+            TradeWalletService tradeWalletService = processModel.getTradeWalletService();
+            BtcWalletService btcWalletService = processModel.getBtcWalletService();
+            String tradeId = processModel.getOffer().getId();
+
+            Transaction depositTx = btcWalletService.getTxFromSerializedTx(processModel.getPreparedDepositTx());
+            TransactionOutput depositTxOutput = depositTx.getOutput(0);
+            long lockTime = trade.getLockTime();
+            byte[] buyerPubKey = processModel.getMyMultiSigPubKey();
+            byte[] sellerPubKey = processModel.getTradePeer().getMultiSigPubKey();
+            long claimDelay = StagedPayoutTxParameters.CLAIM_DELAY;
+            long miningFee = StagedPayoutTxParameters.getWarningTxMiningFee(trade.getDepositTxFeeRate());
+            AddressEntry feeBumpAddressEntry = btcWalletService.getOrCreateAddressEntry(tradeId, AddressEntry.Context.WARNING_TX_FEE_BUMP);
+            Tuple2<Long, String> feeBumpOutputAmountAndAddress = new Tuple2<>(StagedPayoutTxParameters.WARNING_TX_FEE_BUMP_OUTPUT_VALUE, feeBumpAddressEntry.getAddressString());
+
+            Transaction unsignedWarningTx = tradeWalletService.createUnsignedWarningTx(true,
+                    depositTxOutput,
+                    lockTime,
+                    buyerPubKey,
+                    sellerPubKey,
+                    claimDelay,
+                    miningFee,
+                    feeBumpOutputAmountAndAddress);
+            processModel.setUnsignedWarningTx(unsignedWarningTx);
+
+            processModel.getTradeManager().requestPersistence();
+
+            complete();
+        } catch (Throwable t) {
+            failed(t);
+        }
+    }
+}
