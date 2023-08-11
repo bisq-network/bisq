c903e9cca71d868c66f5efc9086b4167c83357fa
HenrikJannsen <boilingfrog@gmx.com>
2023-08-11 10:24:19 +0200

    Refactoring: Use SellerProtocol interfaces instead of BaseSellerProtocol at client code
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

--- core/src/main/java/bisq/core/api/CoreTradesService.java
+++ core/src/main/java/bisq/core/api/CoreTradesService.java
@@ -38,8 +38,8 @@ import bisq.core.trade.model.Tradable;
 import bisq.core.trade.model.TradeModel;
 import bisq.core.trade.model.bisq_v1.Trade;
 import bisq.core.trade.model.bsq_swap.BsqSwapTrade;
-import bisq.core.trade.protocol.bisq_v1.BaseSellerProtocol;
 import bisq.core.trade.protocol.bisq_v1.BuyerProtocol;
+import bisq.core.trade.protocol.bisq_v1.SellerProtocol;
 import bisq.core.user.User;
 import bisq.core.util.validation.BtcAddressValidator;
 
@@ -236,7 +236,7 @@ class CoreTradesService {
                                 trade.getId()));
             }
             var tradeProtocol = tradeManager.getTradeProtocol(trade);
-            ((BaseSellerProtocol) tradeProtocol).onPaymentReceived(
+            ((SellerProtocol) tradeProtocol).onPaymentReceived(
                     () -> {
                     },
                     errorMessage -> {
