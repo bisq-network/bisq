6d15d626c9eb6e1a25e7f995b899d8aeb694d54a
HenrikJannsen <boilingfrog@gmx.com>
2023-08-11 21:14:23 +0200

    Add InputsForDepositTxResponse_v5 message
    Signed-off-by: HenrikJannsen <boilingfrog@gmx.com>

--- core/src/main/java/bisq/core/proto/network/CoreNetworkProtoResolver.java
+++ core/src/main/java/bisq/core/proto/network/CoreNetworkProtoResolver.java
@@ -67,6 +67,7 @@ import bisq.core.trade.protocol.bisq_v1.messages.RefreshTradeStateRequest;
 import bisq.core.trade.protocol.bisq_v1.messages.ShareBuyerPaymentAccountMessage;
 import bisq.core.trade.protocol.bisq_v1.messages.TraderSignedWitnessMessage;
 import bisq.core.trade.protocol.bisq_v5.messages.InputsForDepositTxResponse_v5;
+import bisq.core.trade.protocol.bisq_v5.messages.StagedPayoutTxRequest;
 import bisq.core.trade.protocol.bsq_swap.messages.BsqSwapFinalizeTxRequest;
 import bisq.core.trade.protocol.bsq_swap.messages.BsqSwapFinalizedTxMessage;
 import bisq.core.trade.protocol.bsq_swap.messages.BsqSwapTxInputsMessage;
@@ -165,8 +166,6 @@ public class CoreNetworkProtoResolver extends CoreProtoResolver implements Netwo
                     return InputsForDepositTxRequest.fromProto(proto.getInputsForDepositTxRequest(), this, messageVersion);
                 case INPUTS_FOR_DEPOSIT_TX_RESPONSE:
                     return InputsForDepositTxResponse.fromProto(proto.getInputsForDepositTxResponse(), this, messageVersion);
-                case INPUTS_FOR_DEPOSIT_TX_RESPONSE_V_5:
-                    return InputsForDepositTxResponse_v5.fromProto(proto.getInputsForDepositTxResponseV5(), this, messageVersion);
                 case DEPOSIT_TX_MESSAGE:
                     return DepositTxMessage.fromProto(proto.getDepositTxMessage(), messageVersion);
                 case DELAYED_PAYOUT_TX_SIGNATURE_REQUEST:
@@ -266,6 +265,13 @@ public class CoreNetworkProtoResolver extends CoreProtoResolver implements Netwo
                     return GetAccountingBlocksResponse.fromProto(proto.getGetAccountingBlocksResponse(), messageVersion);
                 case NEW_ACCOUNTING_BLOCK_BROADCAST_MESSAGE:
                     return NewAccountingBlockBroadcastMessage.fromProto(proto.getNewAccountingBlockBroadcastMessage(), messageVersion);
+
+                // Trade protocol v5 messages
+                case INPUTS_FOR_DEPOSIT_TX_RESPONSE_V_5:
+                    return InputsForDepositTxResponse_v5.fromProto(proto.getInputsForDepositTxResponseV5(), this, messageVersion);
+                case STAGED_PAYOUT_TX_REQUEST:
+                    return StagedPayoutTxRequest.fromProto(proto.getStagedPayoutTxRequest(), messageVersion);
+
 
                 default:
                     throw new ProtobufferException("Unknown proto message case (PB.NetworkEnvelope). messageCase=" +
---
+++ core/src/main/java/bisq/core/trade/protocol/bisq_v5/messages/StagedPayoutTxRequest.java
@@ -0,0 +1,124 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.trade.protocol.bisq_v5.messages;
+
+import bisq.core.trade.protocol.TradeMessage;
+
+import bisq.network.p2p.DirectMessage;
+import bisq.network.p2p.NodeAddress;
+
+import bisq.common.app.Version;
+import bisq.common.util.Utilities;
+
+import com.google.protobuf.ByteString;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode(callSuper = true)
+@Getter
+public final class StagedPayoutTxRequest extends TradeMessage implements DirectMessage {
+    private final NodeAddress senderNodeAddress;
+    private final byte[] sellersWarningTx;
+    private final byte[] sellersWarningTxSellerSignature;
+    private final byte[] sellersRedirectionTx;
+    private final byte[] sellersRedirectionTxSellerSignature;
+    private final byte[] buyersWarningTxSellerSignature;
+
+    public StagedPayoutTxRequest(String uid,
+                                 String tradeId,
+                                 NodeAddress senderNodeAddress,
+                                 byte[] sellersWarningTx,
+                                 byte[] sellersWarningTxSellerSignature,
+                                 byte[] sellersRedirectionTx,
+                                 byte[] sellersRedirectionTxSellerSignature,
+                                 byte[] buyersWarningTxSellerSignature) {
+        this(Version.getP2PMessageVersion(),
+                uid,
+                tradeId,
+                senderNodeAddress,
+                sellersWarningTx,
+                sellersWarningTxSellerSignature,
+                sellersRedirectionTx,
+                sellersRedirectionTxSellerSignature,
+                buyersWarningTxSellerSignature);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // PROTO BUFFER
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    private StagedPayoutTxRequest(int messageVersion,
+                                  String uid,
+                                  String tradeId,
+                                  NodeAddress senderNodeAddress,
+                                  byte[] sellersWarningTx,
+                                  byte[] sellersWarningTxSellerSignature,
+                                  byte[] sellersRedirectionTx,
+                                  byte[] sellersRedirectionTxSellerSignature,
+                                  byte[] buyersWarningTxSellerSignature) {
+        super(messageVersion, tradeId, uid);
+        this.senderNodeAddress = senderNodeAddress;
+        this.sellersWarningTx = sellersWarningTx;
+        this.sellersWarningTxSellerSignature = sellersWarningTxSellerSignature;
+        this.sellersRedirectionTx = sellersRedirectionTx;
+        this.sellersRedirectionTxSellerSignature = sellersRedirectionTxSellerSignature;
+        this.buyersWarningTxSellerSignature = buyersWarningTxSellerSignature;
+    }
+
+
+    @Override
+    public protobuf.NetworkEnvelope toProtoNetworkEnvelope() {
+        return getNetworkEnvelopeBuilder()
+                .setStagedPayoutTxRequest(protobuf.StagedPayoutTxRequest.newBuilder()
+                        .setUid(uid)
+                        .setTradeId(tradeId)
+                        .setSenderNodeAddress(senderNodeAddress.toProtoMessage())
+                        .setSellersWarningTx(ByteString.copyFrom(sellersWarningTx))
+                        .setSellersWarningTxSellerSignature(ByteString.copyFrom(sellersWarningTxSellerSignature))
+                        .setSellersRedirectionTx(ByteString.copyFrom(sellersRedirectionTx))
+                        .setSellersRedirectionTxSellerSignature(ByteString.copyFrom(sellersRedirectionTxSellerSignature))
+                        .setBuyersWarningTxSellerSignature(ByteString.copyFrom(buyersWarningTxSellerSignature)))
+                .build();
+    }
+
+    public static StagedPayoutTxRequest fromProto(protobuf.StagedPayoutTxRequest proto,
+                                                  int messageVersion) {
+        return new StagedPayoutTxRequest(messageVersion,
+                proto.getUid(),
+                proto.getTradeId(),
+                NodeAddress.fromProto(proto.getSenderNodeAddress()),
+                proto.getSellersWarningTx().toByteArray(),
+                proto.getSellersWarningTxSellerSignature().toByteArray(),
+                proto.getSellersRedirectionTx().toByteArray(),
+                proto.getSellersRedirectionTxSellerSignature().toByteArray(),
+                proto.getBuyersWarningTxSellerSignature().toByteArray());
+    }
+
+    @Override
+    public String toString() {
+        return "StagedPayoutTxRequest{" +
+                "\r\n     senderNodeAddress=" + senderNodeAddress +
+                ",\r\n     sellersWarningTx=" + Utilities.bytesAsHexString(sellersWarningTx) +
+                ",\r\n     sellersWarningTxSellerSignature=" + Utilities.bytesAsHexString(sellersWarningTxSellerSignature) +
+                ",\r\n     sellersRedirectionTx=" + Utilities.bytesAsHexString(sellersRedirectionTx) +
+                ",\r\n     sellersRedirectionTxSellerSignature=" + Utilities.bytesAsHexString(sellersRedirectionTxSellerSignature) +
+                ",\r\n     buyersWarningTxSellerSignature=" + Utilities.bytesAsHexString(buyersWarningTxSellerSignature) +
+                "\r\n} " + super.toString();
+    }
+}
