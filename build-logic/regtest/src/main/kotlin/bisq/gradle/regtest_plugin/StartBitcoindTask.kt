4575d2a229bcd811bd6a64b6bc4ae64c4186da98
Alva Swanson <alvasw@protonmail.com>
2023-09-11 16:55:11 +0200

    Implement startBitcoindRegtest Gradle Task

---
+++ build-logic/regtest/src/main/kotlin/bisq/gradle/regtest_plugin/StartBitcoindTask.kt
@@ -0,0 +1,47 @@
+package bisq.gradle.regtest_plugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.TaskAction
+
+abstract class StartBitcoindTask : DefaultTask() {
+
+    @get:InputDirectory
+    abstract val dataDirectory: DirectoryProperty
+
+    @get:Input
+    abstract val rpcUser: Property<String>
+
+    @get:Input
+    abstract val rpcPassword: Property<String>
+
+    @get:Input
+    abstract val blockNotifyArg: Property<String>
+
+    @TaskAction
+    fun run() {
+        val processBuilder = ProcessBuilder(
+                "bitcoind",
+                "-datadir=${dataDirectory.asFile.get().absolutePath}",
+                "-regtest",
+
+                "-prune=0",
+                "-txindex=1",
+                "-peerbloomfilters=1",
+                "-server",
+
+                "-rpcuser=${rpcUser.get()}",
+                "-rpcpassword=${rpcPassword.get()}",
+
+                "-blocknotify= ${blockNotifyArg.get()}"
+        )
+
+        processBuilder.redirectErrorStream(true)
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.DISCARD)
+
+        processBuilder.start()
+    }
+}
