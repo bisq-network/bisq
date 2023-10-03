58b4376e6fea5c6c089d79ca3496b1f25b49eb77
Alva Swanson <alvasw@protonmail.com>
2023-10-03 21:26:04 +0200

    seednode-docker: Run java binary directly

--- build-logic/docker-image-builder/src/main/java/bisq/gradle/docker/image_builder/DockerImageBuilderPlugin.kt
+++ build-logic/docker-image-builder/src/main/java/bisq/gradle/docker/image_builder/DockerImageBuilderPlugin.kt
@@ -2,13 +2,22 @@ package bisq.gradle.docker.image_builder
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaApplication
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Sync
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Tar
+import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.register
+import java.io.File
 
 class DockerImageBuilderPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val javaApplicationExtension = project.extensions.findByType<JavaApplication>()
+        checkNotNull(javaApplicationExtension) { "Can't find JavaApplication extension." }
+
+        val installDistTask: TaskProvider<Sync> = project.tasks.named("installDist", Sync::class.java)
         val distTarTask: TaskProvider<Tar> = project.tasks.named("distTar", Tar::class.java)
 
         val copyTask = project.tasks.register<Copy>("copyDistTar") {
@@ -18,6 +27,14 @@ class DockerImageBuilderPlugin : Plugin<Project> {
 
         project.tasks.register<CreateDockerfileTask>("generateDockerfile") {
             archiveFileName.set(distTarTask.flatMap { it.archiveFileName })
+
+            val classpathFiles: Provider<List<String>> = installDistTask.map { syncTask ->
+                val appLibsDir = File(syncTask.destinationDir, "lib")
+                appLibsDir.listFiles()!!.map { it.name }
+            }
+            classpathFileNames.set(classpathFiles)
+
+            mainClassName.set(javaApplicationExtension.mainClass)
             outputFile.set(project.layout.buildDirectory.file("docker/Dockerfile"))
         }
 
