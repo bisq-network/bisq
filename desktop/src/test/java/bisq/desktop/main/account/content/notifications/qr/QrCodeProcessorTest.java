dd255d00cf1c59a3621e2234f3f5ad6a7879875d
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2024-06-27 23:26:58 -0700

    Disable QrCodeProcessorTest on macOS

--- desktop/src/test/java/bisq/desktop/main/account/content/notifications/qr/QrCodeProcessorTest.java
+++ desktop/src/test/java/bisq/desktop/main/account/content/notifications/qr/QrCodeProcessorTest.java
@@ -20,6 +20,8 @@ package bisq.desktop.main.account.content.notifications.qr;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static bisq.desktop.main.account.content.notifications.qr.FrameUtil.createFrameFromImageResource;
 import static bisq.desktop.main.account.content.notifications.qr.FrameUtil.createRandomFrame;
@@ -32,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.google.zxing.BinaryBitmap;
 import org.bytedeco.javacv.Frame;
 
+@DisabledOnOs(value= {OS.MAC})
 class QrCodeProcessorTest {
     private final FrameConverter<BinaryBitmap> converter = new FrameToBitmapConverter();
     private final QrCodeProcessor processor = new QrCodeProcessor(converter);
