0fa9c873953768dd747de6789fd47e13ac230aea
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2025-05-01 22:34:57 -0700

    Remove my BSQ explorer
    I can retire my node now that @runbtc added his BSQ explorer
    in bisq-network#7422.

--- core/src/main/java/bisq/core/user/Preferences.java
+++ core/src/main/java/bisq/core/user/Preferences.java
@@ -117,7 +117,6 @@ public final class Preferences implements PersistedDataHost, BridgeAddressProvid
     ));
 
     public static final ArrayList<BlockChainExplorer> BSQ_MAIN_NET_EXPLORERS = new ArrayList<>(Arrays.asList(
-            new BlockChainExplorer("mempool.bisq.services (@devinbileck)", "https://mempool.bisq.services/tx/", "https://mempool.bisq.services/address/"),
             new BlockChainExplorer("BSQ Explorer - Tor (@runbtc)", "http://runbtcxzz4v2haszypwbrn2baqdo7tlwt6dw7g27cwwaootd4gktwayd.onion/tx/", "http://runbtcxzz4v2haszypwbrn2baqdo7tlwt6dw7g27cwwaootd4gktwayd.onion/address/")
     ));
 
@@ -129,7 +128,8 @@ public final class Preferences implements PersistedDataHost, BridgeAddressProvid
             "bsq.ninja",
             "bsq.sqrrm.net",
             "bsq.vante.me",
-            "mempool.space"
+            "mempool.space",
+            "mempool.bisq.services"
     );
 
     private static final ArrayList<String> XMR_TX_PROOF_SERVICES_CLEAR_NET = new ArrayList<>(Arrays.asList(
