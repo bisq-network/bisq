b9e785cb0896c3bef961e0a4158be638341d4395
Alva Swanson <alvasw@protonmail.com>
2025-12-01 18:48:31 +0000

    build-logic: Migrate to Gradle 8.0

--- build-logic/gradle-tasks/src/main/kotlin/bisq/gradle/tasks/PgpFingerprint.kt
+++ build-logic/gradle-tasks/src/main/kotlin/bisq/gradle/tasks/PgpFingerprint.kt
@@ -1,7 +1,9 @@
 package bisq.gradle.tasks
 
+import java.util.*
+
 object PgpFingerprint {
     fun normalize(fingerprint: String): String =
         fingerprint.filterNot { it.isWhitespace() }  // Remove all spaces
-            .toLowerCase()
+            .lowercase(Locale.getDefault())
 }
