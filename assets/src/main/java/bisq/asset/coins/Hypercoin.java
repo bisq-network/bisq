bb5c5f617dd011c23396a69b6059dbb2dfdc4e84
Hypercoin-net <hypercoin.network@gmail.com>
2026-06-02 12:09:53 +0700

    Create Hypercoin.java

---
+++ assets/src/main/java/bisq/asset/coins/Hypercoin.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.asset.coins;
+
+import bisq.asset.BitcoinAddressValidator;
+import bisq.asset.Coin;
+import bisq.asset.NetworkParametersAdapter;
+
+public class Hypercoin extends Coin {
+    public Hypercoin() {
+        super("Hypercoin", "HRC", new BitcoinAddressValidator(new HypercoinMainNetParams()), Network.MAINNET);
+    }
+
+    public static class HypercoinMainNetParams extends NetworkParametersAdapter {
+        public HypercoinMainNetParams() {
+            this.addressHeader = 0;
+            this.p2shHeader = 5;
+            this.segwitAddressHrp = "hc";
+        }
+    }
+}
