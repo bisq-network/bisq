41838319228b558024d35feb02031750d5ef9e65
HenrikJannsen <boilingfrog@gmx.com>
2026-06-04 12:18:39 +0700

    Add extraDataMap TreeMap serialization tests
    Cover the CompensationProposal and MailboxStoragePayload single-entry extraDataMap paths added in the previous commits. The tests compare protobuf bytes produced from the new TreeMap-backed domain paths with equivalent generated protobuf messages populated from HashMap, and verify fromProto keeps the data in TreeMap without changing single-entry bytes.

---
+++ core/src/test/java/bisq/core/dao/state/model/governance/CompensationProposalTest.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.dao.state.model.governance;
+
+import org.bitcoinj.core.Coin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CompensationProposalTest {
+    @Test
+    public void singleEntryExtraDataMapSerializesLikeHashMap() {
+        TreeMap<String, String> extraDataMap = new TreeMap<>();
+        extraDataMap.put(CompensationProposal.BURNING_MAN_RECEIVER_ADDRESS, "receiverAddress");
+
+        CompensationProposal proposal = new CompensationProposal(
+                "name",
+                "link",
+                Coin.valueOf(10_000),
+                "bsqAddress",
+                extraDataMap);
+
+        protobuf.Proposal treeMapProto = proposal.toProtoMessage();
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put(CompensationProposal.BURNING_MAN_RECEIVER_ADDRESS, "receiverAddress");
+        protobuf.Proposal hashMapProto = treeMapProto.toBuilder()
+                .clearExtraData()
+                .putAllExtraData(hashMap)
+                .build();
+
+        assertArrayEquals(hashMapProto.toByteArray(), treeMapProto.toByteArray());
+    }
+
+    @Test
+    public void fromProtoConvertsExtraDataMapToTreeMapWithoutChangingSingleEntryBytes() {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put(CompensationProposal.BURNING_MAN_RECEIVER_ADDRESS, "receiverAddress");
+        protobuf.Proposal hashMapProto = protobuf.Proposal.newBuilder()
+                .setName("name")
+                .setLink("link")
+                .setVersion(1)
+                .setCreationDate(1)
+                .setTxId("txId")
+                .setCompensationProposal(protobuf.CompensationProposal.newBuilder()
+                        .setRequestedBsq(10_000)
+                        .setBsqAddress("bsqAddress"))
+                .putAllExtraData(hashMap)
+                .build();
+
+        CompensationProposal proposal = CompensationProposal.fromProto(hashMapProto);
+
+        assertTrue(proposal.getExtraDataMap() instanceof TreeMap);
+        assertArrayEquals(hashMapProto.toByteArray(), proposal.toProtoMessage().toByteArray());
+    }
+}
