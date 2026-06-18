ff36958467e3d7f95434f30094c33433c590c05a
Alejandro García <alejandro.garcia@disroot.org>
2026-06-18 18:46:46 +0000

    Bump version number for v1.10.2

--- build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/PackagingPlugin.kt
+++ build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/PackagingPlugin.kt
@@ -22,7 +22,7 @@ import javax.inject.Inject
 class PackagingPlugin @Inject constructor(private val javaToolchainService: JavaToolchainService) : Plugin<Project> {
 
     companion object {
-        const val APP_VERSION = "1.10.1"
+        const val APP_VERSION = "1.10.2"
         const val OUTPUT_DIR_PATH = "packaging/jpackage/packages"
     }
 
